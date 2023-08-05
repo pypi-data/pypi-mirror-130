8749f0bb72eafb4cf40cc7dd9abdf9122e476e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:41 +0000

    Add package sphinxcontrib-seqdiag/3.0.0/sphinxcontrib-seqdiag-3.0.0.tar.gz

---
+++ packages/sphinxcontrib-seqdiag/sphinxcontrib-seqdiag-3.0.0.tar.gz/sphinxcontrib-seqdiag-3.0.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requires = ['blockdiag>=1.5.0', 'seqdiag>=0.9.3', 'Sphinx>=2.0']
+
+setup(
+    name='sphinxcontrib-seqdiag',
+    version='3.0.0',
+    url='https://github.com/blockdiag/sphinxcontrib-seqdiag',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    description='Sphinx "seqdiag" extension',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
