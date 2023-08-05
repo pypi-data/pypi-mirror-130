399a490111a549f4f465115f61df34022e7859c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:26 +0000

    Add package SLUSpell/0.1.4/SLUSpell-0.1.4.tar.gz

---
+++ packages/SLUSpell/SLUSpell-0.1.4.tar.gz/SLUSpell-0.1.4/sluspell/main.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# Spell_Checker
+# Developers: --
+# License: -
+
+
+# added for debugging purposes
+import logging, traceback 
+import pickle as pkl 
+import os 
+import sys
+
+
+_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(_path))
+from flask import Flask, render_template, request, jsonify
+from apps.spellchecker import SpellChecker
+
+
+with open(os.path.join(_path,'templates','assets','main_page.pkl'), 'rb') as f:
+    main_page = pkl.load(f)
+
+
+app = Flask(__name__)
+
+
+@app.route('/',  methods =['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        page_lang = 'en'
+        return render_template('index.html' , message = main_page[page_lang])
+    elif request.method == 'POST':
+        page_lang = request.form['main_page_lang']
+        if page_lang == "en":
+            return render_template('index.html', message = main_page[page_lang])
+        elif page_lang == "ga": 
+            return render_template('index.html', message = main_page[page_lang])
+        elif page_lang == "fa":
+            return render_template('index.html', message = main_page[page_lang])
+        elif page_lang == "es":
+            return render_template('index.html', message = main_page[page_lang])
+        else:
+            return render_template('index.html' , message = main_page['en'])
+    else:
+        return "Something went wrong while loading the page!"
+
+
+@app.route('/check', methods =['GET','POST'])
+def check():
+    try:
+        if request.method == 'POST':      
+            data = request.get_json(force=True)
+            input_text = data['input_text'] 
+            language_selector = data['language_selector'] 
+            formality_selector = data['formality_selector']             
+            if input_text and language_selector and formality_selector:
+                global spell_checker # Global Variable 
+                spell_checker = SpellChecker(input_text, language_selector, formality_selector)
+                spellChecked_text = spell_checker.call()
+                return jsonify({'checked_text' : spellChecked_text })
+            return jsonify({'error' : "Something went wrong!" })        
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        error_message = 'Something went wrong! Please try latter!'
+        return render_template('index.html', checked_text = error_message)
+
+@app.route('/correct', methods =['GET','POST'])
+def correct():
+    try:
+        if request.method == 'POST':      
+            data = request.get_json(force=True)
+            element_id = data['element_id'] 
+            list_index = data['list_index'] 
+            
+            spellChecked_text = spell_checker.call_corrector(element_id, list_index)
+
+            return jsonify({'checked_text' : spellChecked_text })
+        return jsonify({'error' : "Something went wrong!" })        
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        error_message = 'Something went wrong! Please try latter!'
+        return render_template('index.html', checked_text = error_message)
+            
+    
+
+@app.route('/login', methods =['GET', 'Post'])
+def login():
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+    else:
+        username = request.form['Username'] 
+        password = request.form['Password']
+        if (username).lower() == 'kscanne@gmail.com' and password.lower() == 'programmingisfun':
+            return render_template('auth/login.html', message = 'Login Successful!')
+        else: 
+            error_message = 'The Login Information is Incorrect!'
+            return render_template('auth/login.html', message = error_message)
+
+@app.route('/register', methods =['GET'])
+def register():
+    return render_template('auth/register.html')
+
+
+def main():
+    app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/SLUSpell/SLUSpell-0.1.4.tar.gz/SLUSpell-0.1.4/sluspell/static/js/async.js
@@ -0,0 +1,177 @@
+
+function WordDictionary(input_json){   
+        let word_list = [];
+       
+ 
+        let n = 0;
+        for (let key in input_json) {
+            n++;
+        }
+        for (let i = 0; i < n; i++) {
+            let temp = input_json[i];
+            if (temp['correction_flag']){
+              word_list.push(temp['new_string']); // &nbsp;    .replace(/ /g, '\u00a0')
+
+            }else{
+              word_list.push(temp['new_string'].replace(/ /g, '\u00a0')); // &nbsp;    
+            }
+              
+            }
+
+             
+        let wordString = word_list.join(''); 
+        return wordString;   
+  };
+
+
+function ShowMisspelled(input_json){
+    let word_id = [];
+    let word_list = [];
+    let temp_string = [];
+    let n = 0;
+
+    for (let key in input_json) {
+        n++;
+    }
+    for (let i = 0; i < n; i++) {
+        let temp = input_json[i];
+        if (temp['correction_flag'] == true){
+            word_list.push(temp['correct']);   
+            word_id.push(temp['id']);
+            // temp_string.push("<a class='hl_faded' id='word"+ temp['id'] + "\'" + "onmouseover='show_correct("+ temp['id'] +")' onclick='corrector("+temp['id']+")'>"+temp['correct']+"<a>");
+            // console.log(temp);
+            let new_string= '';
+            for (let j = 0; j < temp['correct'].length; j++) {
+                new_string = new_string + "<button type='button' class='btn btn-light word"+temp['id']+ " hl_"+j+" hl_faded' id='word"+ temp['id']+ "\'" + "onmouseover='show_correct("+ temp['id'] +")' onclick='corrector("+temp['id']+"," +j + ")'>"+temp['correct'][j]+"</button>" + "</br>";
+                    
+                }  
+                temp_string.push(new_string);  
+            }
+        }
+    let correct_string = temp_string.join('<br>'); 
+    return correct_string;   
+};
+
+
+
+  function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined" &&
+      typeof document.createRange != "undefined") {
+      var range = document.createRange();
+      range.selectNodeContents(el);
+      range.collapse(false);
+      var sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+      var textRange = document.body.createTextRange();
+      textRange.moveToElementText(el);
+      textRange.collapse(false);
+      textRange.select();
+    }
+  }
+
+
+
+function async(){
+      let textData = document.getElementById("Text_box").textContent;
+      let languageSelector = document.getElementById("Language_Selection").value;
+      let formalitySelector = document.getElementById("Formality_Selection").value;
+      
+
+        let xml = new XMLHttpRequest();
+        xml.open("POST", "/check", true);
+        xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xml.onload = function(event){
+            dataReply = JSON.parse(xml.responseText) ;// checked_text
+            // document.getElementById("Text_display").innerHTML = dataReply.checked_text;
+            // console.log(dataReply);
+            // console.log(dataReply.checked_text);
+
+            let html_string = WordDictionary(dataReply.checked_text);
+            // console.log("called from inside async\n"+html_string);
+            let corrected_string = ShowMisspelled(dataReply.checked_text);
+            // console.log("HTML String" + html_string);
+
+            
+            document.getElementById("Text_box").innerHTML = html_string;
+            document.getElementById("Text_display").innerHTML = corrected_string;
+            placeCaretAtEnd(document.getElementById("Text_box"));
+
+            REQUEST_STATUS = true ; 
+
+            // let input_element = document.getElementById("Text_box");
+            // input_element.focus();
+            // input_element.selectionStart = input_element.selectionEnd = input_element.innerHTML.length;
+
+            event.preventDefault();
+
+    };
+    dataSend = JSON.stringify({
+        'input_text' : textData,
+        'language_selector' : languageSelector,
+        'formality_selector' : formalitySelector
+    });
+    // REQUEST_STATUS = false; 
+    xml.send(dataSend)
+    
+
+    // event.preventDefault();
+};
+
+
+function async_correction(input_id, word_index){
+  let word = dataReply.checked_text[input_id];
+  dataReply.checked_text[input_id]['correction_flag'] = true;
+  dataReply.checked_text[input_id]['word'] = word['correct'][word_index];
+  dataReply.checked_text[input_id]['new_string'] = word['correct'][word_index];
+  dataReply.checked_text[input_id]['correction_flag'] = false;
+  dataReply.checked_text[input_id]['word'] = word['correct'][word_index];
+  dataReply.checked_text[input_id]['new_string'] = word['correct'][word_index];
+
+  // console.log(dataReply.checked_text[input_id])
+  let html_string = WordDictionary(dataReply.checked_text);
+  let corrected_string = ShowMisspelled(dataReply.checked_text);
+  
+  document.getElementById("Text_box").innerHTML = html_string;
+  document.getElementById("Text_display").innerHTML = corrected_string;
+  placeCaretAtEnd(document.getElementById("Text_box"));
+
+  REQUEST_STATUS = true ; 
+};
+
+
+// function async_correction(input_id, word_index){
+//     let element_id = input_id;    
+//     let list_index = word_index;
+
+//       let xml2 = new XMLHttpRequest();
+//       xml2.open("POST", "/correct", true);
+//       xml2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//       xml2.onload = function(event){
+//           let dataReply = JSON.parse(xml2.responseText) ;// checked_text
+//           // console.log(dataReply.checked_text);
+
+//           let html_string = WordDictionary(dataReply.checked_text);
+//           let corrected_string = ShowMisspelled(dataReply.checked_text);
+
+//         //   console.log("HTML String" + html_string);
+//           document.getElementById("Text_box").innerHTML = html_string;
+//           document.getElementById("Text_display").innerHTML = corrected_string;
+//           REQUEST_STATUS = true ; 
+
+//           event.preventDefault();
+//   };
+//   dataSend = JSON.stringify({
+//       'element_id' : element_id,
+//       'list_index' : list_index
+//   });
+//   // REQUEST_STATUS = false; 
+//   xml2.send(dataSend)
+  
+// //   event.preventDefault();
+  
+// };
+
+
