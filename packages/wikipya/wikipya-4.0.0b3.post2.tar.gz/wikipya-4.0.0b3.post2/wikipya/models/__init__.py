6c9351b17e85aa7a771f9461bd917b470456cdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:22 +0000

    Add package wikipya/4.0.0b3.post2/wikipya-4.0.0b3.post2.tar.gz

---
+++ packages/wikipya/wikipya-4.0.0b3.post2.tar.gz/wikipya-4.0.0b3.post2/wikipya/models/__init__.py
@@ -0,0 +1,17 @@
+from .page import Page
+from .image import Image
+from .images import Images
+from .search import Search, SearchResult
+from .opensearch import OpenSearch, OpenSearchResult
+from .url import URL
+from .summary import Summary
+
+
+__all__ = (
+    "Page",
+    "Image", "Images",
+    "Search", "SearchResult",
+    "OpenSearch", "OpenSearchResult",
+    "URL",
+    "Summary"
+)
