8f81b2878bd92e80236156296cb2639833b43b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package fact-checking/0.0.1/fact_checking-0.0.1.tar.gz

---
+++ packages/fact-checking/fact_checking-0.0.1.tar.gz/fact_checking-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='fact_checking',
+      version='0.0.1',
+      url='http://github.com/fractalego/zero-shot-relation-extractor',
+      author='Alberto Cetoli',
+      author_email='alberto@nlulite.com',
+      description="A zero-shot relation extractor",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['fact_checking',
+                ],
+      install_requires=[
+          'numpy==1.19.1',
+          'transformers==4.9.2',
+          'torch==1.9.0',
+          'jupyterlab==2.2.9',
+      ],
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+      ],
+      include_package_data=True,
+      zip_safe=False)
