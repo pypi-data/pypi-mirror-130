6d4725fb4318d839129af6c8b3077c1c5849820b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package caosadvancedtools/0.3.1/caosadvancedtools-0.3.1-py3-none-any.whl

---
+++ packages/caosadvancedtools/caosadvancedtools-0.3.1-py3-none-any.whl/caosadvancedtools/scifolder/__init__.py
@@ -0,0 +1,5 @@
+from .analysis_cfood import AnalysisCFood
+from .experiment_cfood import ExperimentCFood
+from .publication_cfood import PublicationCFood
+from .simulation_cfood import SimulationCFood
+from .software_cfood import SoftwareCFood
