cf459aa3ac091f9e3ffd8df0b90ce71e3643dd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:33 +0000

    Add package zjwocr/0.0.3/zjwocr-0.0.3.tar.gz

---
+++ packages/zjwocr/zjwocr-0.0.3.tar.gz/zjwocr-0.0.3/setup.py
@@ -0,0 +1,37 @@
+# !/usr/bin/python3
+# -*-coding:utf-8-*-
+# Author: zhou jun wei
+# CreatDate: 2021/12/3 11:38
+
+from setuptools import find_packages, setup
+# version = '0.0.1'
+# author = 'zhoujunwei'
+
+
+setup(
+    name='zjwocr',  #　包的名字
+    version='0.0.3',
+    description='test',
+    author='周军威',
+    author_email='505104341@qq.com',
+    url='https://github.com/zjw505104341/ocrimg',
+    # packages=find_packages(where='.', exclude=(), include=('*',)),
+    packages=["zjwocr"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'torch',
+        'torchaudio',
+        'torchvision'
+        'numpy'
+        'opencv-python'
+        'Pillow'
+        'requests'
+    ], # 依赖包
+    python_requires='>=3.7',# python 的版本
+    include_package_data=True, #
+    install_package_data=True,
+)
