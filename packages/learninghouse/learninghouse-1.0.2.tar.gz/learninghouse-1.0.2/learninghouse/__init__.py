1ee3026104b5bbd5d3cb16af249c8903c33a0992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:38 +0000

    Add package learninghouse/1.0.2/learninghouse-1.0.2.tar.gz

---
+++ packages/learninghouse/learninghouse-1.0.2.tar.gz/learninghouse-1.0.2/learninghouse/__init__.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi import __version__ as fastapi_version
+from numpy.version import version as np_version
+from pandas import __version__ as pd_version
+from pydantic import BaseModel
+from sklearn import __version__ as skl_version
+
+from learninghouse.models import LearningHouseVersions
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+versions = LearningHouseVersions(
+    service=__version__,
+    fastapi=fastapi_version,
+    sklearn=skl_version,
+    numpy=np_version,
+    pandas=pd_version
+)
