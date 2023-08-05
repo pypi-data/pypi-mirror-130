1b7373fa13633e77ffcd17c9ed0132263fa723ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:04 +0000

    Add package napari-skimage-regionprops/0.2.1/napari_skimage_regionprops-0.2.1-py3-none-any.whl

---
+++ packages/napari-skimage-regionprops/napari_skimage_regionprops-0.2.1-py3-none-any.whl/napari_skimage_regionprops/__init__.py
@@ -0,0 +1,7 @@
+from ._table import add_table, get_table, TableWidget
+from ._regionprops import regionprops
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.2.1"
