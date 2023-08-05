3137c0c79ff4a3836f8e9dd2dc8dd58b90c55d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:42 +0000

    Add package cdk-cloudfront-plus/0.3.170/cdk_cloudfront_plus-0.3.170-py3-none-any.whl

---
+++ packages/cdk-cloudfront-plus/cdk_cloudfront_plus-0.3.170-py3-none-any.whl/cdk_cloudfront_plus/_jsii/__init__.py
@@ -0,0 +1,36 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigatewayv2._jsii
+import aws_cdk.aws_apigatewayv2_integrations._jsii
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_cloudfront_origins._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kinesisfirehose._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_sam._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-cloudfront-plus",
+    "0.3.170",
+    __name__[0:-6],
+    "cdk-cloudfront-plus@0.3.170.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
