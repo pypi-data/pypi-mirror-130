ca7d6368fde38c43535ad9606178ba2bda0d2680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:06 +0000

    Add package pandas-diff/0.7.12/pandas_diff-0.7.12.tar.gz

---
+++ packages/pandas-diff/pandas_diff-0.7.12.tar.gz/pandas_diff-0.7.12/pandas_diff/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Pandas Diff."""
+
+__author__ = """Jaime Valero"""
+__email__ = 'jaimevalero78@gmail.com'
+__version__ = '0.7.12'
+
+import pandas as pd
+
+from pandas_diff.pandas_diff import get_diffs
