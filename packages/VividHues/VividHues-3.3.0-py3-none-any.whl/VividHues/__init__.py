42c7f1b570b4dddf6de23a8fdccf67b4ce372c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package VividHues/3.3.0/VividHues-3.3.0-py3-none-any.whl

---
+++ packages/VividHues/VividHues-3.3.0-py3-none-any.whl/VividHues/__init__.py
@@ -0,0 +1,23 @@
+import os
+os.system("")  # allows colours to be displayed in the console
+
+
+class Clr:
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    ORANGE = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    LIME = "\033[92m"
+    YELLOW = "\033[93m"
+    PINK = "\033[95m"
+    
+    UNDERLINE = "\033[4m"
+    UL = UNDERLINE
+    BOLD = "\033[01m"
+    BO = BOLD
+    RESET = "\033[0m"
+    RS = RESET
