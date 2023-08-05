2e447e3d06c9feae1052c9d4017445a979a8afaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:41 +0000

    Add package sqleasy/0.3/sqleasy-0.3.tar.gz

---
+++ packages/sqleasy/sqleasy-0.3.tar.gz/sqleasy-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.3'
+DESCRIPTION = 'A simple SQL system for beginners'
+
+# Setting up
+setup(
+    name="sqleasy",
+    version=VERSION,
+    author="sk4yx",
+    author_email="skayz.oficial3@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sqlite', 'database', 'sql3', 'mysql', 'sqleasy'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
