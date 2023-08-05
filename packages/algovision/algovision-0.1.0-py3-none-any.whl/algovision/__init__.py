74107418bc8b079e511cdd6262463596b5e9f2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:54 +0000

    Add package algovision/0.1.0/algovision-0.1.0-py3-none-any.whl

---
+++ packages/algovision/algovision-0.1.0-py3-none-any.whl/algovision/__init__.py
@@ -0,0 +1,4 @@
+from .core import Algorithm, Input, Output, Variable, Var, VariableInt, VarInt
+from .conditions import Eq, NEq, LT, LEq, GT, GEq, CatProbEq, CosineSimilarity, IsTrue, IsFalse
+from .control_structures import If, While, For
+from .functions import Let, LetInt, Print, Min, ArgMin, Max, ArgMax
