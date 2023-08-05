cf7742c9bc19b1f3b4f50dbe3dbd43d604857dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:27 +0000

    Add package rcn-production-api/0.0.1/rcn-production-api-0.0.1.tar.gz

---
+++ packages/rcn-production-api/rcn-production-api-0.0.1.tar.gz/rcn-production-api-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rcn-production-api",
+    version="0.0.1",
+    author="nikitacrit (Nikita Sidorov)",
+    author_email="nikitacrit@mail.ru",
+    description="Recommendation production api proto client lib",
+    packages=find_packages()
+)
