f856e5c9e0fec268c89d3c238ccc43373e5f6427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:46 +0000

    Add package fake-bpy-module-latest/20211206/fake_bpy_module_latest-20211206-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20211206-py3-none-any.whl/gpu/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import state
+from . import matrix
+from . import capabilities
+from . import platform
+from . import types
+from . import shader
+from . import texture
+from . import select
