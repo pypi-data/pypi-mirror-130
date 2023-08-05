cbc3e590fa52284cf4d9051fddb61ad5436fe3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:54 +0000

    Add package cca-zoo/1.10.5/cca_zoo-1.10.5-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.10.5-py3-none-any.whl/cca_zoo/__init__.py
@@ -0,0 +1,11 @@
+import cca_zoo.data
+import cca_zoo.models
+
+try:
+    import cca_zoo.deepmodels
+except ModuleNotFoundError:
+    pass
+try:
+    import cca_zoo.probabilisticmodels
+except ModuleNotFoundError:
+    pass
