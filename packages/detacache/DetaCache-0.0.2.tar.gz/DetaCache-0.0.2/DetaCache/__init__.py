fbc221e9420272607b53fbe321e1d8cc7845258d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:38 +0000

    Add package detacache/0.0.2/DetaCache-0.0.2.tar.gz

---
+++ packages/detacache/DetaCache-0.0.2.tar.gz/DetaCache-0.0.2/DetaCache/__init__.py
@@ -0,0 +1,7 @@
+from DetaCache._detaCache import detaCache
+
+
+__version__ ='v0.0.2'
+__all__=[
+    'detaCache'
+]
---
+++ packages/detacache/DetaCache-0.0.2.tar.gz/DetaCache-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name = 'DetaCache', 
+    packages = ['DetaCache'],
+    version = 'v0.0.2',
+    license='MIT', 
+    description = 'Decorator to cache to Deta base',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'vidya sagar', 
+    author_email = 'svidya051@gmail.com',
+    url = 'https://github.com/vidyasagar1432/JioSaavn',
+    # download_url = '',
+    keywords = ['deta','cache','base','cache api call'], 
+    install_requires=[
+        'deta',
+        ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent', 
+    'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
