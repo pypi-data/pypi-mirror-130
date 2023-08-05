4778468527bd0072d48f5a8af231c2a996ed75bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:22 +0000

    Add package pyflame/0.1.0/pyflame-0.1.0-py3-none-any.whl

---
+++ packages/pyflame/pyflame-0.1.0-py3-none-any.whl/pyflame/__main__.py
@@ -0,0 +1,6 @@
+# Copyright 2020-2021 Daniel Harding
+# Distributed as part of the pyflame project under the terms of the MIT license
+
+from pyflame.runner import main
+
+main()
---
+++ packages/pyflame/pyflame-0.1.0-py3-none-any.whl/pyflame/exceptions.py
@@ -0,0 +1,5 @@
+# Copyright 2020-2021 Daniel Harding
+# Distributed as part of the pyflame project under the terms of the MIT license
+
+class MissingFlamegraphScript(Exception):
+    pass
