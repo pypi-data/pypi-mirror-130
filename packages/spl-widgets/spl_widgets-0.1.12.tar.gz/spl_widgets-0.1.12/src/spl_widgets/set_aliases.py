b4a819a32a2dc9d876df6c82bca2f4db91c1f765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:08 +0000

    Add package spl-widgets/0.1.12/spl_widgets-0.1.12.tar.gz

---
+++ packages/spl-widgets/spl_widgets-0.1.12.tar.gz/spl_widgets-0.1.12/src/spl_widgets/set_aliases.py
@@ -0,0 +1,9 @@
+import os
+
+modules_to_alias=["update_widgets","widgets_help","gorilla_clean","stk_swx","tuner"]
+
+def main():
+    for module in modules_to_alias:
+        alias_str = f'alias {module}="python3 -m spl_widgets {module}"'
+        os.system(alias_str)
+        
