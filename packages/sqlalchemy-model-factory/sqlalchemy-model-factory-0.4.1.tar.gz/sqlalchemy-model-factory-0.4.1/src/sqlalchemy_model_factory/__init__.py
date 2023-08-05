e13740fe313a2fe40bc49b521570aa0c3247c372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package sqlalchemy-model-factory/0.4.1/sqlalchemy-model-factory-0.4.1.tar.gz

---
+++ packages/sqlalchemy-model-factory/sqlalchemy-model-factory-0.4.1.tar.gz/sqlalchemy-model-factory-0.4.1/src/sqlalchemy_model_factory/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from sqlalchemy_model_factory.base import ModelFactory
+from sqlalchemy_model_factory.declarative import declarative
+from sqlalchemy_model_factory.registry import register_at, Registry, registry
+from sqlalchemy_model_factory.utils import autoincrement, fluent, for_model
