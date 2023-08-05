e3065a35e51bea4e00865555eb4cf477f41ab293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package docrun/3.1.1.0/docrun-3.1.1.0.tar.gz

---
+++ packages/docrun/docrun-3.1.1.0.tar.gz/docrun-3.1.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name="docrun",
+        version="3.1.1.0",
+        author="Xiao, Hu",
+        author_email="service@ovo.ltd",
+        description="Package for https://doc.run",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://doc.run",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'websockets',
+            'paramiko',
+            'psutil',
+            'ipykernel',
+            'matlab-kernel',
+            'powershell_kernel',
+            'bash_kernel',
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+    )
