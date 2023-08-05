c348a9b4113a5e775c08181ff9f7740798c272db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:54 +0000

    Add package deepquantum/0.0.1/deepquantum-0.0.1.tar.gz

---
+++ packages/deepquantum/deepquantum-0.0.1.tar.gz/deepquantum-0.0.1/deepquantum/nn/__init__.py
@@ -0,0 +1,12 @@
+from .quconv import QuConvXYZ
+from .quconv import QuConvSXZ
+from .dequconv import DeQuConvXYZ
+from .dequconv import DeQuConvSXZ
+from .qupool import QuPoolXYZ
+from .qupool import QuPoolSX
+from .dequpool import DeQuPoolXYZ
+from .dequpool import DeQuPoolSX
+from .quconvAE import Qu_AEnet
+from .qugru import QuGRU
+from .qulinear import QuLinear
+
