80ab1211dd0bd82f42ed908138d148870f1a533b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:09 +0000

    Add package jury/2.1.4/jury-2.1.4.tar.gz

---
+++ packages/jury/jury-2.1.4.tar.gz/jury-2.1.4/jury/utils/common.py
@@ -0,0 +1,81 @@
+import os
+from typing import Any, Dict, List, Optional
+
+
+class NestedSingleType:
+    @staticmethod
+    def is_iterable(obj):
+        if isinstance(obj, str) or isinstance(obj, dict):
+            return False
+        try:
+            iter(obj)
+        except TypeError:
+            return False
+        return True
+
+    @staticmethod
+    def join(types: List[str]):
+        nested_types = f"{types.pop(-1)}"
+
+        for _type in types:
+            nested_types = f"{_type}<{nested_types}>"
+        return nested_types.lower()
+
+    @classmethod
+    def get_type(cls, obj, order: Optional[int] = None):
+        _obj = obj
+
+        types = []
+        while cls.is_iterable(_obj):
+            types.append(type(_obj).__name__)
+            _obj = _obj[0]
+        types.append(type(_obj).__name__)
+
+        if order is not None:
+            try:
+                return types[order]
+            except IndexError:
+                return None
+
+        return cls.join(types)
+
+
+def bulk_remove_keys(obj: Dict, keys: List[str]) -> Dict:
+    return {k: v for k, v in obj.items() if k not in keys}
+
+
+def get_common_keys(d1: Dict, d2: Dict) -> List[str]:
+    set1 = set(d1.keys())
+    set2 = set(d2.keys())
+
+    return list(set1.intersection(set2))
+
+
+def pop_item_from_dict(d: Dict[str, Any], key: str, default: Any = None, must_exists: bool = False):
+    """
+    Pops key from dict d if key exists, returns d otherwise.
+
+    Args:
+        d: (``Dict``) Dictionary for key to be removed.
+        key: (``str``) Key name to remove from dictionary d.
+        default: (``Any``) Default value to return if key not found.
+        must_exists: (``bool``) Raises an exception if True when given key does not exists.
+
+    Returns: Popped value for key if found, None otherwise.
+    """
+    if key not in d and must_exists:
+        raise KeyError(f"'{key}' not found in '{d}'.")
+    val = d.pop(key) if key in d else default
+    return val
+
+
+def replace(a: List, obj: object, index=-1):
+    del a[index]
+    a.insert(index, obj)
+    return a
+
+
+def set_env(name: str, value: str):
+    if not isinstance(value, str):
+        raise ValueError(f"Expected type str for 'value', got {type(value)}.")
+    os.environ[name] = value
