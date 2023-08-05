79a8ce9d60060a1786aba29f085c06f45f480a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package sagemath-repl/9.5b7/sagemath-repl-9.5b7.tar.gz

---
+++ packages/sagemath-repl/sagemath-repl-9.5b7.tar.gz/sagemath-repl-9.5b7/sage/all__sagemath_repl.py
@@ -0,0 +1,11 @@
+from .all__sagemath_objects import *
+from .all__sagemath_environment import *
+
+# FIXME: all.py should import from here and remove these imports.
+from sage.doctest.all    import *
+from sage.repl.all       import *
+
+# For doctesting. These are overwritten later
+
+Integer = int
+RealNumber = float
