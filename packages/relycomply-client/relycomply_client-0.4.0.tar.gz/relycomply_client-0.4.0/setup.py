e3360059b883ecf9cf0ae477293300fd03bac48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:39 +0000

    Add package relycomply-client/0.4.0/relycomply_client-0.4.0.tar.gz

---
+++ packages/relycomply-client/relycomply_client-0.4.0.tar.gz/relycomply_client-0.4.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['relycomply_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'Pygments>=2.10.0,<3.0.0',
+ 'boto3',
+ 'dryenv',
+ 'gql==3.0.0a1',
+ 'littleutils',
+ 'pandas',
+ 'pyarrow>=6.0.1,<7.0.0',
+ 'requests',
+ 'requests-toolbelt',
+ 'tabulate>=0.8.9,<0.9.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'toml>=0.10.2,<0.11.0']
+
+setup_kwargs = {
+    'name': 'relycomply-client',
+    'version': '0.4.0',
+    'description': 'A python client for the RelyComply platform',
+    'long_description': None,
+    'author': 'James Saunders',
+    'author_email': 'james@relycomply.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
