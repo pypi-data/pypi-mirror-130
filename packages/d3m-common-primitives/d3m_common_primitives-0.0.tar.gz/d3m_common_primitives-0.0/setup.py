63fb5a18ee86d97aced40e6052989abe5d4d4d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package d3m-common-primitives/0.0/d3m_common_primitives-0.0.tar.gz

---
+++ packages/d3m-common-primitives/d3m_common_primitives-0.0.tar.gz/d3m_common_primitives-0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='d3m_common_primitives',
+    version='0.0',
+    py_modules=['d3m_common_primitives'],
+    description='D3M common primitives',
+    url='https://gitlab.com/datadrivendiscovery/common-primitives',
+    license='Apache-2.0',
+    classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+    ],
+)
