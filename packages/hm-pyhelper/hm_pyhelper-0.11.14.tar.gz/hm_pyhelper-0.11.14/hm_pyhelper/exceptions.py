e6b790fb69058758da88c76bbf51fc90b72901a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:39 +0000

    Add package hm-pyhelper/0.11.14/hm_pyhelper-0.11.14.tar.gz

---
+++ packages/hm-pyhelper/hm_pyhelper-0.11.14.tar.gz/hm_pyhelper-0.11.14/hm_pyhelper/exceptions.py
@@ -0,0 +1,18 @@
+class MalformedRegionException(Exception):
+    pass
+
+
+class SPIUnavailableException(Exception):
+    pass
+
+
+class ECCMalfunctionException(Exception):
+    pass
+
+
+class GatewayMFRFileNotFoundException(Exception):
+    pass
+
+
+class MinerFailedToFetchMacAddress(Exception):
+    pass
