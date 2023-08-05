3098af5877a353ae88174c3bbf5246d957d85f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:42 +0000

    Add package ultrafastFitFunctions/0.3.0/ultrafastFitFunctions-0.3.0-py3-none-any.whl

---
+++ packages/ultrafastFitFunctions/ultrafastFitFunctions-0.3.0-py3-none-any.whl/ultrafastFitFunctions/__init__.py
@@ -0,0 +1,9 @@
+from .backgrounds import *
+from .dynamics import *
+from .miscellaneous import *
+#from .peaks.peaks import *
+#from .peaks.gauss import *
+#from .peaks.assymetries import *
+
+
+__version__ = '0.3.0'
