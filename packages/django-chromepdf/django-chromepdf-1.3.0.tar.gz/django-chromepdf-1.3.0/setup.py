eb6d172269181f909c06a67a1a129674f4577e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:16 +0000

    Add package django-chromepdf/1.3.0/django-chromepdf-1.3.0.tar.gz

---
+++ packages/django-chromepdf/django-chromepdf-1.3.0.tar.gz/django-chromepdf-1.3.0/setup.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version():
+    """Return the version string stored at chromepdf.__version__"""
+    with open(os.path.join(os.path.dirname(__file__), 'chromepdf', '__init__.py')) as fp:
+        return re.match(r".*__version__ = '(.*?)'", fp.read(), re.S).group(1)  # @UndefinedVariable
+
+
+def get_long_description():
+    """
+    Return a long description consisting of the readme's contents.
+    The contents are Markdown, so be sure to set the "long_description_content_type" accordingly.
+    """
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fp:
+        return fp.read()
+
+
+setup(
+    name='django-chromepdf',
+    version=get_version(),  # Make sure to update the string in chromepdf.__init__.__version__ too
+    description='ChromePDF is a small Python 3 library that uses Selenium and Google Chrome to convert HTML into a PDF.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Andrew Kukwa',
+    author_email='kukwaa@imsweb.com',
+    url='https://github.com/imsweb/django-chromepdf',
+    license='BSD',
+    packages=find_packages(exclude=('testapp',)),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'selenium<5,>=3'
+    ],
+)
