4ba288352dd0de926ec53bf1e53c95b38e4d43f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:50 +0000

    Add package fibonacci-lol/1.0.0/fibonacci_lol-1.0.0.tar.gz

---
+++ packages/fibonacci-lol/fibonacci_lol-1.0.0.tar.gz/fibonacci_lol-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup 
+
+setup(
+        name = 'fibonacci_lol',
+        version = '1.0.0',
+        py_modules = ['fibonacci_lol'] , 
+        author = 'Shammo',
+        author_email = 'khan1803015cse@gmail.com',
+        description = 'A programme to find Nth fibonacci Number',
+    )
+
