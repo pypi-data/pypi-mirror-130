35605ab7842ebc66c9b104f4351f628b50dc543d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package txt2pdf/0.7.1/txt2pdf-0.7.1.tar.gz

---
+++ packages/txt2pdf/txt2pdf-0.7.1.tar.gz/txt2pdf-0.7.1/txt2pdf/__init__.py
@@ -0,0 +1,13 @@
+"""txt2pdf, a Markdown to PDF conversion tool"""
+
+__title__ = "txt2pdf"
+__version__ = "0.7.1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2013-2021 Julien Maupetit & c4ffein"
+
+from .core import txt2pdf
+
+
+__all__ = [
+    "txt2pdf",
+]
