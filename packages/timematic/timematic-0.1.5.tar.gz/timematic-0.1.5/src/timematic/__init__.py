0b35b61de3e0c4c6e4d51d7ece57f6ec094ccdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:43 +0000

    Add package timematic/0.1.5/timematic-0.1.5.tar.gz

---
+++ packages/timematic/timematic-0.1.5.tar.gz/timematic-0.1.5/src/timematic/__init__.py
@@ -0,0 +1,3 @@
+"""Utilities for working with datetime."""
+
+__version__ = "0.1.5"
---
+++ packages/timematic/timematic-0.1.5.tar.gz/timematic-0.1.5/src/timematic/_constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+S_IN_MIN: Final = 60
+MIN_IN_H: Final = 60
+S_IN_H: Final = 3600
+US_IN_S: Final = 1000000
