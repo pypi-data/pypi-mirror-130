d332744ba308353cf95b7547dc594961fcd47006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:20 +0000

    Add package FamcyTools/0.0.21/FamcyTools-0.0.21-py3-none-any.whl

---
+++ packages/FamcyTools/FamcyTools-0.0.21-py3-none-any.whl/FamcyTools/__init__.py
@@ -0,0 +1,11 @@
+import os
+import pwd
+import FamcyTools
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+
+USERNAME = get_username()
+# TODO: in the future, this needs to be more modular
+FAMCY_DIR = "/home/%s/.local/share/famcy/%s/venv/lib/python3.7/site-packages/Famcy"
+FAMCYTOOLS_DIR = os.path.dirname(FamcyTools.__file__)
