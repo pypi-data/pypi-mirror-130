235a5bdf389156389684c03eb220265770b5e858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package cadwangpagckage1/0.1/cadwangpagckage1-0.1.tar.gz

---
+++ packages/cadwangpagckage1/cadwangpagckage1-0.1.tar.gz/cadwangpagckage1-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='cadwangpagckage1', # package name
+	version='0.1', # version
+	description='Package Description', # short description
+	url='http://example.com', # package URL
+	install_requires=[], # list of packages this package depends on.
+	packages=['cadwangpackage1'], # List of module names that installing
+	# this package will provide.
+)
+
+
