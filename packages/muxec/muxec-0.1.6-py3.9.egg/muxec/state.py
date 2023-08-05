84d702b0e2cc6f29cf0b30fed3d26671d3dea7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:34 +0000

    Add package muxec/0.1.6/muxec-0.1.6-py3.9.egg

---
+++ packages/muxec/muxec-0.1.6-py3.9.egg/muxec/state.py
@@ -0,0 +1,10 @@
+panes = []
+total = 0
+all_processes_to_rolling_output = {}
+completed_processes = set()
+failed_processes = set()
+exhausted = False
+break_on_fail = False
+stdScr = None
+full_height, full_width = None, None
+is_tty = True
