6a4dfe4d62fe32f229b629ade0edd54ec74f8a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package genovi/0.1.4/GenoVi-0.1.4.tar.gz

---
+++ packages/genovi/GenoVi-0.1.4.tar.gz/GenoVi-0.1.4/GenoVi/__init__.py
@@ -0,0 +1,7 @@
+from GenoVi import *
+from scripts import addText
+from scripts import create_raw
+from scripts import createConf
+from scripts import GC_analysis
+from scripts import genbank2faa
+from scripts import genbank2fna
