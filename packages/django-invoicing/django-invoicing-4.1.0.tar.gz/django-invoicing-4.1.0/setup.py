3ccc7c580ccf2ecc903a2475da64dafa67f2f3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:40 +0000

    Add package django-invoicing/4.1.0/django-invoicing-4.1.0.tar.gz

---
+++ packages/django-invoicing/django-invoicing-4.1.0.tar.gz/django-invoicing-4.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-invoicing',
+    version='4.1.0',
+    description='Django app for invoicing.',
+    long_description=open('README.rst').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-invoicing',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=(
+        'django>=2.2', 'django-countries', 'django-iban', 'django-model-utils', 'django-money',
+        'django-internationalflavor'
+    ),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha'
+    ],
+    license='GPL License',
+    keywords="django invoice invoicing billing supplier issuer purchaser buyer commerce products taxes vat pdf",
+)
