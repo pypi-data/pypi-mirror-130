4081908f17fa5f4924cd0888ee7b2524f7e96089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:03 +0000

    Add package products/0.3.2/products-0.3.2.tar.gz

---
+++ packages/products/products-0.3.2.tar.gz/products-0.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "products"
+version = "0.3.2"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=["parts~=1.2",],
+    license="MIT",
+    url="https://github.com/lapets/products",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Simple function for building ensembles of iterables "+\
+                "that are disjoint partitions of an overall Cartesian "+\
+                "product.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
