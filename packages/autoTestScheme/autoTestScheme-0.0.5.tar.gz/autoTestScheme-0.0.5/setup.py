4e33f007b32633f9bb4f6c04b5b2f1c413f5c2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:59 +0000

    Add package autoTestScheme/0.0.5/autoTestScheme-0.0.5.tar.gz

---
+++ packages/autoTestScheme/autoTestScheme-0.0.5.tar.gz/autoTestScheme-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import sys
+
+from setuptools import setup, find_packages
+
+packages = find_packages("src")
+
+setup(name='autoTestScheme',
+      version='0.0.5',
+      url='https://gitee.com/xiongrun/auto-test-scheme',
+      author='wuxing',
+      description='auto test scheme',
+      long_description='file: README.md',
+      long_description_content_type='text/markdown',
+      author_email='xr18668178362@163.com',
+      project_urls={'Bug Tracker': 'https://gitee.com/xiongrun/auto-test-scheme/issues'},
+      package_dir={'': 'src'},
+      packages=packages,
+      include_package_data=True,
+      entry_points={'pytest11': ['pytest_autoTestScheme = autoTestScheme']},
+      package_data={
+          'demo': ['demo/*'],
+          'autoTestScheme': ['allure/*'],
+      },
+      )
