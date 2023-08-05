e0be047071f1a73aeedf8013d8e8630298a29b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:26 +0000

    Add package snowflake-integration/0.3.0/snowflake_integration-0.3.0.tar.gz

---
+++ packages/snowflake-integration/snowflake_integration-0.3.0.tar.gz/snowflake_integration-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='snowflake_integration',
+    version='0.3.0',    
+    description='Its a package to integrate snowflake with sagemaker',
+    author='Shubham Setia',
+    author_email='shubham.setia.3701@gmail.com',    
+    license='BSD 2-clause',
+    packages=['snowflake_integration'],
+    setup_requires=['wheel'],
+    install_requires=['numpy','pandas','snowflake','boto3','snowflake-connector-python[secure-local-storage,pandas]',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)    
