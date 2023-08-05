3671f29944963f47bf949e0ef72cdcae34a3a036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:50 +0000

    Add package requests-abc/0.3.0/requests-abc-0.3.0.tar.gz

---
+++ packages/requests-abc/requests-abc-0.3.0.tar.gz/requests-abc-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='requests-abc',
+    version='0.3.0',
+    description='requests-abc',
+    url='https://github.com/JustIceQAQ/Introspection/tree/main/code/requests_abc',
+    author='justiceqaq',
+    author_email='justiceqaq@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={'requests_abc': ['requests_abc.py']},
+    keywords=['requests-abc'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+
+    ],
+    install_requires=['requests'],
+    python_requires=">=3.5"
+)
