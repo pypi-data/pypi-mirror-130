bc9043ed7502cbc84437ae0a836104266bd0711a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:20 +0000

    Add package munityapps/0.1.35/munityapps-0.1.35-py3-none-any.whl

---
+++ packages/munityapps/munityapps-0.1.35-py3-none-any.whl/munity/workspaces/models.py
@@ -0,0 +1,16 @@
+from django_extensions.db.models import TimeStampedModel
+from django.db import models
+
+
+class Workspace(TimeStampedModel):
+    class Meta:
+        ordering = ("slug",)
+
+    slug = models.SlugField(primary_key=True, max_length=50, unique=True)
+    db_connection = models.CharField(max_length=100, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return f"/workspaces/{self.slug}"
+
+    def __str__(self):
+        return self.slug
