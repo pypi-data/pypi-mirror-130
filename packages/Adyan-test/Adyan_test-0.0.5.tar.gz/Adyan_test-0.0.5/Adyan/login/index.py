0ca0f852fa9922891ff3d597bc5defff711f96e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:29 +0000

    Add package Adyan-test/0.0.5/Adyan_test-0.0.5.tar.gz

---
+++ packages/Adyan-test/Adyan_test-0.0.5.tar.gz/Adyan_test-0.0.5/Adyan/login/index.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/12/6 10:32
+# @Author  : Adyan
+# @File    : index.py
+
+
+from flask import Flask, request
+from flask_cors import CORS
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+from .cookies import Cookies
+from ..config_mongo import settings
+from ..Utils import ReidsClient
+
+monkey.patch_all()
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True
+)
+CORS(app, supports_credentials=True)
+set = settings.Settings
+
+
+def get_conn(config):
+    if not hasattr(g, 'redis_client'):
+        g.redis_client = ReidsClient(
+            name=config.get("PROXY_NAME"),
+            config={"HOST": config.get("HOST"), "PORT": config.get("PORT"), "DB": config.get("DB"),
+                    "PAW": config.get("PASSWORD")}
+        )
+    return g.redis_client
+
+
+# TB_detail
+@app.route('/cookie', methods=["post", "get"])
+def detail():
+    types = request.args.to_dict().get("type")
+    host = request.args.to_dict().get("host")
+    user = set(host=host).config
+    res = ReidsClient({"HOST": host, 'DB': 2}).redis_conn
+    ip = request.remote_addr
+    if res.exists(ip):
+        res.setrange(ip, 0, int(res.get(ip)) + 1)
+    else:
+        res.set(ip, 0, ex=5400)
+    code = int(res.get(ip))
+    if int(res.get(ip)) == 0:
+        res.set("cookie", Cookies().taobao_cookies(user.get(types)))
+        print(res.get("cookie"))
+        return {
+            "code": code,
+            "data": res.get("cookie").decode('utf-8'),
+        }
+    else:
+        return {
+            "code": code,
+            "data": res.get("cookie").decode('utf-8'),
+        }
+
+
+def start(host, prot):
+    """9090"""
+    app.run(host=host, port=prot)
+    http_server = WSGIServer((host, prot), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
