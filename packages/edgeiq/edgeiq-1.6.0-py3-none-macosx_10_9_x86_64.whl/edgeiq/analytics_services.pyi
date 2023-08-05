d414e9cc23ff5a53793c044e05d4522835181c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package edgeiq/1.6.0/edgeiq-1.6.0-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-1.6.0-py3-none-macosx_10_9_x86_64.whl/edgeiq/analytics_services.pyi
@@ -0,0 +1,11 @@
+from edgeiq import bounding_box as bounding_box, image_classification as image_classification, object_detection as object_detection, object_tracking as object_tracking, pose_estimation as pose_estimation
+from edgeiq._production_client import PRODUCTION_CLIENT as PRODUCTION_CLIENT
+from typing import Any
+
+POSE_ESTIMATION_RESULT: Any
+OBJECT_DETECTION_RESULT: Any
+CLASSIFICATION_RESULT: Any
+TRACKING_RESULT: Any
+
+def load_analytics_results(filepath): ...
+def publish_analytics(results, tag: Any | None = ...) -> None: ...
