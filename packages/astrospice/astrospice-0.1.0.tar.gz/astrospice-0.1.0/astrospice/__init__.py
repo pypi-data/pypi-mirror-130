3382a4506056fb6b1c2c4f42ef3300037bfe7246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package astrospice/0.1.0/astrospice-0.1.0.tar.gz

---
+++ packages/astrospice/astrospice-0.1.0.tar.gz/astrospice-0.1.0/astrospice/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import astrospice.time
+from astrospice.net.generic import *
+from astrospice.net.generic import _setup_generic_files
+from .body import *
+from .config import *
+from .coords import *
+from .kernel import *
+from .net import registry
+from .version import __version__
+
+_setup_generic_files()
---
+++ packages/astrospice/astrospice-0.1.0.tar.gz/astrospice-0.1.0/astrospice/net/sources/__init__.py
@@ -0,0 +1,3 @@
+from .psp import *
+from .solo import *
+from .stereo import *
