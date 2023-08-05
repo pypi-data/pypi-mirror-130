85674f0f0be9056ff0c04f92dcbed451c057619e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:20 +0000

    Add package doubledate/0.0.7/doubledate-0.0.7-py3-none-any.whl

---
+++ packages/doubledate/doubledate-0.0.7-py3-none-any.whl/doubledate/constants.py
@@ -0,0 +1,32 @@
+MON = "MON"
+TUE = "TUE"
+WED = "WED"
+THU = "THU"
+FRI = "FRI"
+SAT = "SAT"
+SUN = "SUN"
+
+WEEKDAYS = {MON: 0, TUE: 1, WED: 2, THU: 3, FRI: 4, SAT: 5, SUN: 6}
+
+Y = "Y"
+S = "H"
+H = "H"
+T = "T"
+Q = "Q"
+M = "M"
+W = "W"
+
+SOW = "WS"
+EOW = "WE"
+SOM = "MS"
+EOM = "ME"
+SOQ = "QS"
+EOQ = "QE"
+SOT = "TS"
+EOT = "TE"
+SOS = "SS"
+EOS = "SE"
+SOY = "YS"
+EOY = "YE"
+
+RAISE = "RAISE"
