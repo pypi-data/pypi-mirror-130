8a891adf817deab5685c4fedca923f783cbd965a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:22 +0000

    Add package exposan/1.0.2/exposan-1.0.2.tar.gz

---
+++ packages/exposan/exposan-1.0.2.tar.gz/exposan-1.0.2/exposan/bsm1/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+EXPOsan: Exposition of sanitation and resource recovery systems
+
+This module is developed by:
+    Joy Zhang <joycheung1994@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+import os
+bsm1_path = os.path.dirname(__file__)
+del os
+
+from . import system
+
+from .system import *
+
+__all__ = (
+    'bsm1_path',
+    *system.__all__,
+	)
