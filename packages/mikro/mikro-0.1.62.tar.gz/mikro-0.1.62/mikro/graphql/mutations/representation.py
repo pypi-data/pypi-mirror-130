f6bd3c77d8f96f637cfe2f200ddb4379eb3c58fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:42 +0000

    Add package mikro/0.1.62/mikro-0.1.62.tar.gz

---
+++ packages/mikro/mikro-0.1.62.tar.gz/mikro-0.1.62/mikro/graphql/mutations/representation.py
@@ -0,0 +1,43 @@
+from herre.wards.graphql import ParsedQuery
+
+REP_FRAGMENT = """
+  name
+  id
+  variety
+  tags
+  store
+  unique
+  meta
+  origin {
+    id
+    name
+  }
+"""
+
+
+CREATE_REPRESENTATION = ParsedQuery(
+    """
+mutation Representation($sample: ID, $name: String, $tags: [String], $variety: RepresentationVarietyInput, $creator: String, $meta: GenericScalar, $omero: OmeroRepresentationInput, $origin: ID){
+  createRepresentation(sample: $sample, name: $name, tags: $tags, variety: $variety, creator: $creator, meta: $meta, omero: $omero, origin: $origin){
+    dateRepresentation(rep: $id){
+    """
+    + REP_FRAGMENT
+    + """
+  
+  }
+}
+"""
+)
+
+UPDATE_REPRESENTATION = ParsedQuery(
+    """
+mutation Representation($id: ID!){
+  updateRepresentation(rep: $id){
+    """
+    + REP_FRAGMENT
+    + """
+
+  }
+}
+"""
+)
