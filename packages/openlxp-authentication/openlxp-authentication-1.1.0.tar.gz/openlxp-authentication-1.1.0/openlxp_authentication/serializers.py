a66ba17debe3edc89dd94b59be71a3eeff961585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:30 +0000

    Add package openlxp-authentication/1.1.0/openlxp-authentication-1.1.0.tar.gz

---
+++ packages/openlxp-authentication/openlxp-authentication-1.1.0.tar.gz/openlxp-authentication-1.1.0/openlxp_authentication/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import SAMLConfiguration
+
+
+class SAMLConfigurationSerializer(serializers.ModelSerializer):
+    """Serializes the SAMLConfiguration Model"""
+
+    class Meta:
+        model = SAMLConfiguration
+
+        fields = ['name', 'endpoint']
