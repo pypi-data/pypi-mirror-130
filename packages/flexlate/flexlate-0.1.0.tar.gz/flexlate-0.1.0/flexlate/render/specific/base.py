d6d1e777bb53199cd7f9ede09302e7721c0758a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package flexlate/0.1.0/flexlate-0.1.0.tar.gz

---
+++ packages/flexlate/flexlate-0.1.0.tar.gz/flexlate-0.1.0/flexlate/render/specific/base.py
@@ -0,0 +1,24 @@
+import abc
+from pathlib import Path
+from typing import Optional, TypeVar, Type, Protocol
+
+from flexlate.template.base import Template
+from flexlate.template.types import TemplateType
+from flexlate.template_data import TemplateData
+
+T = TypeVar("T", bound=Template)
+
+
+class SpecificTemplateRenderer(Protocol[T]):
+    # Override these in subclass
+    _template_cls: Type[T]
+    _template_type: TemplateType = TemplateType.BASE
+
+    def render(
+        self,
+        template: T,
+        data: Optional[TemplateData] = None,
+        out_path: Path = Path("."),
+        no_input: bool = False,
+    ) -> TemplateData:
+        ...
---
+++ packages/flexlate/flexlate-0.1.0.tar.gz/flexlate-0.1.0/tests/fileutils.py
@@ -0,0 +1,28 @@
+from tests.config import GENERATED_FILES_DIR
+
+COOKIECUTTER_ONE_GENERATED_TEXT_PATH = GENERATED_FILES_DIR / "b" / "text.txt"
+COOKIECUTTER_TWO_GENERATED_TEXT_PATH = GENERATED_FILES_DIR / "b" / "text2.txt"
+
+
+def _generated_text_content(folder: str, file: str) -> str:
+    path = GENERATED_FILES_DIR / folder / file
+    assert path.exists()
+    return path.read_text()
+
+
+def cookiecutter_one_generated_text_content(
+    folder: str = "b", file: str = "text.txt"
+) -> str:
+    return _generated_text_content(folder, file)
+
+
+def cookiecutter_two_generated_text_content(
+    folder: str = "b", file: str = "text2.txt"
+) -> str:
+    return _generated_text_content(folder, file)
+
+
+def preprend_cookiecutter_one_generated_text(content: str):
+    current_content = COOKIECUTTER_ONE_GENERATED_TEXT_PATH.read_text()
+    full_content = content + current_content
+    COOKIECUTTER_ONE_GENERATED_TEXT_PATH.write_text(full_content)
