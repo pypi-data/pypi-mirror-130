6b1a6817ed4d2a6bfd23dab3718f0c4eae410ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:28 +0000

    Add package sgci-resource/0.0.1/sgci_resource-0.0.1-py3-none-any.whl

---
+++ packages/sgci-resource/sgci_resource-0.0.1-py3-none-any.whl/sgci_resource/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Project Jupyter.
+# Distributed under the terms of the Modified BSD License.
+
+from .core import SCGICatalog
+from .core import SCGICatalogGithub
+from .core import SCGICatalogXsede
+from .core import SCGICatalogLocal
+
+from ._version import __version__, version_info
+
