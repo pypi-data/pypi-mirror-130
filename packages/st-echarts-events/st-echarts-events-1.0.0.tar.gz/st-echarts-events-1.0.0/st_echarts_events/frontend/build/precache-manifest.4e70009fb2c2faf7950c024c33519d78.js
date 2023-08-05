072802835a31c8a5d1f2ad27d8b973ba049bde15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package st-echarts-events/1.0.0/st-echarts-events-1.0.0.tar.gz

---
+++ packages/st-echarts-events/st-echarts-events-1.0.0.tar.gz/st-echarts-events-1.0.0/st_echarts_events/frontend/build/precache-manifest.4e70009fb2c2faf7950c024c33519d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb37930019752bbb8abd25c8e6e8a3a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0ff9e60e764efb84ec2",
+    "url": "./static/js/2.9205fdfb.chunk.js"
+  },
+  {
+    "revision": "b90093d3f89f95f28b53885d2cd2e274",
+    "url": "./static/js/2.9205fdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907c506c9b1ab8e55160",
+    "url": "./static/js/main.e0a2b715.chunk.js"
+  },
+  {
+    "revision": "74da7fd84cbe2867938c",
+    "url": "./static/js/runtime-main.c97481d4.js"
+  }
+]);
