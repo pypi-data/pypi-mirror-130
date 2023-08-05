6338d2ca12a15d07840c046eeea9c379e8dcb79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:28 +0000

    Add package ops2deb/0.11.0/ops2deb-0.11.0-py3-none-any.whl

---
+++ packages/ops2deb/ops2deb-0.11.0-py3-none-any.whl/ops2deb/exceptions.py
@@ -0,0 +1,26 @@
+class Ops2debError(Exception):
+    pass
+
+
+class GenerateError(Ops2debError):
+    pass
+
+
+class GenerateScriptError(GenerateError):
+    pass
+
+
+class FetchError(Ops2debError):
+    pass
+
+
+class UpdaterError(Ops2debError):
+    pass
+
+
+class ParseError(Ops2debError):
+    pass
+
+
+class AptError(Ops2debError):
+    pass
