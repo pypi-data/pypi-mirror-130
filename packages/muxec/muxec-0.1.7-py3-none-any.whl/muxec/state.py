812c74f4d1c40def1ea8edcd6e9a0a4957d03e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:25 +0000

    Add package muxec/0.1.7/muxec-0.1.7-py3-none-any.whl

---
+++ packages/muxec/muxec-0.1.7-py3-none-any.whl/muxec/state.py
@@ -0,0 +1,23 @@
+panes = []
+total = 0
+all_processes_to_rolling_output = {}
+completed_processes = set()
+failed_processes = set()
+exhausted = False
+break_on_fail = False
+stdScr = None
+full_height, full_width = None, None
+is_tty = True
+
+
+def reset():
+    panes = []
+    total = 0
+    all_processes_to_rolling_output = {}
+    completed_processes = set()
+    failed_processes = set()
+    exhausted = False
+    break_on_fail = False
+    stdScr = None
+    full_height, full_width = None, None
+    is_tty = True
