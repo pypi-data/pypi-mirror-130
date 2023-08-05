e63d79d46b55906bdcc7d51e78928e5aff09f9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package triangler-patch/0.1.1/triangler-patch-0.1.1.tar.gz

---
+++ packages/triangler-patch/triangler-patch-0.1.1.tar.gz/triangler-patch-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['triangler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numba>=0.53.1,<0.54.0', 'numpy==1.21.4', 'scikit-image>=0.19.0,<0.20.0']
+
+setup_kwargs = {
+    'name': 'triangler-patch',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'djkcyl',
+    'author_email': 'cyl@cyllive.cn',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
