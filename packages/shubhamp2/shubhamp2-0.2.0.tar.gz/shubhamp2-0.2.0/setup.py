83c80e25924a0c512e03c89d6d12f5544648fede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:47 +0000

    Add package shubhamp2/0.2.0/shubhamp2-0.2.0.tar.gz

---
+++ packages/shubhamp2/shubhamp2-0.2.0.tar.gz/shubhamp2-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='shubhamp2',
+    version='0.2.0',    
+    description='An example of Python package',
+    author='Shubham Setia',
+    author_email='shubham.setia.3701@gmail.com',    
+    license='BSD 2-clause',
+    packages=['shubhamp2'],
+    setup_requires=['wheel'],
+    install_requires=['numpy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)    
---
+++ packages/shubhamp2/shubhamp2-0.2.0.tar.gz/shubhamp2-0.2.0/shubhamp2/__init__.py
@@ -0,0 +1,3 @@
+def add(a,b):
+    print('This is an addition fucnction')
+    return a+b
