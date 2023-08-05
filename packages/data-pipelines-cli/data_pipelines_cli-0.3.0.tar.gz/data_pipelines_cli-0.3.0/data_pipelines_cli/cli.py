31e21ff3baa4a98783a78fff4362aec47cabcbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:37 +0000

    Add package data-pipelines-cli/0.3.0/data_pipelines_cli-0.3.0.tar.gz

---
+++ packages/data-pipelines-cli/data_pipelines_cli-0.3.0.tar.gz/data_pipelines_cli-0.3.0/data_pipelines_cli/cli.py
@@ -0,0 +1,21 @@
+import click
+
+from .cli_commands.compile import compile_project_command
+from .cli_commands.create import create_command
+from .cli_commands.dbt import dbt_command
+from .cli_commands.deploy import deploy_command
+from .cli_commands.init import init_command
+from .cli_commands.template import template_group
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+cli.add_command(init_command)
+cli.add_command(template_group)
+cli.add_command(create_command)
+cli.add_command(dbt_command)
+cli.add_command(compile_project_command)
+cli.add_command(deploy_command)
