24d1ce84f1645044180d316494391632254ecb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:23 +0000

    Add package qz/2/qz-2.tar.gz

---
+++ packages/qz/qz-2.tar.gz/qz-2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='qz',
+    version='2',
+    description='Print "Hello, World!"',
+    url='https://github.com/donno2048/qz',
+    packages=find_packages(),
+    license='MIT',
+    author='Elisha Hollander'
+)
