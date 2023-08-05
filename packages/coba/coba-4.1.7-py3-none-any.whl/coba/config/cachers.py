2b6008d88e05649e006def058bd3d2f62889f3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:33 +0000

    Add package coba/4.1.7/coba-4.1.7-py3-none-any.whl

---
+++ packages/coba/coba-4.1.7-py3-none-any.whl/coba/config/cachers.py
@@ -0,0 +1,135 @@
+"""Various caching implementations."""
+
+import gzip
+
+from hashlib import md5
+from pathlib import Path
+from typing import Union, Dict, TypeVar, Iterable, Optional
+
+from coba.config.core import Cacher, CobaConfig
+
+_K = TypeVar("_K")
+_V = TypeVar("_V")
+
+class NullCacher(Cacher[_K, _V]):
+    def __init__(self) -> None:
+        self._cache: Dict[_K,_V] = {}
+
+    def __contains__(self, key: _K) -> bool:
+        return False
+
+    def get(self, key: _K) -> _V:
+        raise Exception("the key didn't exist in the cache")
+
+    def put(self, key: _K, value: _V) -> None:
+        pass
+
+    def rmv(self, key: _K):
+        pass
+
+class MemoryCacher(Cacher[_K, _V]):
+    def __init__(self) -> None:
+        self._cache: Dict[_K,_V] = {}
+
+    def __contains__(self, key: _K) -> bool:
+        return key in self._cache
+
+    def get(self, key: _K) -> _V:
+        return self._cache[key]
+
+    def put(self, key: _K, value: _V) -> None:
+        self._cache[key] = value
+
+    def rmv(self, key: _K) -> None:
+        del self._cache[key]
+
+class DiskCacher(Cacher[str, Iterable[bytes]]):
+    """A cache that writes bytes to disk.
+
+    The DiskCache compresses all values before storing in order to conserve space.
+    """
+
+    def __init__(self, cache_dir: Union[str, Path] = None) -> None:
+        """Instantiate a DiskCache.
+
+        Args:
+            cache_dir: The directory path where all given keys will be cached as files
+        """
+        self.cache_directory = cache_dir
+
+    @property
+    def cache_directory(self) -> Optional[str]:
+        return str(self._cache_dir) if self._cache_dir is not None else None
+
+    @cache_directory.setter
+    def cache_directory(self,value:Union[Path,str,None]) -> None:
+        self._cache_dir = value if isinstance(value, Path) else Path(value).expanduser() if value else None
+        if self._cache_dir is not None: self._cache_dir.mkdir(parents=True, exist_ok=True)
+
+    def __contains__(self, key: str) -> bool:
+        return self._cache_dir is not None and self._cache_path(key).exists()
+
+    def get(self, key: str) -> Iterable[bytes]:
+        """Get a key from the cache.
+
+        Args:
+            key: Requested key to retreive from the cache.
+        """
+
+        if key not in self: return []
+
+        try:
+            with gzip.open(self._cache_path(key), 'rb') as f:
+                for line in f:
+                    yield line.rstrip(b'\r\n')
+        except:
+            #do we want to clear the cache here if something goes wrong?
+            #it seems reasonable since this would indicate the cache is corrupted...
+            raise
+
+    def put(self, key: str, value: Iterable[bytes]):
+        """Put a key and its bytes into the cache. In the case of a key collision nothing will be put.
+
+        Args:
+            key: The key to store in the cache.
+            value: The bytes that should be cached for the given filename.
+        """
+
+        if self._cache_dir is None: return
+
+        #I'm not crazy about this... This means we can only put one thing at a time...
+        #What we really want is a lock on `key` though I haven't been able to find a good
+        #way to do this. A better method might be to create a manager.List() and then only
+        #lock long enough to add and remove keys from the manager.List() rather than locking
+        #for the entire time it takes to put something (which could be a considerable amount)
+        #of time.
+        if CobaConfig.store.get("cachelck"): CobaConfig.store.get("cachelck").acquire()
+
+        try:
+            if key in self: return
+
+            if isinstance(value,bytes): value = [value]
+
+            with gzip.open(self._cache_path(key), 'wb+') as f:
+                for line in value:
+                    f.write(line.rstrip(b'\r\n') + b'\r\n')
+        except:
+            if key in self: self.rmv(key)
+            raise
+        finally:
+            if CobaConfig.store.get("cachelck"): CobaConfig.store.get("cachelck").release()
+
+    def rmv(self, key: str) -> None:
+        """Remove a key from the cache.
+
+        Args:
+            key: The key to remove from the cache.
+        """
+
+        if self._cache_path(key).exists(): self._cache_path(key).unlink()
+
+    def _cache_name(self, key: str) -> str:
+        return md5(key.encode('utf-8')).hexdigest() + ".gz"
+
+    def _cache_path(self, key: str) -> Path:
+        return self._cache_dir/self._cache_name(key)
---
+++ packages/coba/coba-4.1.7-py3-none-any.whl/coba/multiprocessing.py
@@ -0,0 +1,91 @@
+from copy import deepcopy
+from multiprocessing import Manager
+from multiprocessing.managers import SyncManager
+from threading import Thread
+from typing import Iterable, Any
+
+from coba.utilities  import coba_exit
+from coba.config     import CobaConfig, BasicLogger, IndentLogger
+from coba.pipes      import Filter, Sink, QueueIO, MultiprocessFilter
+
+class CobaMultiprocessFilter(Filter[Iterable[Any], Iterable[Any]]):
+
+    class MarshalableFilter:
+
+        def __init__(self, filter: Filter, logger_sink: Sink, with_name:bool, manager: SyncManager) -> None:
+
+            self._logger    = deepcopy(CobaConfig.logger)
+            self._cacher    = deepcopy(CobaConfig.cacher)
+            self._srcsema   = manager.Semaphore(2)
+            self._cachelock = manager.Lock()
+
+            if isinstance(self._logger, IndentLogger):
+                self._logger._with_name = with_name
+                self._logger._sink      = logger_sink
+
+            if isinstance(self._logger, BasicLogger):
+                self._logger._with_name = with_name
+                self._logger._sink      = logger_sink
+
+            self._filter = filter
+
+        def filter(self, item: Any) -> Any:
+
+            #placing this here means this is only set inside the process 
+            CobaConfig.logger            = self._logger
+            CobaConfig.cacher            = self._cacher
+            CobaConfig.store["srcsema"]  = self._srcsema
+            CobaConfig.store["cachelck"] = self._cachelock
+
+            result = self._filter.filter(item)
+
+            try:
+                try:
+                    if isinstance(result,str):
+                        return result
+                    else:
+                        for item in result: yield item
+                except TypeError as e:
+                    if "not iterable" in str(e):
+                        return result
+                    else:
+                        raise 
+                except (EOFError,BrokenPipeError):
+                    pass
+            except Exception as e:
+                self._logger.log(e)
+
+    def __init__(self, filter: Filter, processes=1, maxtasksperchild=0) -> None:
+        self._filter           = filter
+        self._processes        = processes
+        self._maxtasksperchild = maxtasksperchild
+
+    def filter(self, items: Iterable[Any]) -> Iterable[Any]:
+
+        try:
+
+            with Manager() as manager:
+
+                stderr = QueueIO(manager.Queue())
+
+                def log_stderr():
+                    for err in stderr.read():
+                        if isinstance(err,str):
+                            CobaConfig.logger.sink.write(err)
+                        elif isinstance(err,tuple):
+                            CobaConfig.logger.log(err[2])
+                        elif isinstance(err,Exception):
+                            CobaConfig.logger.log(err)
+
+                log_thread = Thread(target=log_stderr)
+                log_thread.daemon = True
+                log_thread.start()
+
+                filter = CobaMultiprocessFilter.MarshalableFilter(self._filter, stderr, self._processes>1, manager)
+
+                for item in MultiprocessFilter(filter, self._processes, self._maxtasksperchild, stderr).filter(items):
+                    yield item
+
+        except RuntimeError as e:
+            #This happens when importing main causes this code to run again
+            coba_exit(str(e))
