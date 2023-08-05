237ef0f93349557be875fe5791838b1d9c790bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package ideatools/0.1.6/ideatools-0.1.6.tar.gz

---
+++ packages/ideatools/ideatools-0.1.6.tar.gz/ideatools-0.1.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='ideatools',
+    author="my_cherry",
+    author_email="1031622947@qq.com",
+    version='0.1.6',
+    packages=find_packages(),
+)
