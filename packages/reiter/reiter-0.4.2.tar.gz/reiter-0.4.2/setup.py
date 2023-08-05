e1e68f2eec96a22cbaab862d697e77a39d0390a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package reiter/0.4.2/reiter-0.4.2.tar.gz

---
+++ packages/reiter/reiter-0.4.2.tar.gz/reiter-0.4.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "reiter"
+version = "0.4.2"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/lapets/reiter",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Wrapper for Python iterators and iterables that "+\
+                "implements a list-like random-access interface.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
