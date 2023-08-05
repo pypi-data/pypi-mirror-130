55d26e18dfdeb4f29fec05222df631ba7d90a780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:41 +0000

    Add package lightning-grid/0.6.0rc3/lightning_grid-0.6.0rc3-py2.py3-none-any.whl

---
+++ packages/lightning-grid/lightning_grid-0.6.0rc3-py2.py3-none-any.whl/grid/sdk/utils/navigation.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+import os
+from pathlib import Path
+
+
+@contextmanager
+def set_directory(path: Path):
+    """Sets the cwd within the context
+
+    Args:
+        path (Path): The path to the cwd
+
+    Yields:
+        None
+    """
+
+    origin = Path().absolute()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
