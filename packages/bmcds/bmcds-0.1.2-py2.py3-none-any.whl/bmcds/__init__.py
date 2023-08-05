fa8f6923d8f8162493a6978f03b6c706f3e61df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package bmcds/0.1.2/bmcds-0.1.2-py2.py3-none-any.whl

---
+++ packages/bmcds/bmcds-0.1.2-py2.py3-none-any.whl/bmcds/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for bmcds."""
+
+__author__ = """Ben McCarty"""
+__email__ = 'bmccarty505@gmail.com'
+__version__ = '0.1.2'
+
+from bmcds import functions
+from bmcds import classification
+from bmcds import eda
+from bmcds import regression
+from bmcds import time_series_modeling
+from bmcds import time_series_modeling_old
