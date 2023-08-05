542918e9f8b10b8bb2668a9bf41f9e74456c130e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package FileManagerPro/1.0.0/FileManagerPro-1.0.0.tar.gz

---
+++ packages/FileManagerPro/FileManagerPro-1.0.0.tar.gz/FileManagerPro-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='FileManagerPro',
+    version='1.0.0',
+    description='A Python package with extensive file managing capabilities',
+    url='https://github.com/arpansahu/FileManagerPro',
+    author='Arpan Sahu',
+    author_email='arpanrocks95@gmail.com',
+    license='BSD 2-clause',
+    packages=['FileManagerPro'],
+    install_requires=[
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
