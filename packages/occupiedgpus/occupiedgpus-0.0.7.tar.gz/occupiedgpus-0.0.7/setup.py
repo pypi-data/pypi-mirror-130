b0900c59ba8f1a2b96c02cc287bfd63d6c68fd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:33 +0000

    Add package occupiedgpus/0.0.7/occupiedgpus-0.0.7.tar.gz

---
+++ packages/occupiedgpus/occupiedgpus-0.0.7.tar.gz/occupiedgpus-0.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="occupiedgpus",
+    version="0.0.7",
+    author="Zhichao Jin",
+    author_email="jinzcdev@icloud.com",
+    description="The program for occupation of GPUs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jinzcdev/occupied-gpus.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['torch', 'nvidia-ml-py'],
+)
+
+'''
+python3 -m pip install --user --upgrade setuptools wheel
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/* -u jinzcdev
+'''
