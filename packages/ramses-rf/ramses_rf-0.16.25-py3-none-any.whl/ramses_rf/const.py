cae814b191f2144e71d11a51f583f64385f0da4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:35 +0000

    Add package ramses-rf/0.16.25/ramses_rf-0.16.25-py3-none-any.whl

---
+++ packages/ramses-rf/ramses_rf-0.16.25-py3-none-any.whl/ramses_rf/const.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser."""
+
+from types import SimpleNamespace
+
+__dev_mode__ = False
+DEV_MODE = __dev_mode__
+
+DHW_HACK = False  # TODO: remove
+
+ATTR_ALIAS = "alias"
+ATTR_CLASS = "class"
+ATTR_FAKED = "faked"
+
+ATTR_ORPHANS = "orphans"
+
+Discover = SimpleNamespace(
+    NOTHING=0, SCHEMA=1, PARAMS=2, STATUS=4, FAULTS=8, SCHEDS=16, ALL=(1 + 2 + 4)
+)
+
+DONT_CREATE_MESSAGES = 3
+DONT_CREATE_ENTITIES = 2
+DONT_UPDATE_ENTITIES = 1
