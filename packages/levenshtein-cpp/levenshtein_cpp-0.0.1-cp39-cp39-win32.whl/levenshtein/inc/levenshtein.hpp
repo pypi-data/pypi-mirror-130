09fa6937993059686ad4f3ec3a5e021a492ded0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:59 +0000

    Add package levenshtein-cpp/0.0.1/levenshtein_cpp-0.0.1-cp39-cp39-win32.whl

---
+++ packages/levenshtein-cpp/levenshtein_cpp-0.0.1-cp39-cp39-win32.whl/levenshtein/inc/levenshtein.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+namespace lev {
+static constexpr unsigned VERSION_MAJOR = 0;
+static constexpr unsigned VERSION_MINOR = 0;
+static constexpr unsigned VERSION_PATCH = 1;
+
+int distance(std::string a, std::string b);
+} // namespace lev
