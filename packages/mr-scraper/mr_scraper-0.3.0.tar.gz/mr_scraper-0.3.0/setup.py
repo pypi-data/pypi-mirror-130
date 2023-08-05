007413465f9e3cec573af6274b0976541b990a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:09 +0000

    Add package mr-scraper/0.3.0/mr_scraper-0.3.0.tar.gz

---
+++ packages/mr-scraper/mr_scraper-0.3.0.tar.gz/mr_scraper-0.3.0/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+
+from setuptools import find_namespace_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='mr_scraper',
+    packages=find_namespace_packages(exclude=['build', 'dist', 'example', 'example.scrapers', 'tests']),
+    version='0.3.0',
+    description='Library to create scrapers in python',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='fernandojerez',
+    license='Apache',
+    requires=['webpilot', 'requests'],
+    install_requires=['webpilot', 'requests'],
+    test_suite='tests'
+)
