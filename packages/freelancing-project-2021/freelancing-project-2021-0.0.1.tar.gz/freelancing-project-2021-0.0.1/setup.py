8fab253e0b0b1c2f44dd5819346af8c69acba757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:36 +0000

    Add package freelancing-project-2021/0.0.1/freelancing-project-2021-0.0.1.tar.gz

---
+++ packages/freelancing-project-2021/freelancing-project-2021-0.0.1.tar.gz/freelancing-project-2021-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="freelancing-project-2021",
+    version="0.0.1",
+    author="Abdallah Ziad",
+    author_email="boodyziad1@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    data_files=[
+        ('templates', [
+            'src/NOWNT/templates/index.html',
+            'src/NOWNT/templates/base.html',
+            'src/NOWNT/templates/previous.html',
+            'src/NOWNT/templates/results.html']),
+        ('static/styles', ['src/NOWNT/static/styles/styles.css'])
+    ]
+)
