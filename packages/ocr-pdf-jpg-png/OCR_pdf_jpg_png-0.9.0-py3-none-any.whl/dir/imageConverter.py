fa1082c572aaa2fe25f1c7c0c395a443a8ef19bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:56 +0000

    Add package ocr-pdf-jpg-png/0.9.0/OCR_pdf_jpg_png-0.9.0-py3-none-any.whl

---
+++ packages/ocr-pdf-jpg-png/OCR_pdf_jpg_png-0.9.0-py3-none-any.whl/dir/imageConverter.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from pdf2image import convert_from_path
+from PIL import Image
+
+from dir.filetypeDetector import filetypeDetector
+
+
+class ImageConverter(object):
+    def __init__(self, file_path: str):
+        self.file_path = Path(file_path)
+
+    def convert(self) -> []:
+        images = []
+        filetype = filetypeDetector(self.file_path).detect()
+        if filetype == filetypeDetector.FILETYPE_PDF:
+            images = convert_from_path(self.file_path)
+            print("Detected filetype: pdf, " + str(len(images)) + " page(s)")
+        elif filetype in [
+                filetypeDetector.FILETYPE_JPEG, filetypeDetector.FILETYPE_PNG,
+                filetypeDetector.FILETYPE_TIFF
+        ]:
+            images.append(Image.open(self.file_path))
+            print("Detected filetype: " + filetype)
+        else:
+            print(
+                "Unsupported File Type", "This task can accept PDF, TIFF, JPEG, PNG only",
+                "Check your input file and convert it into the supported file types.")
+
+        return images
