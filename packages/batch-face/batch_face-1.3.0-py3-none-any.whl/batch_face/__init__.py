bbd31822e6bb2c271abc7bfe755a911f7cd7161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:44 +0000

    Add package batch-face/1.3.0/batch_face-1.3.0-py3-none-any.whl

---
+++ packages/batch-face/batch_face-1.3.0-py3-none-any.whl/batch_face/__init__.py
@@ -0,0 +1,5 @@
+from .utils import drawLandmark_multiple, detection_adapter, bbox_from_pts, Aligner
+from .fast_alignment import *
+from .face_detection import *
+from .face_reconstruction import *
+from .version import __version__
