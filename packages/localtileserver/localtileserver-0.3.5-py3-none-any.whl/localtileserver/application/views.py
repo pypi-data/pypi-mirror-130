02bbb64b5e12e87c6c8c7aca73b6d2a43d8b30b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package localtileserver/0.3.5/localtileserver-0.3.5-py3-none-any.whl

---
+++ packages/localtileserver/localtileserver-0.3.5-py3-none-any.whl/localtileserver/application/views.py
@@ -0,0 +1,34 @@
+import logging
+
+from flask import render_template
+from flask.views import View
+
+from localtileserver import utilities
+from localtileserver.application import app
+from localtileserver.examples import get_data_path
+
+logger = logging.getLogger(__name__)
+
+
+class GeoJSViewer(View):
+    def dispatch_request(self):
+        return render_template("geojsViewer.html")
+
+
+class CesiumViewer(View):
+    def dispatch_request(self):
+        return render_template("cesiumViewer.html")
+
+
+@app.context_processor
+def inject_context():
+    try:
+        filename = utilities.get_clean_filename(app.config["filename"])
+    except KeyError:
+        logger.error("No filename set in app config. Using sample data.")
+        filename = get_data_path("bahamas_rgb.tif")
+    tile_source = utilities.get_tile_source(filename)
+    context = utilities.get_meta_data(tile_source)
+    context["bounds"] = utilities.get_tile_bounds(tile_source, projection="EPSG:4326")
+    context["filename"] = filename
+    return context
