d1818d58c0c029cddbf335a3359ff36138f0b0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package flexlate/0.1.0/flexlate-0.1.0-py3-none-any.whl

---
+++ packages/flexlate/flexlate-0.1.0-py3-none-any.whl/flexlate/render/specific/cookiecutter.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from typing import Optional
+
+from cookiecutter.config import get_user_config
+from cookiecutter.generate import generate_context, generate_files
+from cookiecutter.main import cookiecutter
+from cookiecutter.prompt import prompt_for_config
+
+from flexlate.render.specific.base import SpecificTemplateRenderer
+from flexlate.template.cookiecutter import CookiecutterTemplate
+from flexlate.template.types import TemplateType
+from flexlate.template_data import TemplateData
+
+
+class CookiecutterRenderer(SpecificTemplateRenderer[CookiecutterTemplate]):
+    _template_cls = CookiecutterTemplate
+    _template_type = TemplateType.COOKIECUTTER
+
+    def render(
+        self,
+        template: CookiecutterTemplate,
+        data: Optional[TemplateData] = None,
+        out_path: Path = Path("."),
+        no_input: bool = False,
+    ) -> TemplateData:
+        config_dict = get_user_config()
+        context_file = template.path / "cookiecutter.json"
+
+        context = generate_context(
+            context_file=context_file,
+            default_context=config_dict['default_context'],
+            extra_context=data,
+        )
+        context['cookiecutter'] = prompt_for_config(context, no_input)
+        context['cookiecutter']['_template'] = template.path
+
+        generate_files(
+            repo_dir=str(template.path),
+            context=context,
+            overwrite_if_exists=False,
+            skip_if_file_exists=False,
+            output_dir=str(out_path),
+        )
+
+        used_data = dict(context["cookiecutter"])
+        used_data.pop("_template")
+
+        return used_data
---
+++ packages/flexlate/flexlate-0.1.0-py3-none-any.whl/tests/test_add.py
@@ -0,0 +1,58 @@
+from git import Repo
+
+from flexlate.adder import Adder
+from flexlate.config import FlexlateConfig
+from flexlate.template.types import TemplateType
+from tests.config import GENERATED_FILES_DIR
+from tests.fixtures.git import *
+from tests.fixtures.template import *
+from tests.fixtures.templated_repo import *
+
+
+def test_add_template_source_to_repo(
+    repo_with_placeholder_committed: Repo, cookiecutter_one_template: CookiecutterTemplate
+):
+    repo = repo_with_placeholder_committed
+    adder = Adder()
+    adder.add_template_source(
+        repo,
+        cookiecutter_one_template,
+        out_root=GENERATED_FILES_DIR,
+        target_version="some version"
+    )
+    config_path = GENERATED_FILES_DIR / "flexlate.json"
+    config = FlexlateConfig.load(config_path)
+    assert len(config.applied_templates) == 0
+    assert len(config.template_sources) == 1
+    source = config.template_sources[0]
+    assert source.name == cookiecutter_one_template.name
+    assert source.path == str(cookiecutter_one_template.path)
+    assert source.version == cookiecutter_one_template.version
+    assert source.type == TemplateType.COOKIECUTTER
+    assert source.target_version == "some version"
+
+
+def test_add_applied_template_to_repo(
+    repo_with_cookiecutter_one_template_source: Repo,
+    cookiecutter_one_template: CookiecutterTemplate,
+):
+    repo = repo_with_cookiecutter_one_template_source
+    adder = Adder()
+    adder.apply_template_and_add(
+        repo, cookiecutter_one_template, out_root=GENERATED_FILES_DIR, no_input=True
+    )
+
+    config_path = GENERATED_FILES_DIR / "flexlate.json"
+    config = FlexlateConfig.load(config_path)
+    assert len(config.applied_templates) == 1
+    assert len(config.template_sources) == 1
+    at = config.applied_templates[0]
+    assert at.name == cookiecutter_one_template.name
+    assert at.version == cookiecutter_one_template.version
+    assert at.data == {"a": "b", "c": ""}
+    assert at.root == GENERATED_FILES_DIR
+
+
+# TODO: tests for different add modes, different out roots
+# TODO: ensure it uses a relative path when it is a project or local config
+# TODO: test for adding to existing
