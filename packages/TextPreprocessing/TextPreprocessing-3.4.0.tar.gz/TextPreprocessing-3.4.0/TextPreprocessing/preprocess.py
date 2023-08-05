e71ee3266715c11ee9ff5d18f0033eee0f5683c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package TextPreprocessing/3.4.0/TextPreprocessing-3.4.0.tar.gz

---
+++ packages/TextPreprocessing/TextPreprocessing-3.4.0.tar.gz/TextPreprocessing-3.4.0/TextPreprocessing/preprocess.py
@@ -0,0 +1,82 @@
+import re
+from TextPreprocessing.utility import process_text
+
+class preprocess:
+    
+    def __init__(self):
+        self.__version__ = "3.4.0"
+        self.obj = process_text()
+        self.counter = 1
+        
+    
+    def load_language_model(self, modal_name):
+        self.obj.set_language_modal(modal_name)
+    
+    def set_entity_ignore_list(self, ignore_list):
+        self.obj.set_entity_exclusion_list(ignore_list)
+
+    def clean_entities(self, text):
+        return self.obj.clear_generic_entity(text)
+    
+    def clear_email_address(self, text):
+        return self.obj.clear_email_id(text)
+    
+    def clear_urls(self, text):
+        return self.obj.clear_web_url(text)
+    
+    def clear_html_tag(self, text):
+        return self.obj.clear_html_tags(text)
+    
+    def expand_abbreviations(self, text):
+        return self.obj.convert_abbreviations(text)
+    
+    def clear_special_char(self, text):
+        return self.obj.clear_special_chars(text)
+    
+    def clear_accented_char(self, text):
+        return self.obj.clear_accented_chars(text)
+    
+    def clear_all_stop_words(self, text):
+        return self.obj.clear_stop_words(text)
+    
+    def lematize_text(self, text):
+        return self.obj.lemetize(text)
+    
+    def spell_correct(self, text):
+        return self.obj.correct_spelling(text)
+    
+    def covert_lower(self, text):
+        return str(text).lower()
+    
+    def set_cleanup_sequence(self, sequence, text = None, maxRecords=0):
+        self.input_text = text
+        if type(sequence) == 'list' and len(sequence) > 0 and text!=None:
+            for option in sequence:
+                if str(option).lower() == "clean_entities":
+                    self.input_text = self.clean_entities(self.input_text)
+                elif str(option).lower() == "clear_email_address":
+                    self.input_text = self.clear_email_address(self.input_text)
+                elif str(option).lower() == "clear_urls":
+                    self.input_text = re.sub(r"((https?\S+)|(http?\S+))|(\w+:\/\/\S*)", "" , self.input_text, re.MULTILINE)
+                elif str(option).lower() == "clear_accented_char":
+                    self.input_text = self.clear_accented_char(self.input_text)
+                elif str(option).lower() == "clear_all_stop_words":
+                    self.input_text = self.clear_all_stop_words(self.input_text)
+                elif str(option).lower() == "lematize_text":
+                    self.input_text = self.lematize_text(self.input_text)
+                elif str(option).lower() == "spell_correct":
+                    self.input_text = self.spell_correct(self.input_text)
+                elif str(option).lower() == "covert_lower":
+                    self.input_text = self.covert_lower(self.input_text)
+                else:
+                    pass
+            print(f"{self.counter}/{maxRecords}", end="\r\r")
+            self.counter+=1
+            return self.input_text
+        else:
+            return self.input_text
+    
+    def reset_counter():
+        self.counter = 1
+
+
