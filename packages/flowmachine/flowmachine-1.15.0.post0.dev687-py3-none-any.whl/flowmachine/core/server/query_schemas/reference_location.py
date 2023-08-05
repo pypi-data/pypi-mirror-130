e55d91cff011717478cda875e7abda99fbde0f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package flowmachine/1.15.0.post0.dev687/flowmachine-1.15.0.post0.dev687-py3-none-any.whl

---
+++ packages/flowmachine/flowmachine-1.15.0.post0.dev687-py3-none-any.whl/flowmachine/core/server/query_schemas/reference_location.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marshmallow_oneofschema import OneOfSchema
+
+from flowmachine.core.server.query_schemas.daily_location import DailyLocationSchema
+from flowmachine.core.server.query_schemas.majority_location import (
+    MajorityLocationSchema,
+)
+from flowmachine.core.server.query_schemas.modal_location import ModalLocationSchema
+from flowmachine.core.server.query_schemas.most_frequent_location import (
+    MostFrequentLocationSchema,
+)
+from flowmachine.core.server.query_schemas.visited_most_days import (
+    VisitedMostDaysSchema,
+)
+
+
+class ReferenceLocationSchema(OneOfSchema):
+    type_field = "query_kind"
+    type_schemas = {
+        "daily_location": DailyLocationSchema,
+        "modal_location": ModalLocationSchema,
+        "most_frequent_location": MostFrequentLocationSchema,
+        "visited_most_days": VisitedMostDaysSchema,
+        "majority_location": MajorityLocationSchema,
+    }
