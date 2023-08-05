f0c5d3567793becf570fdd2933f5cc3475095555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:29 +0000

    Add package saiph/0.2.0/saiph-0.2.0-py3-none-any.whl

---
+++ packages/saiph/saiph-0.2.0-py3-none-any.whl/saiph/__init__.py
@@ -0,0 +1,12 @@
+from .projection import fit, inverse_transform, stats, transform
+from .visualization import plot_circle, plot_explained_var, plot_var_contribution
+
+__all__ = [
+    "fit",
+    "inverse_transform",
+    "transform",
+    "stats",
+    "plot_circle",
+    "plot_explained_var",
+    "plot_var_contribution",
+]
