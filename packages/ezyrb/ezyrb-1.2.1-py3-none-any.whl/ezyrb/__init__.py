b9c35ab3d8f796c8f21df86cd83edf181cedbb17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:49 +0000

    Add package ezyrb/1.2.1/ezyrb-1.2.1-py3-none-any.whl

---
+++ packages/ezyrb/ezyrb-1.2.1-py3-none-any.whl/ezyrb/__init__.py
@@ -0,0 +1,20 @@
+"""EZyRB package"""
+
+from .meta import *
+from .database import Database
+from .reduction import Reduction
+from .pod import POD
+from .ae import AE
+from .approximation import Approximation
+from .rbf import RBF
+from .linear import Linear
+from .gpr import GPR
+from .reducedordermodel import ReducedOrderModel
+from .ann import ANN
+from .kneighbors_regressor import KNeighborsRegressor
+from .radius_neighbors_regressor import RadiusNeighborsRegressor
+
+__all__ = [
+    'database', 'reduction', 'pod', 'approximation', 'rbf', 'linear', 'gpr',
+    'ann', 'kneighbors_regressor', 'radius_neighbors_regressor', 'ae'
+]
