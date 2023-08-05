72a3c7ce6297fb0687868e58c7168b1e7d69468f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:35 +0000

    Add package spark-etl/0.0.107/spark_etl-0.0.107-py3-none-any.whl

---
+++ packages/spark-etl/spark_etl-0.0.107-py3-none-any.whl/spark_etl/deployers/hdfs_deployer.py
@@ -0,0 +1,61 @@
+import uuid
+import subprocess
+import os
+from urllib.parse import urlparse
+
+from .abstract_deployer import AbstractDeployer
+from spark_etl import Build
+from spark_etl.exceptions import SparkETLDeploymentFailure
+from spark_etl.ssh_config import SSHConfig
+
+
+class HDFSDeployer(AbstractDeployer):
+    """
+    This deployer deploys application to HDFS
+    """
+    def __init__(self, config):
+        super(HDFSDeployer, self).__init__(config)
+        self.ssh_config = SSHConfig(config['ssh_config'])
+
+    def deploy(self, build_dir, deployment_location):
+        self.ssh_config.generate()
+        try:
+            o = urlparse(deployment_location)
+            if o.scheme not in ('hdfs', 's3', 's3a'):
+                raise SparkETLDeploymentFailure("deployment_location must be in hdfs, s3 or s3a")
+
+            # let's copy files to the stage dir
+            bridge_dir = os.path.join(self.config['stage_dir'], str(uuid.uuid4()))
+
+            bridge = self.config["bridge"]
+            self.ssh_config.execute(bridge, ["mkdir", "-p", bridge_dir])
+
+            build = Build(build_dir)
+
+            for artifact in build.artifacts:
+                self.ssh_config.scp(
+                    f"{build_dir}/{artifact}", f"{bridge}:{bridge_dir}/{artifact}"
+                )
+
+            # copy job loader
+            job_loader_filename = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                'job_loader.py'
+            )
+            self.ssh_config.scp(job_loader_filename, f"{bridge}:{bridge_dir}/job_loader.py")
+
+            dest_location = f"{deployment_location}/{build.version}"
+            self.ssh_config.execute(bridge, ["hdfs", "dfs", "-rm", "-r", dest_location], error_ok=True)
+            self.ssh_config.execute(bridge, ["hdfs", "dfs", "-mkdir", "-p", dest_location])
+
+            artifacts = []
+            artifacts.extend(build.artifacts)
+            artifacts.append("job_loader.py")
+            for artifact in artifacts:
+                self.ssh_config.execute(bridge, [
+                    "hdfs", "dfs", "-copyFromLocal", f"{bridge_dir}/{artifact}", f"{dest_location}/{artifact}"
+                ])
+
+            self.ssh_config.execute(bridge, ["rm", "-rf", bridge_dir])
+        finally:
+            self.ssh_config.destroy()
---
+++ packages/spark-etl/spark_etl-0.0.107-py3-none-any.whl/spark_etl/vendors/local/pyspark_job_submitter.py
@@ -0,0 +1,133 @@
+import json
+import os
+import uuid
+import tempfile
+import subprocess
+import json
+import time
+
+from spark_etl.job_submitters import AbstractJobSubmitter
+from spark_etl.core import ClientChannelInterface
+from spark_etl.utils import CLIHandler
+
+class ClientChannel(ClientChannelInterface):
+    def __init__(self, run_dir, run_id):
+        self.run_dir = run_dir
+        self.run_id = run_id
+
+    def _get_json_path(self, name):
+        return os.path.join(self.run_dir, self.run_id, name)
+
+    def read_json(self, name):
+        with open(self._get_json_path(name), "r") as f:
+            return json.load(f)
+
+    def has_json(self, name):
+        return os.path.isfile(self._get_json_path(name))
+
+    def write_json(self, name, payload):
+        with open(self._get_json_path(name), "w") as f:
+            json.dump(payload, f)
+
+    def delete_json(self, name):
+        os.remove(self._get_json_path(name))
+
+
+class PySparkJobSubmitter(AbstractJobSubmitter):
+    def __init__(self, config):
+        super(PySparkJobSubmitter, self).__init__(config)
+        ####################################################################################
+        # here is a sample config
+        # {
+        #     "run_dir": "/home/stonezhong/spark-etl-lab/src/local-lake/runs",
+        #     "enable_aws_s3": true,
+        #     "aws_account": "~/.aws/account.json",
+        #     "aws_s3_buffer_dir": "/home/stonezhong/spark-etl-lab/src/local-lake/s3-buffer"
+        # }
+        # enable_aws_s3: set to True if you want to access AWS S3 buckets
+        # aws_account: point to a json filename, the json file looks like below
+        # {
+        #     "aws_access_key_id": "***",
+        #     "aws_secret_access_key": "***"
+        # }
+        # aws_s3_buffer_dir: a temp dir for s3 buffered data
+        ####################################################################################
+
+
+
+    def run(self, deployment_location, options={}, args={}, handlers=[], on_job_submitted=None, cli_mode=False):
+        # version is already baked into deployment_location
+        # local submitter ignores handlers
+        run_id  = str(uuid.uuid4())
+        run_dir = self.config['run_dir']
+        app_dir = deployment_location
+
+        os.makedirs(os.path.join(run_dir, run_id))
+
+        # generate input.json
+        with open(os.path.join(run_dir, run_id, 'input.json'), 'wt') as f:
+            json.dump(args, f)
+
+        client_channel = ClientChannel(run_dir, run_id)
+
+        run_args = [
+            "spark-submit",
+        ]
+        enable_aws_s3 = self.config.get('enable_aws_s3', False)
+        if enable_aws_s3:
+            # spark will automatically download package
+            run_args.extend([
+                "--packages",
+                "org.apache.hadoop:hadoop-aws:2.7.3"
+            ])
+
+        run_args.extend([
+            os.path.join(deployment_location, "job_loader.py"),
+            "--run-id", run_id,
+            "--run-dir", run_dir,
+            "--app-dir", app_dir
+        ])
+
+        if enable_aws_s3:
+            run_args.append("--enable-aws-s3")
+            aws_s3_buffer_dir = self.config.get('aws_s3_buffer_dir')
+            if aws_s3_buffer_dir is not None:
+                aws_s3_buffer_dir = os.path.expandvars(os.path.expanduser(aws_s3_buffer_dir))
+                run_args.extend(['--aws-s3-buffer-dir', aws_s3_buffer_dir])
+
+
+        aws_account = self.config.get('aws_account')
+        if aws_account is not None:
+            aws_account = os.path.expandvars(os.path.expanduser(aws_account))
+            run_args.extend(['--aws-account', aws_account])
+
+        myenv = os.environ.copy()
+        myenv['SPARK_LOCAL_HOSTNAME'] = 'localhost'
+        p = subprocess.Popen(run_args, env=myenv)
+        exit_code = None
+        cli_entered = False
+
+        if on_job_submitted is not None:
+            on_job_submitted(run_id, vendor_info={})
+
+        while True:
+            time.sleep(1)
+            exit_code = p.poll()
+            if exit_code is not None:
+                break
+
+            if cli_mode and not cli_entered:
+                cli_entered = True
+                cli_handler = CLIHandler(
+                    client_channel,
+                    lambda : p.poll() is None,
+                    handlers
+                )
+                cli_handler.loop()
+
+        if exit_code != 0:
+            Exception(f"Job failed, exit_code = {exit_code}")
+
+        print("Job completed successfully")
+        with open(os.path.join(run_dir, run_id, "result.json"), "r") as f:
+            return json.load(f)
