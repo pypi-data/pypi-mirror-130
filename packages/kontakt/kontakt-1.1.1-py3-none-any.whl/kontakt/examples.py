c5961b3b0d4e075576c2175528771698d8afab9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package kontakt/1.1.1/kontakt-1.1.1-py3-none-any.whl

---
+++ packages/kontakt/kontakt-1.1.1-py3-none-any.whl/kontakt/examples.py
@@ -0,0 +1,31 @@
+"""Examples of how to use the kontakt extension system.
+
+These serve both as a concrete example of how to use kontakt
+as well as for use in the tests.
+"""
+
+import kontakt
+
+
+class ExampleExtension(kontakt.Extension):
+    def __init__(self, flavor, size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.flavor = flavor
+        self.size = size
+
+    KIND = "example-extension"
+
+    def _kind(self):
+        return ExampleExtension.KIND
+
+
+class ExampleExtensionError(kontakt.ExtensionError):
+    pass
+
+
+class Blue(ExampleExtension):
+    pass
+
+
+class Red(ExampleExtension):
+    pass
