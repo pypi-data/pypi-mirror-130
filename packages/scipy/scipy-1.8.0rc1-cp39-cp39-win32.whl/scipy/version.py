7538ce69816077670722c367e11a7040cb6ffb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:04 +0000

    Add package scipy/1.8.0rc1/scipy-1.8.0rc1-cp39-cp39-win32.whl

---
+++ packages/scipy/scipy-1.8.0rc1-cp39-cp39-win32.whl/scipy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.8.0rc1'
+version = '1.8.0rc1'
+full_version = '1.8.0rc1'
+git_revision = '4dc5e3b'
+commit_count = '2226'
+release = True
+
+if not release:
+    version = full_version
