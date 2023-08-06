8c907aff83e2623238792929e27a5076225f6bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:55 +0000

    Add package alpyvision/0.0.2/alpyvision-0.0.2.tar.gz

---
+++ packages/alpyvision/alpyvision-0.0.2.tar.gz/alpyvision-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# Set the contents of the readme file as long_description
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+VERSION = '0.0.2'
+
+# Let setuptools work
+setuptools.setup(
+    name='alpyvision',
+    version=VERSION,
+    author='Joel Läubin',
+    author_email='laeubin.j@protonmail.com',
+    description='AlPyVision is a Python wrapper to work with openCV and the Albion Online game client.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['albion', 'opencv', 'computer vision'],
+    url='https://github.com/MadMowgli/AlPyVision',
+    packages=setuptools.find_packages(),
+    install_requires=['opencv-python', 'numpy'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows'
+    ]
+)
+
