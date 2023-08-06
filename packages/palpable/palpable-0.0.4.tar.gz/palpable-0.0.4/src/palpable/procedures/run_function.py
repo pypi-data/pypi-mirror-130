161ee46d204f78bfee8692ae28ab2ac54317e8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:39 +0000

    Add package palpable/0.0.4/palpable-0.0.4.tar.gz

---
+++ packages/palpable/palpable-0.0.4.tar.gz/palpable-0.0.4/src/palpable/procedures/run_function.py
@@ -0,0 +1,13 @@
+from ..procedures.procedure import Procedure
+from ..units.messenger import Messenger
+
+
+class RunFunction(Procedure):
+    def __init__(self, function, param):
+        self.function = function
+        self.param = param
+
+    def run(self, messenger: Messenger):
+        messenger.info(f"Run function `{self.function.__name__}` with params: `{self.param}`")
+        self.function.__globals__["print"] = messenger.print  # inject messenger.print as print
+        return self.function(self.param)
