719693c412743f74137c8a9fd1527afd74ed2802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:43 +0000

    Add package gnssrefl/1.0.13/gnssrefl-1.0.13-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/gnssrefl/gnssrefl-1.0.13-cp39-cp39-macosx_10_15_x86_64.whl/gnssrefl/rinex2snr_cl.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+command line tool for the rinex2snr module
+pretty much what it sounds like - it translates rinex files and makes SNR files
+kristine larson
+2021aug01 added mk option for uppercase file names
+
+compile the fortran first
+f2py -c -m gnssrefl.gpssnr gnssrefl/gpssnr.f
+"""
+
+import argparse
+import datetime
+import numpy as np
+import os
+import sys
+import subprocess
+import time
+
+
+import gnssrefl.gps as g
+import gnssrefl.rinex2snr as rnx
+ 
+def main():
+#
+    # 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("station", help="station name", type=str)
+    parser.add_argument("year", help="year", type=int)
+    parser.add_argument("doy", help="start day of year", type=int)
+# optional arguments
+    parser.add_argument("-snr", default=66, help="snr file ending", type=str)
+    parser.add_argument("-orb", default='nav', type=str, help="orbit type, gps, gps+glo, gnss, rapid or you can specify nav,igs,igr,jax,gbm,grg,wum,gfr,ultra")
+    parser.add_argument("-rate", default='low', metavar='low',type=str, help="sample rate: low or high")
+    parser.add_argument("-dec", default=0, type=int, help="decimate (seconds)")
+    parser.add_argument("-nolook", default='False', metavar='False', type=str, help="True means only use RINEX files on local machine")
+    parser.add_argument("-fortran", default='False', metavar='False',type=str, help="True means use Fortran RINEX translators ")
+    parser.add_argument("-archive", default=None, metavar='all',help="specify one archive: unavco,sopac,cddis,sonel,nz,ga,ngs,bkg,nrcan,jp", type=str)
+    parser.add_argument("-doy_end", default=None, help="end day of year", type=int)
+    parser.add_argument("-year_end", default=None, help="end year", type=int)
+    parser.add_argument("-overwrite", default=None, help="boolean", type=str)
+    parser.add_argument("-translator", default=None, help="translator(fortran,hybrid,python)", type=str)
+    parser.add_argument("-srate", default=None, help="sample rate (RINEX 3 only)", type=int)
+    parser.add_argument("-mk", default=None, help="use True for uppercase station names ", type=str)
+    parser.add_argument("-weekly", default=None, help="use True for weekly data translation", type=str)
+
+    args = parser.parse_args()
+#   make sure environment variables exist.  set to current directory if not
+    g.check_environ_variables()
+#
+# rename the user inputs as variables
+#
+    station = args.station; NS = len(station)
+    if (NS == 4) or (NS == 6) or (NS == 9):
+        #print('You have submitted a nominally valid station name')
+        okok = 1
+    else:
+        print('Illegal input - Station name must have 4 (RINEX 2), 6 (GSI), or 9 (RINEX 3) characters. Exiting.')
+        sys.exit()
+    year = args.year
+    year_st = year
+
+
+    if len(str(year)) != 4:
+        print('Year must be four characters long. Exiting.', year)
+        sys.exit()
+
+
+    doy= args.doy
+    doy_st = doy
+    isnr = args.snr # defined as an integer
+    #snrt = args.snrEnd # 
+    #isnr = int(snrt)
+    orb = args.orb
+# currently allowed orbit types - shanghai removed 2020sep08
+# added GFZ rapid, aka gfr 2021May19 but it does not work anymore.  point it to gbm
+# 
+# added ESA, thank you to Makan
+    orbit_list = ['gps','gps+glo','gnss','nav', 'igs','igr','jax','gbm','grg','wum','gfr','esa','ultra','rapid']
+    if orb not in orbit_list:
+        print('You picked an orbit type I do not recognize. Here are the ones I allow')
+        print(orbit_list)
+        print('Exiting')
+        sys.exit()
+    # if you choose GPS, you get the nav message
+    if orb == 'gps':
+        orb = 'nav'
+    # if you choose ultra , you get the GFZ rapid 
+    if orb == 'rapid':
+        orb = 'gfr'
+
+    # if you choose GNSS, you get the GFZ sp3 file  (precise)
+    if orb == 'gnss':
+        orb = 'gbm'
+
+    # if you choose GPS+GLO, you get the JAXA sp3 file 
+    if orb == 'gps+glo':
+        orb = 'jax'
+
+    if args.fortran == 'True':
+        fortran = True
+    else:
+        fortran = False
+
+    # check that the fortran exe exist
+    if fortran:
+        if (orb == 'nav'):
+            snrexe = g.gpsSNR_version()
+            if not os.path.isfile(snrexe):
+                print('You have selected the fortran and GPS only options.')
+                print('However, the fortran translator gpsSNR.e has not been properly installed.')
+                print('We are changing to the hybrid translator option.')
+                fortran = False
+                translator = 'hybrid'
+        else:
+            snrexe = g.gnssSNR_version()
+            if not os.path.isfile(snrexe):
+                print('You have selected the fortran and GNSS options.')
+                print('However, the fortran translator gnssSNR.e has not been properly installed.')
+                print('We are changing to the python translator option (the hybrid is not yet working).')
+                fortran = False
+                translator = 'python'
+
+
+# if true ony use local RINEX files, which speeds up analysis of local datasets
+    nolook = args.nolook
+    if nolook == 'True':
+        nol = True
+    else:
+        nol = False
+
+    # default is set to low.  pick high for 1sec files 
+    rate = args.rate
+
+    if args.doy_end == None:
+        doy2 = doy
+    else:
+        doy2 = args.doy_end
+
+
+# currently allowed archives for RINEX observation data
+    archive_list = ['sopac', 'unavco','sonel','cddis','nz','ga','bkg','jeff','ngs','nrcan','special','bev','jp']
+    if args.archive == None:
+        archive = 'all'
+    else:
+        archive = args.archive.lower()
+        if archive not in archive_list:
+            print('You picked an archive that is not allowed')
+            print('For future reference: I allow these archives:') 
+            print(archive_list)
+            print('Exiting')
+            sys.exit()
+
+    year1=year
+    if args.year_end == None:
+        year2 = year 
+    else:
+        year2 = args.year_end
+
+# decimation rate
+    dec_rate = args.dec
+
+# the weekly option
+    skipit = 1
+    if args.weekly == 'True':
+        print('you have invoked the weekly option')
+        skipit = 7
+
+    # change skipit to be sent to rinex2snr.py
+    #doy_list = list(range(doy, doy2+1))
+    #doy_list = list(range(doy, doy2+1,skipit))
+    # this makes the correct lists in the function
+    doy_list = [doy_st, doy2]
+    year_list = list(range(year1, year2+1))
+
+    overwrite = False
+    if (args.overwrite == 'True'):
+        overwrite = True
+
+    # default is to use hybrid for RINEX translator
+    if args.translator == None:
+        translator = 'hybrid'
+    else:
+        translator = args.translator
+        if translator == 'hybrid':
+            fortran = False # override
+        if translator == 'python':
+            fortran = False # override - but this is sllllllooooowwww
+
+    # this is for RINEX 3 only - default will be 30
+    if args.srate == None:
+        srate = 30
+    else:
+        srate = args.srate
+
+    # the Makan option
+    mk = False
+    if args.mk == 'True':
+        print('you have invoked the Makan option')
+        mk = True
+    rnx.run_rinex2snr(station, year_list, doy_list, isnr, orb, rate,dec_rate,archive,fortran,nol,overwrite,translator,srate,mk,skipit)
+    print('Feedback written to subdirectory logs')
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#    if translator == 'hybrid':
+#        snrexe = g.gnssSNR_version()
+#        if (orb == 'jax') or (orb == 'gbm') or (orb=='igr'):
+#            print('The hybrid option does not currently work on multi-GNSS files')
+#            print('I am currently testing this option')
+#           if not os.path.isfile(snrexe):
+#                print('setting to python in the interim')
+                #translator = 'python'
+    #        else:
+    #            print('setting to fortran in the interim')
+    #            translator = 'fortran'
+
---
+++ packages/gnssrefl/gnssrefl-1.0.13-cp39-cp39-macosx_10_15_x86_64.whl/gnssrefl/subdaily_cl.py
@@ -0,0 +1,161 @@
+# preliminary water analysis to apply RHdot correction
+# originally called prelim-tides - now subdaily_cl.py
+# kristine larson, modified may 2021
+import argparse
+import datetime
+import json
+import matplotlib.pyplot as Plt
+import numpy as np
+import os
+import sys
+
+from datetime import date
+
+# my code
+import gnssrefl.gps as g
+import gnssrefl.subdaily as t
+
+
+import scipy.interpolate as interpolate
+from scipy.interpolate import interp1d
+import math
+
+
+def main():
+#   make surer environment variables are set 
+    g.check_environ_variables()
+    xdir = os.environ['REFL_CODE'] 
+
+# must input start and end year
+    parser = argparse.ArgumentParser()
+    parser.add_argument("station", help="station name", type=str)
+    parser.add_argument("year", default=None, type=str, help="for now one year at a time")
+    parser.add_argument("-txtfile", default=None, type=str, help="Filename for editing") 
+    parser.add_argument("-splinefile", default=None, type=str, help="Input filename for rhdot/spline fitting (optional)") 
+    parser.add_argument("-csvfile", default=None, type=str, help="set to True if you prefer csv to plain txt") 
+    parser.add_argument("-plt", default=None, type=str, help="set to False to suppress plots")
+    parser.add_argument("-spline_outlier", default=None, type=str, help="outlier criterion used in splinefit (meters)")
+    parser.add_argument("-knots", default=None, type=str, help="Knots per day, spline fit only (default is 8)")
+    parser.add_argument("-sigma", default=None, type=str, help="simple sigma outlier criterion (e.g. 1 for 1sigma, 3 for 3sigma)")
+    parser.add_argument("-extension", default=None, type=str, help="soln subdirectory")
+    parser.add_argument("-rhdot", default=None, type=str, help="set to True to turn on spline fitting for RHdot correction")
+    parser.add_argument("-doy1", default=None, type=str, help="initial day of year")
+    parser.add_argument("-doy2", default=None, type=str, help="end day of year")
+    parser.add_argument("-testing", default=None, type=str, help="set to True for testing mode")
+    parser.add_argument("-ampl", default=None, type=str, help="new amplitude constraint")
+    parser.add_argument("-azim1", default=None, type=str, help="new min azimuth")
+    parser.add_argument("-azim2", default=None, type=str, help="new max azimuth")
+    parser.add_argument("-peak2noise", default=None, type=str, help="new peak2noise constraint")
+
+    args = parser.parse_args()
+#   these are required
+    station = args.station
+    year = int(args.year)
+
+    # default is plots comes to the screen
+    plt = True
+    if args.plt == 'False':
+        plt= False
+
+    txtdir = xdir + '/Files'
+    if not os.path.exists(txtdir):
+        subprocess.call(['mkdir',txtdir])
+ 
+    if args.extension == None:
+        ext = ''
+    else:
+        ext = args.extension
+
+    # if not specified, use 2.5 sigma
+    if args.sigma == None:
+        sigma = 2.5 
+    else:
+        sigma = float(args.sigma)
+
+    # if not specified, use outlier criterion of 0.5 m
+    # this is JUST for the splinefit. changed variable in thte input to make that more clear
+    if args.spline_outlier == None:
+        outlier = 1.0 
+    else:
+        outlier = float(args.spline_outlier)
+
+# these constraints are added at the command line
+    azim1 = 0
+    if not (args.azim1 == None):
+        azim1 = int(args.azim1)
+
+    azim2 = 360 
+    if not (args.azim2 == None):
+        azim2 = int(args.azim2)
+
+    ampl = 0 
+    if not (args.ampl == None):
+        ampl = float(args.ampl)
+
+    peak2noise = 0
+    if not (args.peak2noise == None):
+        peak2noise = float(args.peak2noise)
+
+    usespline = False # though it is dangerous
+    if args.rhdot == 'True':
+        usespline = True
+
+#   these are optional output options
+    #create the subdaily file
+    if args.doy1 == None:
+        doy1 = 1
+    else:
+        doy1 = int(args.doy1)
+    if args.doy2 == None:
+        doy2 = 366
+    else:
+        doy2 = int(args.doy2)
+        # writejson not allowed as of yet.
+    writecsv = False
+    if (args.csvfile == 'True'):
+        writecsv = True
+    if (args.splinefile == None):
+        txtfile = ''
+        if (args.txtfile == None):
+            print('Will pick up and concatenate daily result files')
+        else:
+            txtfile = args.txtfile 
+            print('Using ',txtfile)
+        # if txtfile provided, you can use that as your starting dataset 
+        ntv,obstimes,fname,fname_new = t.readin_and_plot(station, year,doy1,doy2,plt,ext,sigma,writecsv,azim1,azim2,ampl,peak2noise,txtfile)
+        haveObstimes = True
+        N,M = np.shape(ntv)
+    else:
+        haveObstimes = False
+        fname_new = args.splinefile
+        if not os.path.isfile(fname_new):
+            print('Input subdaily RH file you provided does not exist:', fname_new)
+            sys.exit()
+
+    # testing added so that if it crashes, only effects me.  and I get more useful error messages
+    # added spline input 2021 oct 27. It was not coded well enough for gaps etc.
+    # only allow plaint text?  i think that is what is really going on here
+    input2spline = fname_new; output4spline = fname_new + '.withrhdot'
+    if (args.knots== None):
+        knots = 8
+    else:
+        knots = int(args.knots)
+    if usespline:
+        if (args.testing == None): 
+            try:
+                if haveObstimes:
+                    tv,corr = t.rhdot_correction(station,input2spline, output4spline, plt,outlier,obstimes=obstimes,knots=knots)
+                else:
+                    tv,corr = t.rhdot_correction(station,input2spline, output4spline, plt,outlier,knots=knots)
+            except: 
+                print('Exited the spline code for unknown reasons. Run with testing as True if you want more info')
+                okok = 1
+        else:
+            if haveObstimes:
+                tv,corr = t.rhdot_correction(station,input2spline, output4spline, plt,outlier,obstimes=obstimes,knots=knots)
+            else:
+                tv,corr = t.rhdot_correction(station,input2spline, output4spline, plt,outlier,knots=knots)
+
+
+if __name__ == "__main__":
+    main()
