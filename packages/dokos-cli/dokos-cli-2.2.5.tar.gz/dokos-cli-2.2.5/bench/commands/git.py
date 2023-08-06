e338e5a89f83f658e9b59f0b5c01862b29695764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:58 +0000

    Add package dokos-cli/2.2.5/dokos-cli-2.2.5.tar.gz

---
+++ packages/dokos-cli/dokos-cli-2.2.5.tar.gz/dokos-cli-2.2.5/bench/commands/git.py
@@ -0,0 +1,35 @@
+# imports - standard imports
+import os
+import subprocess
+
+# imports - module imports
+from bench.app import get_repo_dir, get_apps, get_remote
+from bench.utils import set_git_remote_url
+
+# imports - third party imports
+import click
+
+
+@click.command('remote-set-url', help="Set app remote url")
+@click.argument('git-url')
+def remote_set_url(git_url):
+	set_git_remote_url(git_url)
+
+
+@click.command('remote-reset-url', help="Reset app remote url to dokos official")
+@click.argument('app')
+def remote_reset_url(app):
+	git_url = f"https://gitlab.com/dokos/{app}.git"
+	set_git_remote_url(git_url)
+
+
+@click.command('remote-urls', help="Show apps remote url")
+def remote_urls():
+	for app in get_apps():
+		repo_dir = get_repo_dir(app)
+
+		if os.path.exists(os.path.join(repo_dir, '.git')):
+			remote = get_remote(app)
+			remote_url = subprocess.check_output(['git', 'config', '--get', f'remote.{remote}.url'], cwd=repo_dir).strip()
+			print(f"{app}\t{remote_url}")
+
