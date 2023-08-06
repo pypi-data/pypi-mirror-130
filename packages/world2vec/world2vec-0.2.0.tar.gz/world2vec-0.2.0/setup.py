6bffec5c32e1a04e1f5f60d2119b21b2b030e4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:03 +0000

    Add package world2vec/0.2.0/world2vec-0.2.0.tar.gz

---
+++ packages/world2vec/world2vec-0.2.0.tar.gz/world2vec-0.2.0/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="world2vec",
+    version="0.2.0",
+    description="The python API client for world2vec",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="world2vec",
+    author_email="nathan@world2vec.com",
+    license="MIT",
+    packages=["world2vec"],
+    include_package_data=True,
+    install_requires=["pandas"],
+      classifiers=[
+            'Development Status :: 3 - Alpha',      
+            'Intended Audience :: Developers',      
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',   
+            'Programming Language :: Python :: 3',      
+            'Programming Language :: Python :: 3.7',
+  ],
+    entry_points={
+        "console_scripts": [
+            "world2vec=world2vec.__main__:main",
+        ]
+    },
+)
---
+++ packages/world2vec/world2vec-0.2.0.tar.gz/world2vec-0.2.0/world2vec/__init__.py
@@ -0,0 +1,4 @@
+from world2vec.main import World2Vec
+
+
+__version__ = "0.2.0"
