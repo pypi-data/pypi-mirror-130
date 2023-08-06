459353defb1cf244af1c3451c9306fea41d55edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:39 +0000

    Add package spaceant/0.0.1/spaceant-0.0.1.tar.gz

---
+++ packages/spaceant/spaceant-0.0.1.tar.gz/spaceant-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="spaceant",
+  version="0.0.1",
+  description="Spatial Cell Analysis Toolkit.",
+  license="BSD 3-Clause License",
+  packages=find_packages(),
+  author = "Matthias Bruhns",
+  author_email = "matthias.bruhns@posteo.de",
+  url = "https://github.com/mbruhns/spaceant",
+  keywords = ["Spatial single-cell data"],
+  install_requires=[
+          "numpy",
+          "scipy",
+          "joblib",
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+  ])
+
