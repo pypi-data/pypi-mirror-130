0ea5b067ada135f351f0b26608b9b3c3da727077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:25 +0000

    Add package cdk-notify-bucket/0.0.130/cdk-notify-bucket-0.0.130.tar.gz

---
+++ packages/cdk-notify-bucket/cdk-notify-bucket-0.0.130.tar.gz/cdk-notify-bucket-0.0.130/src/cdk_notify_bucket/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_notifications._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-notify-bucket",
+    "0.0.130",
+    __name__[0:-6],
+    "cdk-notify-bucket@0.0.130.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
