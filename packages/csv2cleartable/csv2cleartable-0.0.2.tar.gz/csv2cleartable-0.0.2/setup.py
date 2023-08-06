3603f8e7462db4a7ba9cde2fb5574655d430abc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:24 +0000

    Add package csv2cleartable/0.0.2/csv2cleartable-0.0.2.tar.gz

---
+++ packages/csv2cleartable/csv2cleartable-0.0.2.tar.gz/csv2cleartable-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='csv2cleartable',
+  version='0.0.2',
+  description='Convert into csv into a readable table ',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='bharathraj',
+  author_email='x20225024@student.ncirl.ie',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='csv2table', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
