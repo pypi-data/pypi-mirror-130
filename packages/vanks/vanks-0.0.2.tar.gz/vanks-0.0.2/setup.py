04133ec7e1eaaf7b4bb4de0bb3df6a2ec35d8d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package vanks/0.0.2/vanks-0.0.2.tar.gz

---
+++ packages/vanks/vanks-0.0.2.tar.gz/vanks-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'vanks', # while installing pacakge, e.g., matplotlib...
+    version = '0.0.2',
+    description = 'Simple Bank Account.',
+    long_description = open('Readme.txt').read(),
+    url = 'https://pypi.org/user/imvickykumar999/',
+    author = 'Vicky Kumar',
+    keywords = ['account', 'bank', 'class', 'ideationology'],
+    packages = ['vicks'], # while importing package, e.g., pyplot...
+)
+
+# python setup.py sdist
+# pip install twine
+# twine upload dist/*
