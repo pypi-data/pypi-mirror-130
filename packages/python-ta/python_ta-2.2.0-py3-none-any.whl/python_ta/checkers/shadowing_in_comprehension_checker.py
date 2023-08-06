84876b9e21a5e9dc97dd1e2a1f81ff4f790e48c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:20 +0000

    Add package python-ta/2.2.0/python_ta-2.2.0-py3-none-any.whl

---
+++ packages/python-ta/python_ta-2.2.0-py3-none-any.whl/python_ta/checkers/shadowing_in_comprehension_checker.py
@@ -0,0 +1,39 @@
+"""checker for variable shadowing in a comprehension.
+"""
+from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import check_messages
+from pylint.interfaces import IAstroidChecker
+
+
+class ShadowingInComprehensionChecker(BaseChecker):
+
+    __implements__ = IAstroidChecker
+
+    name = "shadowing_in_comprehension"
+    msgs = {
+        "E9988": (
+            "Comprehension variable '%s' shadows a variable in an outer scope",
+            "shadowing-in-comprehension",
+            "Used when there is shadowing inside a comprehension",
+        )
+    }
+
+    # this is important so that your checker is executed before others
+    priority = -1
+
+    @check_messages("shadowing-in-comprehension")
+    def visit_comprehension(self, node: nodes.Comprehension):
+        if isinstance(node.target, nodes.Tuple):
+            for target in node.target.elts:
+                if target.name in node.parent.frame().locals and target.name != "_":
+                    args = target.name
+                    self.add_message("shadowing-in-comprehension", node=target, args=args)
+        else:  # isinstance(node.target, nodes.AssignName)
+            if node.target.name in node.parent.frame().locals and node.target.name != "_":
+                args = node.target.name
+                self.add_message("shadowing-in-comprehension", node=node.target, args=args)
+
+
+def register(linter):
+    linter.register_checker(ShadowingInComprehensionChecker(linter))
