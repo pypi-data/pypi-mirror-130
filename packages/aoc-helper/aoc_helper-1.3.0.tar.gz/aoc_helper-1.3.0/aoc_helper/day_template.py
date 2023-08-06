b164f9664941a895dea22d52b4e65f5dfd6d083b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:11 +0000

    Add package aoc-helper/1.3.0/aoc_helper-1.3.0.tar.gz

---
+++ packages/aoc-helper/aoc_helper-1.3.0.tar.gz/aoc_helper-1.3.0/aoc_helper/day_template.py
@@ -0,0 +1,23 @@
+import aoc_helper
+from aoc_helper import extract_ints, frange, irange, iter, list, map, range, tail_call
+
+raw = aoc_helper.fetch({day}, {year})
+
+
+def parse_raw():
+    ...
+
+
+data = parse_raw()
+
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+
+aoc_helper.lazy_submit(day={day}, year={year}, solution=part_one)
+aoc_helper.lazy_submit(day={day}, year={year}, solution=part_two)
