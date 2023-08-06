15211f84c841e39be61cfcce534c73e86b08f1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:52 +0000

    Add package fao56/0.0.1/fao56-0.0.1.tar.gz

---
+++ packages/fao56/fao56-0.0.1.tar.gz/fao56-0.0.1/src/fao56/__init__.py
@@ -0,0 +1,11 @@
+"""
+Data and formalisms from FAO56 related to crop evapotranspiration
+"""
+# {# pkglts, src
+# FYEO
+# #}
+# {# pkglts, version, after src
+from . import version
+
+__version__ = version.__version__
+# #}
