a50f477149bd7af78f5264d243faa0c6402c97f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package botbuilder-sangamam-cognitive-text-translate/0.3.0/botbuilder-sangamam-cognitive-text-translate-0.3.0.tar.gz

---
+++ packages/botbuilder-sangamam-cognitive-text-translate/botbuilder-sangamam-cognitive-text-translate-0.3.0.tar.gz/botbuilder-sangamam-cognitive-text-translate-0.3.0/botbuilder/sangamam/cognitive/text/translate/about.py
@@ -0,0 +1,12 @@
+import os
+
+__title__ = "botbuilder-sangamam-cognitive-text-translate"
+__version__ = (
+    os.environ["packageVersion"] if "packageVersion" in os.environ else "0.3.0"
+)
+__uri__ = "https://github.com/rvinothrajendran"
+__author__ = "Bot Builder sangamam"
+__author_email__ = "r.vinoth@live.com"
+__description__ = "Microsoft Bot Builder sangamam Middleware test recognizer"
+__summary__ = "A collection of Bot Framework middleware components that uses Microsoft's cognitive translator services to translate the message for Microsoft Bot Builder SDK for Python"
+__license__ = "MIT"
