17062d51a8a9c14e9df2e1de5d53e50a73c41517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:46 +0000

    Add package cdk-alexa-skill/1.0.5/cdk_alexa_skill-1.0.5-py3-none-any.whl

---
+++ packages/cdk-alexa-skill/cdk_alexa_skill-1.0.5-py3-none-any.whl/cdk_alexa_skill/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.alexa_ask._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_python._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-alexa-skill", "1.0.5", __name__[0:-6], "cdk-alexa-skill@1.0.5.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
