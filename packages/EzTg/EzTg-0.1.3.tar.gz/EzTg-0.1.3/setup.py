bb8c2e6a1d658c0616ff79e598df6524af7a58e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:21 +0000

    Add package EzTg/0.1.3/EzTg-0.1.3.tar.gz

---
+++ packages/EzTg/EzTg-0.1.3.tar.gz/EzTg-0.1.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EzTg",
+    version="0.1.3",
+    author="Hexye",
+    author_email="dragonsale22@gmail.com",
+    description="An api wrapper for telegram easy to use based on telegram core methods.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HexyeDEV/EzTg/",
+    download_url="https://github.com/HexyeDEV/EzTg/releases",
+    project_urls={
+        "Bug Tracker": "https://github.com/HexyeDEV/EzTg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=["EzTg"],
+    python_requires=">=3.6",
+    install_requires=['requests>=2.26.0'],
+)
