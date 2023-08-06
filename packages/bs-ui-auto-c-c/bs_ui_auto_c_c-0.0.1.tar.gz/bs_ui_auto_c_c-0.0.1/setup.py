0238743395160a9e3c7ebe9dd942f47e81f557fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:53 +0000

    Add package bs-ui-auto-c-c/0.0.1/bs_ui_auto_c_c-0.0.1.tar.gz

---
+++ packages/bs-ui-auto-c-c/bs_ui_auto_c_c-0.0.1.tar.gz/bs_ui_auto_c_c-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="bs_ui_auto_c_c",
+    version="0.0.1",
+    author="sijunji",
+    author_email="sijunji@outlook.com",
+    description="一个selenium自动安装驱动、日志的项目",
+    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    license='apache3.0',
+
+)
