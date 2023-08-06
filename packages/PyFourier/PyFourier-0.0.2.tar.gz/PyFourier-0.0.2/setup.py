9168a5d99ccf997ca9b3b759857e030bf83fb677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package PyFourier/0.0.2/PyFourier-0.0.2.tar.gz

---
+++ packages/PyFourier/PyFourier-0.0.2.tar.gz/PyFourier-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='PyFourier',
+   version='0.0.2',
+   description='A module for visualizing Fourier transform and its properties',
+   author='Sergio Lopez Banos',
+   author_email='sergioloba98@gmail.com',
+   packages=find_packages(),  #same as name
+   
+)
