2f896457c52411543f0a2dcb23ec976da85d210c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:13 +0000

    Add package fastapi-misskey/0.0.1/fastapi_misskey-0.0.1.tar.gz

---
+++ packages/fastapi-misskey/fastapi_misskey-0.0.1.tar.gz/fastapi_misskey-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+readme = open('README.md', 'r').read()
+
+setup(
+    name="fastapi_misskey",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Misskey authentication to FastAPI",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='yupix',
+    license='MIT',
+    install_requires=['fastapi==0.70.0', 'uvicorn==0.15.0', 'aiohttp==3.8.1', 'aiocache==0.11.1'],
+    python_requires='>=3.5',
+    url='https://github.com/yupix/fastapi-misskey',
+    keywords='misskey, fastapi',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
