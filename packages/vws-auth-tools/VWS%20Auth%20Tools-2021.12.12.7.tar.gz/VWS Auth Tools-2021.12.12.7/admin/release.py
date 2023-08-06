cb86fb57450990c5af48d26eb77e05e8a211f68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:43 +0000

    Add package vws-auth-tools/2021.12.12.7/VWS%20Auth%20Tools-2021.12.12.7.tar.gz

---
+++ packages/vws-auth-tools/VWS%20Auth%20Tools-2021.12.12.7.tar.gz/VWS Auth Tools-2021.12.12.7/admin/release.py
@@ -0,0 +1,63 @@
+"""
+Release the next version.
+"""
+
+import os
+from pathlib import Path
+
+from github import Github
+from github.ContentFile import ContentFile
+from github.Repository import Repository
+
+
+def update_changelog(version: str, github_repository: Repository) -> None:
+    """
+    Add a version title to the changelog.
+    """
+    changelog_path = Path('CHANGELOG.rst')
+    branch = 'master'
+    changelog_content_file = github_repository.get_contents(
+        path=str(changelog_path),
+        ref=branch,
+    )
+    # ``get_contents`` can return a ``ContentFile`` or a list of
+    # ``ContentFile``s.
+    assert isinstance(changelog_content_file, ContentFile)
+    changelog_bytes = changelog_content_file.decoded_content
+    changelog_contents = changelog_bytes.decode('utf-8')
+    new_changelog_contents = changelog_contents.replace(
+        'Next\n----',
+        f'Next\n----\n\n{version}\n------------',
+    )
+    github_repository.update_file(
+        path=str(changelog_path),
+        message=f'Update for release {version}',
+        content=new_changelog_contents,
+        sha=changelog_content_file.sha,
+    )
+
+
+def main() -> None:
+    """
+    Perform a release.
+    """
+    github_token = os.environ['GITHUB_TOKEN']
+    github_repository_name = os.environ['GITHUB_REPOSITORY']
+    version_str = os.environ['NEXT_VERSION']
+    github_client = Github(github_token)
+    github_repository = github_client.get_repo(
+        full_name_or_id=github_repository_name,
+    )
+    update_changelog(version=version_str, github_repository=github_repository)
+    github_repository.create_git_tag_and_release(
+        tag=version_str,
+        tag_message='Release ' + version_str,
+        release_name='Release ' + version_str,
+        release_message='See CHANGELOG.rst',
+        type='commit',
+        object=github_repository.get_commits()[0].sha,
+    )
+
+
+if __name__ == '__main__':
+    main()
