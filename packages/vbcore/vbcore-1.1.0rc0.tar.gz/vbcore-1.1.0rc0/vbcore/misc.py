467d800f11e6d6b5fc00ff173c5da83201eaf7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:02 +0000

    Add package vbcore/1.1.0rc0/vbcore-1.1.0rc0.tar.gz

---
+++ packages/vbcore/vbcore-1.1.0rc0.tar.gz/vbcore-1.1.0rc0/vbcore/misc.py
@@ -0,0 +1,129 @@
+import os
+import signal
+import string
+import sys
+import tempfile
+import typing as t
+from random import SystemRandom
+from threading import Lock
+
+
+class Printer:
+    function = print
+    thread_lock = Lock()
+
+    @classmethod
+    def safe(cls, *args, **kwargs):
+        with cls.thread_lock:
+            cls.function(*args, **kwargs)
+
+    @classmethod
+    def error(cls, *args, **kwargs):
+        kwargs.setdefault("file", sys.stderr)
+        cls.function(*args, **kwargs)
+
+    @classmethod
+    def safe_error(cls, *args, **kwargs):
+        kwargs.setdefault("file", sys.stderr)
+        cls.error(*args, **kwargs)
+
+
+def parse_value(v):
+    """
+
+    :param v:
+    :return:
+    """
+    try:
+        return float(v) if "." in v else int(v)
+    except ValueError:
+        if v.lower() in ("true", "false"):
+            return v.lower() == "true"
+    return v
+
+
+def get_from_dict(d: dict, k, default=None):
+    """
+    get a value from dict without raising exceptions
+
+    :param d: dict
+    :param k: key
+    :param default: default value if k is missing
+    :return: d[k] or default
+    """
+    if isinstance(d, dict):
+        return d.get(k, default)
+    return default
+
+
+def to_int(n) -> t.Optional[int]:
+    """
+    cast input to int if an error occurred returns None
+
+    :param n: input
+    :return: int or None
+    """
+    try:
+        return int(n)
+    except (TypeError, ValueError):
+        return None
+
+
+def to_float(n) -> t.Optional[float]:
+    """
+    cast input to float if an error occurred returns None
+
+    :param n: input
+    :return: float or None
+    """
+    try:
+        return float(n)
+    except (TypeError, ValueError):
+        return None
+
+
+def random_string(length: int, alphabet: str = string.printable) -> str:
+    """
+
+    :param length:
+    :param alphabet:
+    :return:
+    """
+    return "".join(SystemRandom().choice(alphabet) for _ in range(length))
+
+
+class Signal:
+    @classmethod
+    def handle_terminate(cls, signum, frame, callback: t.Callable = lambda: None):
+        Printer.error(f"received signal: {signum} at frame: {frame}")
+        callback()
+        sys.exit(signum)
+
+    @classmethod
+    def register(cls, handler: t.Callable, *signals):
+        """
+
+        :param handler:
+        :param signals:
+        """
+        for s in signals:
+            try:
+                sig = getattr(signal, s)
+                signal.signal(sig, handler)
+            except Exception as exc:  # pylint: disable=broad-except
+                Printer.error(f"unable to register signal {s}: {exc}")
+
+
+class TempFile:
+    def __init__(self, data):
+        self.data = data
+        # pylint: disable=consider-using-with
+        self.file = tempfile.NamedTemporaryFile(delete=False)
+
+    def __enter__(self):
+        self.file.write(self.data)
+        self.file.close()
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.unlink(self.file.name)
