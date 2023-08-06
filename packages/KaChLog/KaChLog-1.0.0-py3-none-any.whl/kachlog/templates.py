68753a8baa7a4977fca9aa3c725ca4113b4d3a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:57 +0000

    Add package KaChLog/1.0.0/KaChLog-1.0.0-py3-none-any.whl

---
+++ packages/KaChLog/KaChLog-1.0.0-py3-none-any.whl/kachlog/templates.py
@@ -0,0 +1,22 @@
+BASE = """# CHANGELOG
+
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a Changelog](http://keepachangelog.com/).
+"""
+
+DEFAULT_VERSION = "0.0.0"
+
+RELEASE_LINE = {
+    "date": "## {} - ({})\n",
+    "pure": "## {}\n",
+    "bracket_date": "## [{}] - {}\n"
+}
+
+RELEASE_LINE_REGEXES = {
+    "date": r"^##\s(?P<v>\d+\.\d+\.\d+)\s\-\s\(\d{4}-\d{2}-\d{2}\)$",
+    "pure": r"^##\sv?(?P<v>\d+\.\d+\.\d+)",
+    "bracket_date": r"^##\s\[(?P<v>\d+\.\d+\.\d+)\]\s\-\s\d{4}-\d{2}-\d{2}$"
+}
+
+TYPES_OF_CHANGE = ['added', 'changed', 'deprecated', 
+                   'removed', 'fixed', 'security']
