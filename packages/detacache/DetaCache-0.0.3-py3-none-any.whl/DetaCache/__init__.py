3d54474eaeb2b8d15f4c5b6bb777d6a6714e3ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:20 +0000

    Add package detacache/0.0.3/DetaCache-0.0.3-py3-none-any.whl

---
+++ packages/detacache/DetaCache-0.0.3-py3-none-any.whl/DetaCache/__init__.py
@@ -0,0 +1,10 @@
+from DetaCache._detaCache import detaCache
+from DetaCache._localCache import localCache
+from DetaCache._cacheApi import CacheApi
+
+__version__ ='v0.0.3'
+__all__=[
+    'detaCache',
+    'localCache',
+    'CacheApi'
+]
