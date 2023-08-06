6179eefdb9d3ae94625b0ad98f60e7a0aa033da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:09 +0000

    Add package openapi-builder/0.1.6/openapi_builder-0.1.6.tar.gz

---
+++ packages/openapi-builder/openapi_builder-0.1.6.tar.gz/openapi_builder-0.1.6/CHANGELOG.rst
@@ -0,0 +1,24 @@
+Changelog
+=========
+
+All notable changes to openapi_builder will be documented here.
+
+The format is based on `Keep a Changelog`_, and this project adheres to `Semantic Versioning`_.
+
+.. _Keep a Changelog: https://keepachangelog.com/en/1.0.0/
+.. _Semantic Versioning: https://semver.org/spec/v2.0.0.html
+
+Categories for changes are: Added, Changed, Deprecated, Removed, Fixed, Security.
+
+
+Version `0.1.0 <https://github.com/FlyingBird95/openapi_builder/tree/v0.1.0>`__
+--------------------------------------------------------------------------------
+
+First release date: 2021-11-17.
+Added setup for:
+- Continuous integration (automated tests)
+- Continuous delivery (publishing to PyPI)
+- Automatically formatting (black and isort)
+- Checking formatting errors (flake8)
+- Automatic documentation integration and delivery
+- Initial code
---
+++ packages/openapi-builder/openapi_builder-0.1.6.tar.gz/openapi_builder-0.1.6/openapi_builder/exceptions.py
@@ -0,0 +1,36 @@
+from openapi_builder.util import to_camelcase
+
+MISSING_PROCESSOR_MESSAGE = '''
+You're missing a converter for class {class_name}.
+You can use the snippet below to generate a converter for it. Don't forget to register it!
+
+
+from typing import Union
+
+from openapi_builder.builder import OpenAPIBuilder
+from openapi_builder.converts import Converter
+from openapi_builder.specification import Reference, Schema
+
+class {class_name}Converter(Converter):
+    """Converter for a certain class that returns a openapi_builder.specification.Schema."""
+
+    converts_class = {class_name}
+    """Specification of the class that it converts."""
+
+    def convert(value: {class_name}) -> Union[Reference, Schema]:
+        """Converts an instance of {class_name} and return a Reference or Schema."""
+'''
+
+
+class OpenApiException(Exception):
+    """Base Exception."""
+
+
+class MissingConverter(OpenApiException):
+    """Missing converter for the given class or instance."""
+
+    def __init__(self, value):
+        class_name = value.__class__.__name__
+        super().__init__(
+            MISSING_PROCESSOR_MESSAGE.format(class_name=to_camelcase(class_name))
+        )
