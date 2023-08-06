3ac909f3cba30b5b2b698770da622e42600d2423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:04 +0000

    Add package apiflask-admin/0.1/apiflask-admin-0.1.tar.gz

---
+++ packages/apiflask-admin/apiflask-admin-0.1.tar.gz/apiflask-admin-0.1/setup.py
@@ -0,0 +1,35 @@
+"""
+APIFlask-Admin
+-----------------
+Add admin support for APIFlask.
+"""
+from setuptools import setup
+
+setup(
+    name='apiflask-admin',
+    version='0.1',
+    url='https://github.com/greyli/apiflask-admin',
+    license='MIT',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    description='',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
+    platforms='any',
+    packages=['apiflask_admin'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
