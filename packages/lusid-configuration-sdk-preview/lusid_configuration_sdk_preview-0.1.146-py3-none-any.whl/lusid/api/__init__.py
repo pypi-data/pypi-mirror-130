21036cc1099f1c362b87e9b34ef33cde257d7dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:02 +0000

    Add package lusid-configuration-sdk-preview/0.1.146/lusid_configuration_sdk_preview-0.1.146-py3-none-any.whl

---
+++ packages/lusid-configuration-sdk-preview/lusid_configuration_sdk_preview-0.1.146-py3-none-any.whl/lusid/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lusid.api.application_metadata_api import ApplicationMetadataApi
+from lusid.api.configuration_sets_api import ConfigurationSetsApi
