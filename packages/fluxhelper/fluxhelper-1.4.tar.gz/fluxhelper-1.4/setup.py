be3f0e198e94a150513763d7dc5d4d694e85bbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:32 +0000

    Add package fluxhelper/1.4/fluxhelper-1.4.tar.gz

---
+++ packages/fluxhelper/fluxhelper-1.4.tar.gz/fluxhelper-1.4/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+setup(
+    name="fluxhelper",
+    packages=["fluxhelper", "fluxhelper.jarvis"],
+    version="1.4",
+    license="MIT",
+    description="Helper library made for my projects",
+    author="Philippe Mathew",
+    author_email="philmattdev@gmail.com",
+    url="https://github.com/bossauh/fluxhelper",
+    download_url="https://github.com/bossauh/fluxhelper/archive/refs/tags/v_14.tar.gz",
+    keywords=["helper"],
+    install_requires=[
+        "pycaw",
+        "termcolor",
+        "flask",
+        "python-dateutil",
+        "requests",
+        "pymongo",
+        "dnspython",
+        "montydb",
+        "aiohttp",
+        "motor"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
