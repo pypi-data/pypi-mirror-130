e31a1e1e790bb0902f53b2c004927f3226f64831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package py-rmrk-tools/0.1.0a3/py-rmrk-tools-0.1.0a3.tar.gz

---
+++ packages/py-rmrk-tools/py-rmrk-tools-0.1.0a3.tar.gz/py-rmrk-tools-0.1.0a3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rmrk_tools', 'rmrk_tools.rmrk2_0_0']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'py-rmrk-tools',
+    'version': '0.1.0a3',
+    'description': '',
+    'long_description': None,
+    'author': 'Alisher A. Khassanov',
+    'author_email': 'a.khssnv@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
