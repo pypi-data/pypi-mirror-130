17e3408746a1cc49558f738e33b9f07fc89c932b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:32 +0000

    Add package evolutionary-py/0.19/evolutionary-py-0.19.tar.gz

---
+++ packages/evolutionary-py/evolutionary-py-0.19.tar.gz/evolutionary-py-0.19/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(name='evolutionary-py', author='Ashish Juneja',description="A genetic algorithm framework for search optimization problems",version='0.19', packages=find_packages(),
+      install_requires=['numpy', 'wheel'])
