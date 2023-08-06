00036740e885ac9886e2c59604f7dcbefbeefa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:53 +0000

    Add package sl-api-client/1.2.130/sl-api-client-1.2.130.tar.gz

---
+++ packages/sl-api-client/sl-api-client-1.2.130.tar.gz/sl-api-client-1.2.130/sl_api_client/api/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sl_api_client.api.analytics_api import AnalyticsApi
+from sl_api_client.api.auth_api import AuthApi
+from sl_api_client.api.b_territory_api import BTerritoryApi
+from sl_api_client.api.calendar_api import CalendarApi
+from sl_api_client.api.catalogs_api import CatalogsApi
+from sl_api_client.api.collections_api import CollectionsApi
+from sl_api_client.api.discount_api import DiscountApi
+from sl_api_client.api.documents_api import DocumentsApi
+from sl_api_client.api.event_api import EventApi
+from sl_api_client.api.file_api import FileApi
+from sl_api_client.api.infosystems_api import InfosystemsApi
+from sl_api_client.api.loyalty_api import LoyaltyApi
+from sl_api_client.api.message_api import MessageApi
+from sl_api_client.api.orders_api import OrdersApi
+from sl_api_client.api.orders_v2_admin_api import OrdersV2AdminApi
+from sl_api_client.api.orders_v2_client_api import OrdersV2ClientApi
+from sl_api_client.api.pay_api import PayApi
+from sl_api_client.api.payments_v2_api import PaymentsV2Api
+from sl_api_client.api.permissions_api import PermissionsApi
+from sl_api_client.api.pizzaman_api import PizzamanApi
+from sl_api_client.api.router_api import RouterApi
+from sl_api_client.api.service_api import ServiceApi
+from sl_api_client.api.settings_api import SettingsApi
+from sl_api_client.api.shininvest_api import ShininvestApi
+from sl_api_client.api.shops_api import ShopsApi
+from sl_api_client.api.space_api import SpaceApi
+from sl_api_client.api.statistics_api import StatisticsApi
+from sl_api_client.api.support_api import SupportApi
+from sl_api_client.api.talan_api import TalanApi
+from sl_api_client.api.talan_auth_api import TalanAuthApi
+from sl_api_client.api.users_api import UsersApi
