6191db7d0b2959cc42ac7873efcbfdde76fc1943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:43 +0000

    Add package energicity/0.0.10.2/energicity-0.0.10.2.tar.gz

---
+++ packages/energicity/energicity-0.0.10.2.tar.gz/energicity-0.0.10.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='energicity',
+    version='0.0.10.2',
+    description="EnergiCity main python package, it's a toolkit",
+    py_modules=["hello", "powerleone"],
+    package_dir={"":"src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.com/EnergiCity/energicity-py-package',
+    author="Ahmed Ali",
+    author_email='ahmed.elsir.khalfalla@gmail.com',
+    install_requires = [
+        "requests",
+    ],
+    extras_requires = {
+        "dev": [
+            "twine",
+        ],
+    }
+)
---
+++ packages/energicity/energicity-0.0.10.2.tar.gz/energicity-0.0.10.2/src/powerleone.py
@@ -0,0 +1,27 @@
+import requests as req
+
+def sending_sms(kannel_username:str, kannel_password:str, phone_number:str ="23273578902", body:str = "Demo", name:str ="Default"):
+    """
+    :param kannel_username: Kannel SMS gateway username
+    :param phone_number: the phone number of the customer in formation 07XXXXXXX
+    """
+
+    #cleaning
+    phone_number = phone_number.replace("-", "")
+    phone_number = f"232{phone_number[-8:]}"
+    params = {"username":kannel_username, "password": kannel_password, "to": phone_number, "dlr-mask":0, "text":body}
+
+    body = body.replace("  ", " ")
+
+    params = {"username":kannel_username, "password": kannel_password, "to": f"232{phone_number[-8:]}", "dlr-mask":0, "test":body}
+
+
+    try:
+        sms = req.get(url='http://35.156.141.49:13013/cgi-bin/sendsms', params=params)
+        return sms
+    except NameError as e:
+        print(f"{e}, please add \"import requests as req\" to your code")
+        return 0
+    except Exception as e:
+        print(e)
+        return 0
