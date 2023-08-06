f16a4dcd8a3d027551b2bdf900e38231d6425d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:57 +0000

    Add package tinychain/0.6.0.1/tinychain-0.6.0.1-py3-none-any.whl

---
+++ packages/tinychain/tinychain-0.6.0.1-py3-none-any.whl/tinychain/collection/collection.py
@@ -0,0 +1,60 @@
+from tinychain.ref import Post, Put
+from tinychain.reflect import is_ref
+from tinychain.state import Map, State, Tuple
+from tinychain.util import uri, URI
+
+
+class Collection(State):
+    """Data structure responsible for storing a collection of :class:`Value`s."""
+
+    __uri__ = uri(State) + "/collection"
+
+    @classmethod
+    def copy_from(cls, schema, source):
+        """
+        Copy a :class:`Collection` from a :class:`Stream` of values.
+
+        Example:
+            .. highlight:: python
+            .. code-block:: python
+
+                columns = [tc.Column("first", tc.String, 128), tc.Column("second", tc.String, 128)]
+                row_schema = tc.btree.Schema(*columns)
+                table_schema = tc.table.Schema([columns[0]], [columns[1]])
+
+                btree = tc.btree.BTree.load(row_schema, [["hello", "world"]])
+                table = tc.table.Table.copy_from(table_schema, btree.keys())
+        """
+
+        return cls(Post(uri(cls) + "/copy_from", Map(schema=schema, source=source)))
+
+    @classmethod
+    def load(cls, schema, data):
+        """
+        Load a :class:`Collection` from an external data set.
+
+        Example:
+            .. highlight:: python
+            .. code-block:: python
+
+                elements = range(10)
+                dense = tc.tensor.Dense([2, 5], tc.I32, elements)
+        """
+
+        if is_ref(schema):
+            raise ValueError(f"cannot load schema {schema} (consider calling `copy_from` instead)")
+
+        if is_ref(data):
+            raise ValueError(f"cannot load data {data} (consider calling `copy_from` instead)")
+
+        return cls(Put(cls, schema, data))
+
+    def copy(self):
+        """Return a copy of this `Collection`."""
+
+        return self.__class__.copy_from(self.schema(), self)
+
+    def schema(self):
+        """Return the schema of this `Collection`."""
+
+        return self._get("schema", rtype=Tuple)
---
+++ packages/tinychain/tinychain-0.6.0.1-py3-none-any.whl/tinychain/reflect/op.py
@@ -0,0 +1,205 @@
+import inspect
+
+from tinychain import op, ref
+from tinychain.state import State
+from tinychain.util import form_of, requires, to_json, uri, Context, URI
+from tinychain.value import Nil, Value
+
+from . import _get_rtype, resolve_class
+
+
+EMPTY = inspect.Parameter.empty
+
+
+class Op(object):
+    __uri__ = uri(op.Op)
+
+    def __init__(self, form):
+        if not inspect.isfunction(form):
+            raise ValueError(f"reflection requires a Python callable, not {form}")
+
+        self.form = form
+
+    def __deps__(self):
+        params = inspect.signature(self.form).parameters
+        provided = set(URI(param) for param in params)
+        return requires(form_of(self)) - provided
+
+    def __json__(self):
+        return {str(uri(self)): to_json(form_of(self))}
+
+    def __ref__(self):
+        raise RuntimeError("cannot reference a reflected Op; use Get, Put, Post, or Delete instead")
+
+    def dtype(self):
+        return self.__class__.__name__
+
+
+class Get(Op):
+    __uri__ = uri(op.Get)
+
+    def __init__(self, form):
+        self.rtype = _get_rtype(form, State)
+        Op.__init__(self, form)
+
+    def __call__(self, key=None):
+        return ref.Get(self, key)
+
+    def __form__(self):
+        cxt, args = _maybe_first_arg(self)
+
+        sig = inspect.signature(self.form)
+        key_name = "key"
+        if len(sig.parameters) > len(args):
+            key_name = list(sig.parameters.keys())[len(args)]
+            param = sig.parameters[key_name]
+            dtype = resolve_class(self.form, param.annotation, Value)
+            args.append(dtype(URI(key_name)))
+
+        cxt._return = self.form(*args)  # populate the Context
+        return key_name, cxt
+
+    def __ref__(self, name):
+        rtype = self.rtype
+
+        class GetRef(op.Get):
+            def __call__(self, *args, **kwargs):
+                return rtype(op.Get.__call__(self, *args, **kwargs))
+
+        return GetRef(URI(name))
+
+    def __repr__(self):
+        return f"GET Op with form {to_json(self)}"
+
+
+class Put(Op):
+    __uri__ = uri(op.Put)
+
+    def __call__(self, key=None, value=None):
+        return ref.Put(self, key, value)
+
+    def __form__(self):
+        cxt, args = _maybe_first_arg(self)
+
+        sig = inspect.signature(self.form)
+        key_name = "key"
+        value_name = "value"
+
+        if len(sig.parameters) == len(args):
+            pass
+        elif len(sig.parameters) - len(args) == 1:
+            param_name = list(sig.parameters.keys())[-1]
+            param = sig.parameters[key_name]
+            dtype = resolve_class(self.form, param.annotation, Value)
+            if param_name in ["key", "value"]:
+                args.append(dtype(URI(param_name)))
+            else:
+                raise ValueError(f"{self.dtype()} argument {param_name} is ambiguous--use 'key' or 'value' instead")
+        elif len(sig.parameters) - len(args) == 2:
+            param_names = list(sig.parameters.keys())
+            key_name = param_names[-2]
+            param = sig.parameters[key_name]
+            dtype = resolve_class(self.form, param.annotation, Value)
+            args.append(dtype(URI(key_name)))
+
+            value_name = param_names[-1]
+            param = sig.parameters[value_name]
+            dtype = resolve_class(self.form, param.annotation, State)
+            args.append(dtype(URI(value_name)))
+        else:
+            param_names = list(sig.parameters.keys())
+            raise ValueError(
+                f"{self.dtype()} accepts a maximum of three arguments: (cxt, key, value) (found {param_names})")
+
+        cxt._return = self.form(*args)
+        return key_name, value_name, cxt
+
+    def __ref__(self, name):
+        class PutRef(op.Put):
+            def __call__(self, *args, **kwargs):
+                return Nil(op.Put.__call__(self, *args, **kwargs))
+
+        return PutRef(URI(name))
+
+    def __repr__(self):
+        return f"PUT Op with form {self.form}"
+
+
+class Post(Op):
+    __uri__ = uri(op.Post)
+
+    def __init__(self, form):
+        self.rtype = _get_rtype(form, State)
+        Op.__init__(self, form)
+
+    def __call__(self, *args, **kwargs):
+        if args and kwargs:
+            raise ValueError("POST Op takes one arg (a Map) or kwargs, but not both")
+
+        if not args:
+            args = kwargs
+
+        return ref.Get(self, args)
+
+    def __form__(self):
+        cxt, args = _maybe_first_arg(self)
+
+        sig = inspect.signature(self.form)
+        kwargs = {}
+        for name, param in list(sig.parameters.items())[len(args):]:
+            dtype = resolve_class(self.form, param.annotation, State)
+            kwargs[name] = dtype(URI(name))
+
+        cxt._return = self.form(*args, **kwargs)
+        return cxt
+
+    def __ref__(self, name):
+        rtype = self.rtype
+
+        class PostRef(op.Post):
+            def __call__(self, *args, **kwargs):
+                return rtype(op.Post.__call__(self, *args, **kwargs))
+
+        return PostRef(URI(name))
+
+    def __repr__(self):
+        return f"POST Op with form {self.form}"
+
+
+class Delete(Op):
+    __uri__ = uri(op.Delete)
+
+    def __call__(self, key=None):
+        return ref.Get(self, key)
+
+    def __form__(self):
+        return Get.__form__(self)
+
+    def __ref__(self, name):
+        class DeleteRef(op.Delete):
+            def __call__(self, *args, **kwargs):
+                return Nil(op.Delete.__call__(self, *args, **kwargs))
+
+        return DeleteRef(URI(name))
+
+    def __repr__(self):
+        return f"DELETE Op with form {self.form}"
+
+
+def _maybe_first_arg(op):
+    sig = inspect.signature(op.form)
+    param_names = list(sig.parameters.keys())
+
+    cxt = Context()
+    args = []
+
+    if sig.parameters:
+        first_param = param_names[0]
+        if first_param == "cxt" or first_param == "txn":
+            param = sig.parameters[first_param]
+            if param.annotation == EMPTY or param.annotation is Context:
+                args.append(cxt)
+            else:
+                raise ValueError(f"{param} must be a {Context}, not {param.annotation}")
+
+    return cxt, args
