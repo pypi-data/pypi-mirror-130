7a491e6a0f28092e6391eaa1c24e8929eedf24c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:24 +0000

    Add package hexagonal-py/0.0.24/hexagonal_py-0.0.24-py3-none-any.whl

---
+++ packages/hexagonal-py/hexagonal_py-0.0.24-py3-none-any.whl/hexagonal/use_cases/generate_diagram_usecase.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from diagrams import Diagram, Cluster
+from diagrams.aws.storage import Storage
+
+from hexagonal.domain.hexagonal_layer import HexagonalLayer
+
+
+class GenerateDiagramUseCase:
+
+    @staticmethod
+    def execute(*, project_name: str, hexagonal_composition: List[HexagonalLayer], show: bool = False,
+                output_file_name: str = '') -> bool:
+        last_node = None
+        with Diagram(project_name, show=show, filename=output_file_name):
+            for layer in hexagonal_composition:
+                with Cluster(layer.name):
+                    is_first = True
+                    for dirs in layer.directories_groups:
+                        for module in dirs:
+                            cur_node = Storage(label=module)
+                            if is_first:
+                                is_first = False
+                                if last_node is not None:
+                                    last_node >> cur_node
+
+                            last_node = cur_node
+
+        return last_node is not None
