4f66dcc20579021245d0e2c06a3b14f861188687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:33 +0000

    Add package git-interface/0.5.1/git-interface-0.5.1.tar.gz

---
+++ packages/git-interface/git-interface-0.5.1.tar.gz/git-interface-0.5.1/git_interface/constants.py
@@ -0,0 +1,15 @@
+"""
+Constants
+"""
+EMPTY_REPO_RE = r"fatal: your current branch '\w+' does not have any commits yet"
+UNKNOWN_REV_RE = r"fatal: ambiguous argument '\w+': unknown revision or path not in the working tree."
+BRANCH_ALREADY_EXISTS_RE = r"fatal: A branch named '\w+' already exists."
+BRANCH_NOT_FOUND_RE = r"error: branch '\w+' not found."
+BRANCH_REFNAME_NOT_FOUND_RE = r"error: refname \w+ not found"
+NOT_VALID_OBJECT_NAME_RE = r"fatal: Not a valid object name \w+"
+INVALID_OBJECT_NAME = r"fatal: invalid object name '.+'"
+PATH_DOES_NOT_EXIST = r"fatal: path '.+' does not exist in '.+'"
+# git ls-tree <tree-ish>
+LS_TREE_RE = r"^(\d{6}) (\w+) (\w{40})\t(.+)$"
+# git ls-tree <tree-ish> -l
+LS_TREE_LONG_RE = r"^(\d{6}) (\w+) (\w{40}) +(\d+|\-)\t(.+)$"
---
+++ packages/git-interface/git-interface-0.5.1.tar.gz/git-interface-0.5.1/git_interface/exceptions.py
@@ -0,0 +1,35 @@
+"""
+Exceptions that could be raised during one of the git commands
+"""
+
+
+class GitException(Exception):
+    pass
+
+
+class NoCommitsException(GitException):
+    pass
+
+
+class NoBranchesException(GitException):
+    pass
+
+
+class AlreadyExistsException(GitException):
+    pass
+
+
+class NoLogsException(GitException):
+    pass
+
+
+class UnknownRevisionException(GitException):
+    pass
+
+
+class UnknownRefException(GitException):
+    pass
+
+
+class PathDoesNotExistInRevException(GitException):
+    pass
