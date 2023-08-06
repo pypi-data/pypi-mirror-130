9aa0db9ccaa38337e099b7fdad76730d4d583fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package casbin/1.13.0/casbin-1.13.0-py3-none-any.whl

---
+++ packages/casbin/casbin-1.13.0-py3-none-any.whl/casbin/model/function.py
@@ -0,0 +1,27 @@
+from casbin import util
+
+
+class FunctionMap:
+    fm = None
+
+    def __init__(self):
+        self.fm = dict()
+
+    def add_function(self, name, func):
+        self.fm[name] = func
+
+    @staticmethod
+    def load_function_map():
+        fm = FunctionMap()
+        fm.add_function("keyMatch", util.key_match_func)
+        fm.add_function("keyMatch2", util.key_match2_func)
+        fm.add_function("keyMatch3", util.key_match3_func)
+        fm.add_function("keyMatch4", util.key_match4_func)
+        fm.add_function("regexMatch", util.regex_match_func)
+        fm.add_function("ipMatch", util.ip_match_func)
+        fm.add_function("globMatch", util.glob_match_func)
+
+        return fm
+
+    def get_functions(self):
+        return self.fm
