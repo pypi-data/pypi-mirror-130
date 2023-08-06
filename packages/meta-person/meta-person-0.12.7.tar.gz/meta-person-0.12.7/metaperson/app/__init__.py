ca43f51cfff9afc31859be24971bb195a89b7d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:53 +0000

    Add package meta-person/0.12.7/meta-person-0.12.7.tar.gz

---
+++ packages/meta-person/meta-person-0.12.7.tar.gz/meta-person-0.12.7/metaperson/app/__init__.py
@@ -0,0 +1,4 @@
+from .person_detection import PersonDetection
+from .person_detection_onnx import PersonDetectionOnnx
+from .person_recognition import PersonTracker
+from .person_recognition_onnx import PersonTrackerOnnx
