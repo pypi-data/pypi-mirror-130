69bbafb545f641b0146e21d8bbd02ad7ba5567bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:04 +0000

    Add package py-steamcmd-wrapper/1.1.1/py_steamcmd_wrapper-1.1.1-py3-none-any.whl

---
+++ packages/py-steamcmd-wrapper/py_steamcmd_wrapper-1.1.1-py3-none-any.whl/pysteamcmdwrapper/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+__author__ = "Wouter Mellema"
+__copyright__ = "Copyright 2020"
+__licence__ = "GNU GPLv3"
+__version__ = "1.1.1"
+__maintainer__ = "Wouter Mellema"
+__status__ = "Development"
+
+from pysteamcmdwrapper.SteamCMD import SteamCMD
+from pysteamcmdwrapper.SteamCMD_command import SteamCMD_command
+from pysteamcmdwrapper.exceptions import SteamCMDException, SteamCMDDownloadException, SteamCMDInstallException
