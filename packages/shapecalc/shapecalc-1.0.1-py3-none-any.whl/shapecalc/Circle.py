9b4f5e31f35dcbb4a1f6e9b08186e3b8930a1157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:18 +0000

    Add package shapecalc/1.0.1/shapecalc-1.0.1-py3-none-any.whl

---
+++ packages/shapecalc/shapecalc-1.0.1-py3-none-any.whl/shapecalc/Circle.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+def circle_s(Radius):
+    pi = 3.1415926
+    S=Radius * Radius * pi
+
+def circle_c(Radius):
+    pi = 3.1415926
+    R=Radius * 2 * math.pi
---
+++ packages/shapecalc/shapecalc-1.0.1-py3-none-any.whl/shapecalc/Square.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+def square_s(a):
+    s=a ** 2
+
+def square_c(a):
+    c = a * 4
