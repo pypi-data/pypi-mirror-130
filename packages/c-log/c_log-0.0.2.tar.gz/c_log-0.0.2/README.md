0d533da1abcec7bdbc214f0cc6b5a29f73f1b86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:41 +0000

    Add package c-log/0.0.2/c_log-0.0.2.tar.gz

---
+++ packages/c-log/c_log-0.0.2.tar.gz/c_log-0.0.2/README.md
@@ -0,0 +1,11 @@
+# c_logger
+TODO: make desc
+
+## Build
+
+```sh
+python3 setup.py sdist bdist_wheel
+```
+```sh
+twine upload dist/*
+```
---
+++ packages/c-log/c_log-0.0.2.tar.gz/c_log-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='c_log',
+    version='0.0.2',
+    author='Andrey Bolshakov',
+    author_email='andrey.bolshakov@accenture.com',
+    description='Colored logger',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires='>=3.8',
+)
