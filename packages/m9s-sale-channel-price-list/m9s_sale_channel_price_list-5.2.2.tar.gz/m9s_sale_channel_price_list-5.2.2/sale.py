1285c93480e2bd79893e974dd0e957c1c1c0d84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:55 +0000

    Add package m9s-sale-channel-price-list/5.2.2/m9s_sale_channel_price_list-5.2.2.tar.gz

---
+++ packages/m9s-sale-channel-price-list/m9s_sale_channel_price_list-5.2.2.tar.gz/m9s_sale_channel_price_list-5.2.2/sale.py
@@ -0,0 +1,37 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import Pool, PoolMeta
+
+
+class Sale(metaclass=PoolMeta):
+    __name__ = 'sale.sale'
+
+    @classmethod
+    def default_price_list(cls):
+        pool = Pool()
+        Sale = pool.get('sale.sale')
+        Channel = pool.get('sale.channel')
+
+        channel = Sale.default_channel()
+        if channel:
+            return Channel(channel).price_list.id
+
+    def set_price_list(self):
+        # Until here super() sets the party price list or sales configuration
+        # price list. So we set the channel price list if there is no party or
+        # no price list on the party defined.
+        if self.channel:
+            if not self.party or (
+                    self.party and not self.party.sale_price_list):
+                self.price_list = self.channel.price_list.id
+
+    @fields.depends('channel', 'party')
+    def on_change_channel(self):
+        super().on_change_channel()
+        self.set_price_list()
+
+    @fields.depends('channel', 'party')
+    def on_change_party(self):
+        super().on_change_party()
+        self.set_price_list()
