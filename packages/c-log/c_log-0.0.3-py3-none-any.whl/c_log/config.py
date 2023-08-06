47648cff069e3dcafd038f3d7f25b70e251462ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:44 +0000

    Add package c-log/0.0.3/c_log-0.0.3-py3-none-any.whl

---
+++ packages/c-log/c_log-0.0.3-py3-none-any.whl/c_log/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Config(object):
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDCOLOR = '\033[0m'
