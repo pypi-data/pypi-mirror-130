34d1dba48f97f237df6da3f1df4d9473ee7dfa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:54 +0000

    Add package odoo12-addon-remove-dup-acc-num-constraint/12.0.1.0.1/odoo12_addon_remove_dup_acc_num_constraint-12.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-remove-dup-acc-num-constraint/odoo12_addon_remove_dup_acc_num_constraint-12.0.1.0.1-py3-none-any.whl/odoo/addons/remove_dup_acc_num_constraint/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Remove Duplicated Account Number Constraint",
+    "version": "12.0.1.0.1",
+    "depends": ["base"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Accounting & Finance",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+         It allows one account to be held by multiple partners
+    """,
+    "data": [],
+    "installable": True,
+}
---
+++ packages/odoo12-addon-remove-dup-acc-num-constraint/odoo12_addon_remove_dup_acc_num_constraint-12.0.1.0.1-py3-none-any.whl/odoo/addons/remove_dup_acc_num_constraint/models/res_partner_bank.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class ResPartnerBank(models.Model):
+  _inherit = 'res.partner.bank'
+
+  _sql_constraints = [
+    ('unique_number', 'unique(sanitized_acc_number, company_id, partner_id)', 'Account Number must be unique'),
+  ]
