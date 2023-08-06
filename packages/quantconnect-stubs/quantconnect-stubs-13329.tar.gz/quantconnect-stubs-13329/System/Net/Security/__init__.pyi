080b4894a1f43d2a6bf116bb2224a43a79aa3f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:05 +0000

    Add package quantconnect-stubs/13329/quantconnect-stubs-13329.tar.gz

---
+++ packages/quantconnect-stubs/quantconnect-stubs-13329.tar.gz/quantconnect-stubs-13329/System/Net/Security/__init__.pyi
@@ -0,0 +1,26 @@
+import System
+import System.Net.Security
+
+
+class SslPolicyErrors(System.Enum):
+    """This class has no documentation."""
+
+    # Cannot convert to Python: None = ...
+
+    RemoteCertificateNotAvailable = ...
+
+    RemoteCertificateNameMismatch = ...
+
+    RemoteCertificateChainErrors = ...
+
+
+class AuthenticationLevel(System.Enum):
+    """This class has no documentation."""
+
+    # Cannot convert to Python: None = 0
+
+    MutualAuthRequested = 1
+
+    MutualAuthRequired = 2
+
+
