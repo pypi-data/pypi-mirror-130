1b45074804cd1437425a3488ef160642eadf4037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:03 +0000

    Add package pysofi/v0.1.1-alpha/pysofi-v0.1.1-alpha.tar.gz

---
+++ packages/pysofi/pysofi-v0.1.1-alpha.tar.gz/pysofi-v0.1.1-alpha/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pysofi',
+    packages = ['pysofi'],
+    version = 'v0.1.1-alpha', 
+    description = 'first release',
+    author = 'Yuting Miao, Shimon Weiss, Xiyu Yi',
+    author_email = 'ytmiao@ucla.edu',
+    url = 'https://github.com/xiyuyi-at-LLNL/pysofi',
+    download_url = 'https://github.com/xiyuyi-at-LLNL/pysofi/archive/refs/tags/v0.1.1-alpha.tar.gz',
+    keywords = ['pysofi'],
+    classifiers = [],
+)
+
