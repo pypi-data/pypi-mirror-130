d943f8df628da92302b27dbceb80e11078db018d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:21 +0000

    Add package SecrLib/0.0.3/SecrLib-0.0.3-py3-none-any.whl

---
+++ packages/SecrLib/SecrLib-0.0.3-py3-none-any.whl/Secr/secrmath.py
@@ -0,0 +1,9 @@
+def slope(x1, y1, x2, y2):
+    mksure = int(x1)
+    mksure1 = int(x2)
+    x3 = mksure1 - mksure
+    mksure2 = int(y1)
+    mksure3 = int(y2)
+    y3 = mksure3 - mksure2
+    xy1 = y3 / x3
+    print(xy1)
