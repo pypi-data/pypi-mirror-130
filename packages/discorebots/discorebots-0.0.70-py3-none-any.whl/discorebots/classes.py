deac1c1db0bf1b0ed71ad3057dec66961b7bd57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:42 +0000

    Add package discorebots/0.0.70/discorebots-0.0.70-py3-none-any.whl

---
+++ packages/discorebots/discorebots-0.0.70-py3-none-any.whl/discorebots/classes.py
@@ -0,0 +1,14 @@
+class commandloader:
+
+  """
+  Writes commands to a list for further storage
+  """
+
+  commandsToLoad = []
+
+  def __init__(self, name: str = None, code: list = None):
+    self.name = name
+    self.code = code
+    self.split = ";."
+
+    commandloader.commandsToLoad.append(self)
