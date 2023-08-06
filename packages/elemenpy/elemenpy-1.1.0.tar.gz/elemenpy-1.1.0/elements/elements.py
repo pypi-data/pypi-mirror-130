3bd33fb4952cf6ee8ede9c8fb0ef6dff80dc0713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:45 +0000

    Add package elemenpy/1.1.0/elemenpy-1.1.0.tar.gz

---
+++ packages/elemenpy/elemenpy-1.1.0.tar.gz/elemenpy-1.1.0/elements/elements.py
@@ -0,0 +1,31 @@
+"""A little utility module for element properties."""
+
+from elements.classes import elements_dict
+
+def symbol(atomic_number: int) -> str:
+    """
+    Returns the element symbol of a given atomic number.
+    parameters: atomic number (int)
+    """
+    return elements_dict[atomic_number].get_symbol()
+
+def mass(atomic_number: int) -> float:
+    """
+    Returns the atomic mass of a given element.
+    parameters: atomic number (int)
+    """
+    return elements_dict[atomic_number].get_mass()
+
+def name(atomic_number: int) -> str:
+    """
+    Returns the name of a given element.
+    parameters: atomic number (int)
+    """
+    return elements_dict[atomic_number].get_name()
+
+def group(atomic_number: int) -> str:
+    """
+    Returns the group of a given element.
+    parameters: atomic number (int)
+    """
+    return elements_dict[atomic_number].get_group()
---
+++ packages/elemenpy/elemenpy-1.1.0.tar.gz/elemenpy-1.1.0/tests/test_lists.py
@@ -0,0 +1,11 @@
+from elements import lists
+
+def test_lists():
+    """
+    Test certain indexes of the elements lists.
+    """
+    assert lists.masses[0] == 4.0026
+    assert lists.names[15] == 'Chlorine'
+    assert lists.symbols[27] == 'Cu'
+    assert lists.numbers[0] == 2
+    assert lists.groups[14] == '16'
