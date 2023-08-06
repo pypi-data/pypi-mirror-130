8eeca789ee2f1af55c2de4f0cd805521c22f5fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:17 +0000

    Add package pycloudflare/2.3.0/pycloudflare-2.3.0.tar.gz

---
+++ packages/pycloudflare/pycloudflare-2.3.0.tar.gz/pycloudflare-2.3.0/pycloudflare/exceptions.py
@@ -0,0 +1,9 @@
+class SSLUnavailable(Exception):
+    """If you haven't verified a CNAME zone within the grace period (a week),
+    it can't be verified any more.
+    """
+    pass
+
+
+class CustomHostnameNotFound(Exception):
+    pass
