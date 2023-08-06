2f72b603f9b6bc4b4abb5750a0ca1039b7086e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:52 +0000

    Add package ontolearn/0.5.0/ontolearn-0.5.0.tar.gz

---
+++ packages/ontolearn/ontolearn-0.5.0.tar.gz/ontolearn-0.5.0/ontolearn/__init__.py
@@ -0,0 +1,13 @@
+"""Structured Machine learning modules for Python
+
+Ontolearn is an open-source software library for structured machine learning in Python
+The goal of ontolearn is to provide efficient solutions for concept learning on RDF knowledge bases
+
+
+Author:
+    The Ontolearn team <onto-learn@lists.uni-paderborn.de>
+"""
+__version__ = '0.5.0'
+
+from .knowledge_base import KnowledgeBase
+__all__ = 'KnowledgeBase',
