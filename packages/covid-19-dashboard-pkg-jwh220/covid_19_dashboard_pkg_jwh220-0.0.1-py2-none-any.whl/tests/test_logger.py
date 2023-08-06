67830c3f4ea064e7e81e7093880fa2d2939cd3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package covid-19-dashboard-pkg-jwh220/0.0.1/covid_19_dashboard_pkg_jwh220-0.0.1-py2-none-any.whl

---
+++ packages/covid-19-dashboard-pkg-jwh220/covid_19_dashboard_pkg_jwh220-0.0.1-py2-none-any.whl/tests/test_logger.py
@@ -0,0 +1,12 @@
+from logger import log_infomation
+from logger import log_error
+from logger import log_warning
+
+def test_log_infomation():
+    log_infomation('Test infomation')
+
+def test_log_error():
+    log_error('Test error')
+
+def test_log_warning():
+    log_warning('Test warning')
