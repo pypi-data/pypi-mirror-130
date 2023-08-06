100e059514cae870bba53161b55816e7649696f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:13 +0000

    Add package mapsud/1.0.3/mapsud-1.0.3.tar.gz

---
+++ packages/mapsud/mapsud-1.0.3.tar.gz/mapsud-1.0.3/test/test_mapsud.py
@@ -0,0 +1,19 @@
+#%%
+import sys 
+import os
+import pytest
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+import mapsud
+from mapsud.graph import trajet_optimal_min, trajet_optimal
+
+#%%
+#On test les cas particuliers
+
+def test_traj1():
+    assert trajet_optimal_min(41,42) == -1 
+
+   
+def test_traj2():
+    assert trajet_optimal(29,30) == -1
+
+
