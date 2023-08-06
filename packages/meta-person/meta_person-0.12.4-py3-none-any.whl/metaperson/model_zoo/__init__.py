33a1a70f380ef89d678a0eda25fdb3d0422a7393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:40 +0000

    Add package meta-person/0.12.4/meta_person-0.12.4-py3-none-any.whl

---
+++ packages/meta-person/meta_person-0.12.4-py3-none-any.whl/metaperson/model_zoo/__init__.py
@@ -0,0 +1,4 @@
+from .byte_tracker import BYTETracker
+from .yolo_head import YOLOXHead
+from .yolox import YOLOX
+from .yolo_pafpn import YOLOPAFPN
---
+++ packages/meta-person/meta_person-0.12.4-py3-none-any.whl/metaperson/utils/__init__.py
@@ -0,0 +1,2 @@
+from .build import get_exp_by_file
+from .boxes import *
