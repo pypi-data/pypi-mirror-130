6a366a5627069c0f8abeec654373ea16d713619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:13 +0000

    Add package ucbox-plugin/0.0.18/ucbox_plugin-0.0.18-py3-none-any.whl

---
+++ packages/ucbox-plugin/ucbox_plugin-0.0.18-py3-none-any.whl/ucbox_plugin/__init__.py
@@ -0,0 +1,19 @@
+from extras.plugins import PluginConfig
+
+
+class UCBoxConfig(PluginConfig):
+    name = 'ucbox_plugin'
+    verbose_name = 'UCBox Plugin'
+    description = 'Unified Communications Management Plugin for NetBox.'
+    version = 'v0.0.18'
+    author = 'Jeff Levensailor'
+    author_email = 'jeff@levensailor.com'
+    base_url = 'ucbox'
+    min_version = "2.11.0"
+    required_settings = []
+    default_settings = {}
+    caching_config = {
+        '*': None
+    }
+
+config = UCBoxConfig
