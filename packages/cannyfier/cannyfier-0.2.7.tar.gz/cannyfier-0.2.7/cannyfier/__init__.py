46f81ecca2d0dbf941508a501651fbf92df28318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:08 +0000

    Add package cannyfier/0.2.7/cannyfier-0.2.7.tar.gz

---
+++ packages/cannyfier/cannyfier-0.2.7.tar.gz/cannyfier-0.2.7/cannyfier/__init__.py
@@ -0,0 +1,11 @@
+__name__ = 'cannyfier'
+__version__ = '0.2.7'
+__author__ = 'Pascual Martinez'
+__description__ = 'Show canny on any part of the screen. Better to use with video'
+__IX__ = 0
+__FX__ = 0
+__IY__ = 0
+__FY__ = 0
+__DRAWING__ = False
+
+from .cannyfier import Cannyfier, get_capture_coords
