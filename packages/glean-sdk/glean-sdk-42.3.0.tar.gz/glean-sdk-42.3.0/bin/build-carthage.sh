e8e912d9e546ecec52597c7214c7a5b26284f347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:12 +0000

    Add package glean-sdk/42.3.0/glean-sdk-42.3.0.tar.gz

---
+++ packages/glean-sdk/glean-sdk-42.3.0.tar.gz/glean-sdk-42.3.0/bin/build-carthage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -euvx
+
+FRAMEWORK_NAME="${1:-Glean}"
+CONFIGURATION="${2:-Release}"
+
+WORKSPACE_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )"
+
+set -o pipefail && \
+    carthage build --archive --platform iOS --cache-builds --verbose --configuration "${CONFIGURATION}" "${FRAMEWORK_NAME}" | \
+    xcpretty
+
+# Add dependency information
+zip -u "${FRAMEWORK_NAME}.framework.zip" DEPENDENCIES.md
