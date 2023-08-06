b4e67f5a82c99acda3b8418855acaf661eb3b28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:17 +0000

    Add package json-enhanced/0.1.74/json_enhanced-0.1.74-py3-none-any.whl

---
+++ packages/json-enhanced/json_enhanced-0.1.74-py3-none-any.whl/jsonutils/config/__init__.py
@@ -0,0 +1,9 @@
+from .completion import autocomplete_only_nodes
+from .locals import decimal_separator, thousands_separator
+from .queries import (
+    clever_parsing,
+    include_parents,
+    native_types,
+    query_exceptions,
+    recursive_queries,
+)
