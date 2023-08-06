a5011ab37bbb3c72bfa795a924d46e6a831a3091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package pantsbuild.pants/2.9.0.dev3/pantsbuild.pants-2.9.0.dev3-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.9.0.dev3-cp39-cp39-macosx_11_0_arm64.whl/pants/backend/scala/compile/scalac.py
@@ -0,0 +1,214 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import logging
+from itertools import chain
+
+from pants.backend.java.target_types import JavaFieldSet, JavaGeneratorFieldSet, JavaSourceField
+from pants.backend.scala.compile.scala_subsystem import ScalaSubsystem
+from pants.backend.scala.target_types import ScalaFieldSet, ScalaGeneratorFieldSet, ScalaSourceField
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.fs import EMPTY_DIGEST, AddPrefix, Digest, MergeDigests
+from pants.engine.process import BashBinary, FallibleProcessResult, Process
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.target import SourcesField
+from pants.engine.unions import UnionMembership, UnionRule
+from pants.jvm.compile import (
+    ClasspathEntry,
+    ClasspathEntryRequest,
+    CompileResult,
+    FallibleClasspathEntry,
+)
+from pants.jvm.compile import rules as jvm_compile_rules
+from pants.jvm.jdk_rules import JdkSetup
+from pants.jvm.resolve.coursier_fetch import (
+    ArtifactRequirements,
+    Coordinate,
+    MaterializedClasspath,
+    MaterializedClasspathRequest,
+)
+from pants.jvm.resolve.coursier_setup import Coursier
+from pants.util.logging import LogLevel
+
+logger = logging.getLogger(__name__)
+
+
+class CompileScalaSourceRequest(ClasspathEntryRequest):
+    field_sets = (ScalaFieldSet, ScalaGeneratorFieldSet)
+    field_sets_consume_only = (JavaFieldSet, JavaGeneratorFieldSet)
+
+
+@rule(desc="Compile with scalac")
+async def compile_scala_source(
+    bash: BashBinary,
+    coursier: Coursier,
+    jdk_setup: JdkSetup,
+    scala: ScalaSubsystem,
+    union_membership: UnionMembership,
+    request: CompileScalaSourceRequest,
+) -> FallibleClasspathEntry:
+    # Request classpath entries for our direct dependencies.
+    direct_dependency_classpath_entries = FallibleClasspathEntry.if_all_succeeded(
+        await MultiGet(
+            Get(
+                FallibleClasspathEntry,
+                ClasspathEntryRequest,
+                ClasspathEntryRequest.for_targets(
+                    union_membership, component=coarsened_dep, resolve=request.resolve
+                ),
+            )
+            for coarsened_dep in request.component.dependencies
+        )
+    )
+    if direct_dependency_classpath_entries is None:
+        return FallibleClasspathEntry(
+            description=str(request.component),
+            result=CompileResult.DEPENDENCY_FAILED,
+            output=None,
+            exit_code=1,
+        )
+
+    component_members_with_sources = tuple(
+        t for t in request.component.members if t.has_field(SourcesField)
+    )
+    component_members_and_source_files = zip(
+        component_members_with_sources,
+        await MultiGet(
+            Get(
+                SourceFiles,
+                SourceFilesRequest(
+                    (t.get(SourcesField),),
+                    for_sources_types=(ScalaSourceField, JavaSourceField),
+                    enable_codegen=True,
+                ),
+            )
+            for t in component_members_with_sources
+        ),
+    )
+
+    component_members_and_scala_source_files = [
+        (target, sources)
+        for target, sources in component_members_and_source_files
+        if sources.snapshot.digest != EMPTY_DIGEST
+    ]
+
+    if not component_members_and_scala_source_files:
+        # Is a generator, and so exports all of its direct deps.
+        exported_digest = await Get(
+            Digest, MergeDigests(cpe.digest for cpe in direct_dependency_classpath_entries)
+        )
+        classpath_entry = ClasspathEntry.merge(exported_digest, direct_dependency_classpath_entries)
+        return FallibleClasspathEntry(
+            description=str(request.component),
+            result=CompileResult.SUCCEEDED,
+            output=classpath_entry,
+            exit_code=0,
+        )
+
+    (tool_classpath, merged_transitive_dependency_classpath_entries_digest,) = await MultiGet(
+        Get(
+            MaterializedClasspath,
+            MaterializedClasspathRequest(
+                prefix="__toolcp",
+                artifact_requirements=(
+                    ArtifactRequirements(
+                        [
+                            Coordinate(
+                                group="org.scala-lang",
+                                artifact="scala-compiler",
+                                version=scala.version,
+                            ),
+                            Coordinate(
+                                group="org.scala-lang",
+                                artifact="scala-library",
+                                version=scala.version,
+                            ),
+                        ]
+                    ),
+                ),
+            ),
+        ),
+        Get(
+            Digest,
+            # Flatten the entire transitive classpath.
+            MergeDigests(
+                classfiles.digest
+                for classfiles in ClasspathEntry.closure(direct_dependency_classpath_entries)
+            ),
+        ),
+    )
+
+    usercp = "__cp"
+    prefixed_transitive_dependency_classpath_digest = await Get(
+        Digest, AddPrefix(merged_transitive_dependency_classpath_entries_digest, usercp)
+    )
+
+    merged_tool_digest, merged_input_digest = await MultiGet(
+        Get(Digest, MergeDigests((tool_classpath.digest, jdk_setup.digest))),
+        Get(
+            Digest,
+            MergeDigests(
+                (
+                    prefixed_transitive_dependency_classpath_digest,
+                    *(
+                        sources.snapshot.digest
+                        for _, sources in component_members_and_scala_source_files
+                    ),
+                )
+            ),
+        ),
+    )
+
+    classpath_arg = ClasspathEntry.arg(
+        ClasspathEntry.closure(direct_dependency_classpath_entries), prefix=usercp
+    )
+
+    output_file = f"{request.component.representative.address.path_safe_spec}.scalac.jar"
+    process_result = await Get(
+        FallibleProcessResult,
+        Process(
+            argv=[
+                *jdk_setup.args(bash, tool_classpath.classpath_entries()),
+                "scala.tools.nsc.Main",
+                "-bootclasspath",
+                ":".join(tool_classpath.classpath_entries()),
+                *(("-classpath", classpath_arg) if classpath_arg else ()),
+                "-d",
+                output_file,
+                *sorted(
+                    chain.from_iterable(
+                        sources.snapshot.files
+                        for _, sources in component_members_and_scala_source_files
+                    )
+                ),
+            ],
+            input_digest=merged_input_digest,
+            use_nailgun=merged_tool_digest,
+            output_files=(output_file,),
+            description=f"Compile {request.component} with scalac",
+            level=LogLevel.DEBUG,
+            append_only_caches=jdk_setup.append_only_caches,
+            env=jdk_setup.env,
+        ),
+    )
+    output: ClasspathEntry | None = None
+    if process_result.exit_code == 0:
+        output = ClasspathEntry(
+            process_result.output_digest, (output_file,), direct_dependency_classpath_entries
+        )
+
+    return FallibleClasspathEntry.from_fallible_process_result(
+        str(request.component),
+        process_result,
+        output,
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        *jvm_compile_rules(),
+        UnionRule(ClasspathEntryRequest, CompileScalaSourceRequest),
+    ]
---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.9.0.dev3-cp39-cp39-macosx_11_0_arm64.whl/pants/jvm/target_types.py
@@ -0,0 +1,167 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from pants.engine.target import (
+    COMMON_TARGET_FIELDS,
+    FieldSet,
+    SingleSourceField,
+    SpecialCasedDependencies,
+    StringField,
+    StringSequenceField,
+    Target,
+)
+from pants.util.docutil import git_url
+
+_DEFAULT_PACKAGE_MAPPING_URL = git_url(
+    "src/python/pants/jvm/dependency_inference/jvm_artifact_mappings.py"
+)
+
+
+class JvmArtifactGroupField(StringField):
+    alias = "group"
+    required = True
+    help = (
+        "The 'group' part of a Maven-compatible coordinate to a third-party jar artifact. For the jar coordinate "
+        "com.google.guava:guava:30.1.1-jre, the group is 'com.google.guava'."
+    )
+
+
+class JvmArtifactArtifactField(StringField):
+    alias = "artifact"
+    required = True
+    help = (
+        "The 'artifact' part of a Maven-compatible coordinate to a third-party jar artifact. For the jar coordinate "
+        "com.google.guava:guava:30.1.1-jre, the artifact is 'guava'."
+    )
+
+
+class JvmArtifactVersionField(StringField):
+    alias = "version"
+    required = True
+    help = (
+        "The 'version' part of a Maven-compatible coordinate to a third-party jar artifact. For the jar coordinate "
+        "com.google.guava:guava:30.1.1-jre, the version is '30.1.1-jre'."
+    )
+
+
+class JvmArtifactUrlField(StringField):
+    alias = "url"
+    required = False
+    help = (
+        "A URL that points to the location of this artifact. If specified, Pants will not fetch this artifact "
+        "from default maven repositories, and instead fetch the artifact from this URL. To use default maven "
+        "repositories, do not set this value. \n\nNote that `file:` URLs are not presently supported."
+    )
+
+
+class JvmArtifactPackagesField(StringSequenceField):
+    alias = "packages"
+    help = (
+        "The JVM packages this artifact provides for the purposes of dependency inference.\n\n"
+        'For example, the JVM artifact `junit:junit` might provide `["org.junit.**"]`.\n\n'
+        "Usually you can leave this field off. If unspecified, Pants will fall back to the "
+        "`[java-infer].third_party_import_mapping`, then to a built in mapping "
+        f"({_DEFAULT_PACKAGE_MAPPING_URL}), and then finally it will default to "
+        "the normalized `group` of the artifact. For example, in the absence of any other mapping "
+        "the artifact `io.confluent:common-config` would default to providing "
+        '`["io.confluent.**"]`.\n\n'
+        "The package path may be made recursive to match symbols in subpackages "
+        'by adding `.**` to the end of the package path. For example, specify `["org.junit.**"]` '
+        "to infer a dependency on the artifact for any file importing a symbol from `org.junit` or "
+        "its subpackages."
+    )
+
+
+class JvmProvidesTypesField(StringSequenceField):
+    alias = "experimental_provides_types"
+    help = (
+        "Signals that the specified types should be fulfilled by these source files during "
+        "dependency inference. This allows for specific types within packages that are otherwise "
+        "inferred as belonging to `jvm_artifact` targets to be unambiguously inferred as belonging "
+        "to this first-party source. If a given type is defined, at least one source file captured "
+        "by this target must actually provide that symbol."
+    )
+
+
+class JvmArtifactFieldSet(FieldSet):
+
+    group: JvmArtifactGroupField
+    artifact: JvmArtifactArtifactField
+    version: JvmArtifactVersionField
+    packages: JvmArtifactPackagesField
+    url: JvmArtifactUrlField
+
+    required_fields = (
+        JvmArtifactGroupField,
+        JvmArtifactArtifactField,
+        JvmArtifactVersionField,
+        JvmArtifactPackagesField,
+    )
+
+
+class JvmArtifact(Target):
+    alias = "jvm_artifact"
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        *JvmArtifactFieldSet.required_fields,
+        JvmArtifactUrlField,  # TODO: should `JvmArtifactFieldSet` have an `all_fields` field?
+    )
+    help = (
+        "Represents a third-party JVM artifact as identified by its Maven-compatible coordinate, "
+        "that is, its `group`, `artifact`, and `version` components."
+    )
+
+
+class JvmCompatibleResolveNamesField(StringSequenceField):
+    alias = "compatible_resolves"
+    required = False
+    help = (
+        "The set of resolve names that this target is compatible with. Any targets which depend on "
+        "one another must have at least one compatible resolve in common. Which resolves are actually "
+        "used in a build is calculated based on a target's dependees."
+    )
+
+
+class JvmResolveName(StringField):
+    alias = "resolve"
+    required = False
+    help = (
+        "The name of the resolve to use when building this target. The name must be defined as "
+        "one of the resolves in `--jvm-resolves`. If not supplied, the default resolve will be "
+        "used, otherwise, one resolve that is compatible with all dependency targets will be used."
+    )
+
+
+class JvmRequirementsField(SpecialCasedDependencies):
+    alias = "requirements"
+    required = True
+    help = (
+        "A sequence of addresses to targets compatible with `jvm_artifact` that specify the coordinates for "
+        "third-party JVM dependencies."
+    )
+
+
+class JvmLockfileSources(SingleSourceField):
+    expected_file_extensions = (".lockfile",)
+    # Expect 0 or 1 files.
+    expected_num_files = range(0, 2)
+    required = False
+    help = (
+        "A single Pants Coursier Lockfile source.\n\n"
+        "Use `./pants coursier-resolve ...` to generate (or regenerate) the Lockfile."
+        " If the Lockfile doesn't exist on disk, the first run of `coursier-resolve` will attempt"
+        " to generate it for you to the default file name ('coursier_resolve.lockfile')."
+        " After running `coursier-resolve` for the first time, you should update this field's"
+        "`sources` to explicit take ownership of the generated lockfile."
+    )
+
+
+class JvmDependencyLockfile(Target):
+    alias = "coursier_lockfile"
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        JvmLockfileSources,
+    )
+    help = "A Coursier lockfile along with references to the artifacts to use for the lockfile."
