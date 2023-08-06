70d0ab7766a8b748970a0f121c089c60b24a692c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:35 +0000

    Add package openseesboard/0.0.1/openseesboard-0.0.1-py3-none-any.whl

---
+++ packages/openseesboard/openseesboard-0.0.1-py3-none-any.whl/square/__main__.py
@@ -0,0 +1,8 @@
+# This is a test command
+def main():
+	number = int(input('Enter the number (only integers allowed\n'))
+	print(f'{number} squares in {number**2}')
+
+
+if __name__ == '__main__':
+	main()
