7766127e4cf84552efd72eb9de59d7d23627498f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:42 +0000

    Add package covid19-dashboard-pkg-hwhittle/0.0.5/covid19-dashboard-pkg-hwhittle-0.0.5.tar.gz

---
+++ packages/covid19-dashboard-pkg-hwhittle/covid19-dashboard-pkg-hwhittle-0.0.5.tar.gz/covid19-dashboard-pkg-hwhittle-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covid19-dashboard-pkg-hwhittle",
+    version="0.0.5",
+    author="Harry Whittle",
+    author_email="harry.whittle@cwgsy.net",
+    description="A personalisable UK COVID-19 Dashboard used for demonstration purposes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itselectroz/covid-dashboard-coursework",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
