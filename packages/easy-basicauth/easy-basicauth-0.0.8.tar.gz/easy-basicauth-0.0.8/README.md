420ed47237a2d521cc78393770b9036c395a3fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package easy-basicauth/0.0.8/easy-basicauth-0.0.8.tar.gz

---
+++ packages/easy-basicauth/easy-basicauth-0.0.8.tar.gz/easy-basicauth-0.0.8/README.md
@@ -0,0 +1,12 @@
+# easy-basicauth
+
+WARNING! THIS LIBRARY IS IN PROGRESS! ANYTHING CAN CHANGE AT ANY MOMENT WITHOUT ANY NOTICE!
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+
+## License
+[MIT](https://github.com/bichanna/django-basicauth/blob/master/LICENSE)
---
+++ packages/easy-basicauth/easy-basicauth-0.0.8.tar.gz/easy-basicauth-0.0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = "0.0.8"
+DESCRIPTION = 'Basic auth for Django'
+README = os.path.join(os.path.dirname(__file__), "README.md")
+
+# Setting up
+setup(
+        name="easy-basicauth",
+        version=VERSION,
+        author="bichanna",
+        author_email="nobu.bichanna@gmail.com",
+        description=DESCRIPTION,
+        long_description=open(README).read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/bichanna/django-basic-auth",
+        packages=find_packages(),
+        install_requires=[
+            "django>=1.8.0",
+			"djangorestframework>=3.0.0",
+        ],
+		license="MIT",
+        keywords=["basic-auth", "python", "django", "api"],
+        classifiers= [
+            'Framework :: Django',
+            "Development Status :: 3 - Alpha",
+			"Programming Language :: Python :: 3",
+        ],
+)
