fc033a204c5e768e372fe52e19179db102490075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:14 +0000

    Add package odoo14-addon-sale-limit-partner-address/14.0.0.0.1/odoo14_addon_sale_limit_partner_address-14.0.0.0.1-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-limit-partner-address/odoo14_addon_sale_limit_partner_address-14.0.0.0.1-py3-none-any.whl/odoo/addons/sale_limit_partner_address/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Sale Limit Partner Address',
+    'summary': "Limit invoice and shipping address to childs of main partner",
+    'version': '14.0.0.0.1',
+    'depends': ['purchase'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Limit invoice and shipping address to childs of main partner
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "views/sale_order_view.xml",
+    ]
+
+}
