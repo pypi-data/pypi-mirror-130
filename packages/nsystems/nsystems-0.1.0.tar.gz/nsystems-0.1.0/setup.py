b04ad7a29f352756f9451cc97814d69077798d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:03 +0000

    Add package nsystems/0.1.0/nsystems-0.1.0.tar.gz

---
+++ packages/nsystems/nsystems-0.1.0.tar.gz/nsystems-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='nsystems',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Convert numbers between different numeral systems.',
+    author='nsystems',
+    author_email='nsystems.module@gmail.com',
+    license='MIT',
+    keywords=['number', 'systems', 'system', 'base', 'base64', 'base32', 'basen', 'convert', 'number systems', 'number system', 'binary', 'hexadecimal', 'octal', 'decimal'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
