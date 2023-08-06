5e7d5a9d2627e1642f4b0340fa44999438df541d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:26 +0000

    Add package fake-bpy-module-2.82/20211212/fake_bpy_module_2.82-20211212-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.82/fake_bpy_module_2.82-20211212-py3-none-any.whl/bpy/ops/__init__.py
@@ -0,0 +1,106 @@
+import sys
+import typing
+from . import info
+from . import cloth
+from . import wm
+from . import sound
+from . import import_curve
+from . import uv
+from . import mball
+from . import cachefile
+from . import transform
+from . import ed
+from . import nla
+from . import fluid
+from . import palette
+from . import preferences
+from . import material
+from . import constraint
+from . import text
+from . import paintcurve
+from . import ptcache
+from . import brush
+from . import sculpt
+from . import pose
+from . import camera
+from . import workspace
+from . import curve
+from . import node
+from . import graph
+from . import font
+from . import import_anim
+from . import export_mesh
+from . import dpaint
+from . import clip
+from . import poselib
+from . import screen
+from . import file
+from . import import_scene
+from . import boid
+from . import export_anim
+from . import mesh
+from . import texture
+from . import lattice
+from . import image
+from . import cycles
+from . import anim
+from . import buttons
+from . import import_mesh
+from . import marker
+from . import render
+from . import action
+from . import particle
+from . import armature
+from . import script
+from . import object
+from . import surface
+from . import collection
+from . import ui
+from . import gpencil
+from . import mask
+from . import export_scene
+from . import scene
+from . import paint
+from . import outliner
+from . import view3d
+from . import view2d
+from . import gizmogroup
+from . import rigidbody
+from . import sequencer
+from . import console
+from . import safe_areas
+from . import world
+
+
+class BPyOps:
+    pass
+
+
+class BPyOpsSubMod:
+    pass
+
+
+class BPyOpsSubModOp:
+    def get_rna_type(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname_py(self):
+        ''' 
+
+        '''
+        pass
+
+    def poll(self, args):
+        ''' 
+
+        '''
+        pass
