7f2226a7718cc2eb8785f4c7310b77838bc82ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:13 +0000

    Add package ncspy/0.2.3.1/ncspy-0.2.3.1.tar.gz

---
+++ packages/ncspy/ncspy-0.2.3.1.tar.gz/ncspy-0.2.3.1/README.md
@@ -0,0 +1,6 @@
+# pyNCS
+
+A Python NoCopyrightSounds "API" wrapper written with asyncio.
+
+## This isn't actually an API wrapper
+This module just parses NCS' HTML page, because NCS does not have public APIs (at least not yet).
---
+++ packages/ncspy/ncspy-0.2.3.1.tar.gz/ncspy-0.2.3.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ncspy",
+    version="0.2.3.1",
+    author="Vincy.zsh",
+    author_email="Vincysuper07@gmail.com",
+    description='A NoCopyrightSounds "API" wrapper written with asyncio.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vincydotzsh/pyncs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["aiohttp", "beautifulsoup4", "slimit"],
+    packages=["ncs"],
+    python_requires=">=3.7",
+)
