2dbffc96002912a573c78f7ad601a4a0f9f7346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:12 +0000

    Add package gethome/0.1.6/gethome-0.1.6.tar.gz

---
+++ packages/gethome/gethome-0.1.6.tar.gz/gethome-0.1.6/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+"""
+pip install --editable .
+表示从本地或开发者模式安装项目，
+-e, --editable <路径/网址>
+
+521xiaoJIANG
+"""
+pwd = os.path.dirname(__file__)
+with open(os.path.join(pwd, 'README.md')) as f:
+  README = f.read()
+
+setup(
+  name='gethome',
+  version='0.1.6',
+  description='Xiao shen demo',
+  long_description=README,
+  long_description_content_type='text/markdown',
+  py_modules=find_packages('cli1'),  # 找到包的路径
+  package_dir={
+    '': 'cli1',
+  },
+  install_requires=[
+    'Click',
+  ],
+  entry_points={'console_scripts': [
+    'gohome=hello:hello',  # 对应包里面的文件，文件厘米的方法
+    'gethome=hello:gethome'
+  ]},
+)
