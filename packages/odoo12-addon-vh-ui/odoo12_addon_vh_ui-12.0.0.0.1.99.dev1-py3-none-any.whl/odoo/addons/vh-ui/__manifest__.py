192d765c00e87e50438d373b3b54433ae7411471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:58 +0000

    Add package odoo12-addon-vh-ui/12.0.0.0.1.99.dev1/odoo12_addon_vh_ui-12.0.0.0.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-vh-ui/odoo12_addon_vh_ui-12.0.0.0.1.99.dev1-py3-none-any.whl/odoo/addons/vh-ui/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vertical-habitatge-ui",
+
+    'summary': """
+        UI customizations for habitatge.""",
+
+    'author': "Coopdevs",
+    'website': "http://coopdevs.org",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'vertical-habitatge',
+    'version': '12.0.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+    ],
+}
