e95ffb646b7f85c704a8f9ef9e21b29bd8881ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:17 +0000

    Add package wayremap/0.0.1/wayremap-0.0.1.tar.gz

---
+++ packages/wayremap/wayremap-0.0.1.tar.gz/wayremap-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wayremap']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['evdev>=1.4.0,<2.0.0', 'i3ipc>=2.2.1,<3.0.0', 'python-uinput>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'wayremap',
+    'version': '0.0.1',
+    'description': 'A dynamic keyboard remapper for Wayland.',
+    'long_description': None,
+    'author': 'Kay Gosho',
+    'author_email': 'ketsume0211@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/wayremap/wayremap-0.0.1.tar.gz/wayremap-0.0.1/wayremap/config.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from keycode_map import keycode_map
+import uinput as k
+
+
+@dataclass
+class Binding:
+    remap: str
+    to: list[list[tuple[int, int]]]
+
+    def get_remap_keycode(self) -> str:
+        """
+        @type evdev.ecodes.KEY_F
+        """
+        _k = self.remap.split('.')
+        key_name = _k[len(_k) - 1]
+        return keycode_map[key_name]
+
+    def only_ctrl(self) -> bool:
+        return 'ctrl' in self.remap and not 'alt' in self.remap
+
+    def only_alt(self) -> bool:
+        return 'alt' in self.remap and not 'ctrl' in self.remap
+
+    def require_ctrl_alt(self) -> bool:
+        return 'alt' in self.remap and 'ctrl' in self.remap
+
+
+@dataclass
+class WayremapConfig:
+    applications: list[str]
+    bindings: list[Binding]
+
+
+example_config = WayremapConfig(
+    applications=[
+        'Brave-browser',
+        'Leafpad',
+        'firefoxdeveloperedition',
+    ],
+    bindings=[
+        # Emacs-like key binding
+        Binding('ctrl.alt.a', [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
+        Binding('ctrl.alt.e', [[k.KEY_LEFTCTRL, k.KEY_END]]),
+        Binding('ctrl.alt.h', [[k.KEY_LEFTCTRL, k.KEY_BACKSPACE]]),
+        Binding('ctrl.f', [[k.KEY_RIGHT]]),
+        Binding('ctrl.b', [[k.KEY_LEFT]]),
+        Binding('ctrl.p', [[k.KEY_UP]]),
+        Binding('ctrl.n', [[k.KEY_DOWN]]),
+        Binding('ctrl.k',
+                [[k.KEY_LEFTSHIFT, k.KEY_END], [k.KEY_LEFTCTRL, k.KEY_X]]),
+        Binding('ctrl.a', [[k.KEY_HOME]]),
+        Binding('ctrl.e', [[k.KEY_END]]),
+        Binding('ctrl.y', [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding('alt.f', [[k.KEY_LEFTCTRL, k.KEY_RIGHT]]),
+        Binding('alt.b', [[k.KEY_LEFTCTRL, k.KEY_LEFT]]),
+        Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
+        Binding('ctrl.h', [[k.KEY_BACKSPACE]]),
+        Binding('ctrl.s', [[k.KEY_LEFTCTRL, k.KEY_F]]),
+
+        # OSX-like key binding
+        Binding('alt.a', [[k.KEY_LEFTCTRL, k.KEY_A]]),
+        Binding('alt.c', [[k.KEY_LEFTCTRL, k.KEY_C]]),
+        Binding('alt.v', [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_X]]),
+    ])
