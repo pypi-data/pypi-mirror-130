9e7394e53aa00f26e53d50dc7289aa557b72d862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:18 +0000

    Add package showcode-forge/0.9/showcode_forge-0.9.tar.gz

---
+++ packages/showcode-forge/showcode_forge-0.9.tar.gz/showcode_forge-0.9/showcode_forge/compilers/compilers.py
@@ -0,0 +1,27 @@
+from .pytest_compiler import pytest_compiles
+import json
+
+
+def load_html(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+def compile(args):
+
+    rubric = load_html(args.question_file)
+
+    if args.language == "py" and args.framework == "pytest_scforge":
+        data = pytest_compiles(args)
+    else:
+        raise Exception(f"Compilation is not yet supported for {args.language} {args.framework}")
+
+    with open(args.output, "w") as f:
+        json.dump({
+            "title": "",
+            "difficulty": 1,
+            "className": "",
+            "methodName": "",
+            "rubric": rubric,
+            **data,
+        }, f, indent=4)
