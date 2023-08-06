2dd3a2be751419de5ec192ac64d33306915894ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:58 +0000

    Add package FeatureCloud/0.0.4/FeatureCloud-0.0.4.tar.gz

---
+++ packages/FeatureCloud/FeatureCloud-0.0.4.tar.gz/FeatureCloud-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="FeatureCloud",
+                 version="0.0.4",
+                 author="FeatureCloud",
+                 author_email="mohammad.bakhtiari@uni-hamburg.de",
+                 description="Secure Federated Learning Platform",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/FeatureCloud/app-template",
+                 project_urls={
+                     "Bug Tracker": "https://github.com/FeatureCloud/app-template/issues",
+                 },
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "Operating System :: OS Independent",
+                 ],
+                 # package_dir={"": "FeatureCloud"},
+                 # packages=setuptools.find_packages(where="FeatureCloud"),
+                 packages=['FeatureCloud.apps', 'FeatureCloud.engine', 'FeatureCloud.api'],
+                 python_requires=">=3.7",
+                 entry_points={'console_scripts': ['FeatureCloud = FeatureCloud.__main__:featurecloud',
+                                                   ]
+                               },
+
+                 )
