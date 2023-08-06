1b8e916d12716fe73d4e0d51ed0b9e8bf27663b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:35 +0000

    Add package Hotel/1.1/Hotel-1.1.tar.gz

---
+++ packages/Hotel/Hotel-1.1.tar.gz/Hotel-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Hotel',
+    version='1.1',
+    packages=["Hotel", "Hotel.Person", "Hotel.Service"],
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/anqiubc/533-lab4',
+    author=['Anqi Li','Evelyn Sugihermanto'],
+    author_email='lianqi20209@gmail.com, evelynsugihermanto@yahoo.com'
+)
