5f3c8fd8ef076a19b9fcce776719b07d23049400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:31 +0000

    Add package mlprodict/0.7.1649/mlprodict-0.7.1649-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/mlprodict/mlprodict-0.7.1649-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mlprodict/tools/__init__.py
@@ -0,0 +1,8 @@
+"""
+@file
+@brief Shortcuts to tools.
+"""
+
+from .asv_options_helper import get_opset_number_from_onnx, get_ir_version_from_onnx
+from .code_helper import change_style
+from .speed_measure import measure_time
