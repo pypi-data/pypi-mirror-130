2705991a28381feefb7e8719041b8056ec85a471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:01 +0000

    Add package hbshare/2.0.8/hbshare-2.0.8.tar.gz

---
+++ packages/hbshare/hbshare-2.0.8.tar.gz/hbshare-2.0.8/hbshare/cta_factor/__init__.py
@@ -0,0 +1,9 @@
+from hbshare.cta_factor.factor_func import (
+    factor_table_gen,
+    load_factor,
+    get_last_position,
+    factor_stats,
+    factor_plot,
+    factor_compute
+)
+from hbshare.cta_factor.factor_algo import carry, tsmom, tswr, tsrev, xsrev, xsmom, xswr
---
+++ packages/hbshare/hbshare-2.0.8.tar.gz/hbshare-2.0.8/hbshare/cta_factor/run_cta_factor.py
@@ -0,0 +1,42 @@
+if __name__ == '__main__':
+    from db_cons import sql_write_path_work, sql_user_work
+    from hbshare.cta_factor.hsjy_to_local import (
+        hsjy_fut_pro_info,
+        hsjy_fut_wr,
+        hsjy_fut_com_info,
+        hsjy_fut_com,
+        hsjy_fut_member,
+    )
+    from hbshare.cta_factor.hsjy_func import hsjy_fut_index
+    from hbshare.cta_factor.factor_index import run
+    from hbshare.cta_factor.factor_func import index_gen
+
+    hsjy_fut_pro_info(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    hsjy_fut_wr(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    hsjy_fut_com_info(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    hsjy_fut_com(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    hsjy_fut_member(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    hsjy_fut_index(
+        db_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
+    run(sql_path=sql_write_path_work['daily'])
+    index_gen(
+        sql_path=sql_write_path_work['daily'],
+        sql_info=sql_user_work
+    )
