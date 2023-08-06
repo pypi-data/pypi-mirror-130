553d44a82bb9af025fb6c7ab102800e82eb78e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:01 +0000

    Add package b2d/0.4.0/b2d-0.4.0-cp310-cp310-win32.whl

---
+++ packages/b2d/b2d-0.4.0-cp310-cp310-win32.whl/b2d/testbed/backend/no_gui/no_gui.py
@@ -0,0 +1,54 @@
+import b2d 
+
+class EmptyDebugDraw(b2d.batch_debug_draw_cls(False, True, True)):
+
+    def __init__(self):
+        super(EmptyDebugDraw,self).__init__()
+
+
+    def draw_solid_polygons(self, points, sizes, colors):
+        pass
+
+    def draw_polygons(self, points, sizes, colors):
+        pass
+
+    def draw_solid_circles(self, centers, radii, axis, colors):
+        pass
+
+    def draw_circles(self, centers, radii, colors):
+        pass
+
+    def _draw_circles(self, centers, radii,  colors, lw):
+        pass
+
+    def draw_points(self, centers, sizes, colors):
+        pass
+
+    def draw_segments(self, points, colors):
+        pass
+
+    def draw_particles(self, centers, radius, colors=None):
+        pass
+
+
+class NoGui(object):
+    def __init__(self, testbed_cls, settings, testbed_kwargs=None):
+        
+        self.testbed_cls = testbed_cls
+        self.testbed_kwargs = testbed_kwargs
+        self._testworld = None
+
+        self._dt = settings.get('dt', 1.0/40.0)
+        self._t = settings.get('t',10)
+        self._n = int(0.5 + self._t / self._dt) 
+        self.debug_draw = EmptyDebugDraw()
+        
+
+    # run the world for a limited amount of steps
+    def start_ui(self):
+        
+        self._testworld = self.testbed_cls(**self.testbed_kwargs)
+        self._testworld.set_debug_draw(self.debug_draw)
+
+        for i in range(self._n):
+            self._testworld.step(self._dt)
