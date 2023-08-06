4c85b59100d44ffb7463012ed8134b5b1c3fe597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:44 +0000

    Add package stemdiff/4.2.10/stemdiff-4.2.10-py3-none-any.whl

---
+++ packages/stemdiff/stemdiff-4.2.10-py3-none-any.whl/stemdiff/__init__.py
@@ -0,0 +1,11 @@
+# Python package initialization file.
+
+'''
+STEMDIFF package
+----------------
+Convert a 4D-STEM dataset to a single powder diffraction pattern.
+'''
+
+__version__ = "4.2.10"
+
+
