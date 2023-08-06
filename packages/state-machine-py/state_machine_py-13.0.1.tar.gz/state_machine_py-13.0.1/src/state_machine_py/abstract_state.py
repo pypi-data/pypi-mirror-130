62518ca35b12f60ebb9390c5244d768d61a1ab13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:51 +0000

    Add package state-machine-py/13.0.1/state_machine_py-13.0.1.tar.gz

---
+++ packages/state-machine-py/state_machine_py-13.0.1.tar.gz/state_machine_py-13.0.1/src/state_machine_py/abstract_state.py
@@ -0,0 +1,58 @@
+class AbstractState():
+    """状態"""
+
+    def __init__(self):
+        pass
+
+    def entry(self, req):
+        """この状態に遷移したときに呼び出されます
+
+        Parameters
+        ----------
+        req : Request
+            ステートマシンからステートへ与えられる引数のまとまり
+
+        Examples
+        --------
+        req.intermachine.enqueue_myself("Login")
+
+        自分（ステートマシン）の入力キューに "Login" の文字を送ります
+        """
+        self.on_entry(req)
+
+    def on_entry(self, req):
+        """この状態に遷移したときに呼び出されます
+
+        Parameters
+        ----------
+        req : Request
+            ステートマシンからステートへ与えられる引数のまとまり
+        """
+        pass
+
+    def exit(self, req):
+        """この状態から抜け出たときに呼び出されます。ただし初期化時、アボート時は呼び出されません
+
+        Parameters
+        ----------
+        req : Request
+            ステートマシンからステートへ与えられる引数のまとまり
+
+        Returns
+        -------
+        str
+            次（下位）の辺の名前
+        """
+        self.on_exit(req)
+
+        return None
+
+    def on_exit(self, req):
+        """この状態から抜け出たときに呼び出されます。ただし初期化時、アボート時は呼び出されません
+
+        Parameters
+        ----------
+        req : Request
+            ステートマシンからステートへ与えられる引数のまとまり
+        """
+        pass
