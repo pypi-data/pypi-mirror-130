1db4775614707d8ef47990fd8e31fff42c3d814a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:18 +0000

    Add package grafana-wtf/0.11.1/grafana-wtf-0.11.1.tar.gz

---
+++ packages/grafana-wtf/grafana-wtf-0.11.1.tar.gz/grafana-wtf-0.11.1/README.rst
@@ -0,0 +1,169 @@
+.. image:: https://github.com/panodata/grafana-wtf/workflows/Tests/badge.svg
+    :target: https://github.com/panodata/grafana-wtf/actions?workflow=Tests
+
+.. image:: https://img.shields.io/pypi/pyversions/grafana-wtf.svg
+    :target: https://pypi.org/project/grafana-wtf/
+
+.. image:: https://img.shields.io/pypi/status/grafana-wtf.svg
+    :target: https://pypi.org/project/grafana-wtf/
+
+.. image:: https://img.shields.io/pypi/v/grafana-wtf.svg
+    :target: https://pypi.org/project/grafana-wtf/
+
+.. image:: https://img.shields.io/pypi/dm/grafana-wtf.svg
+    :target: https://pypi.org/project/grafana-wtf/
+
+.. image:: https://img.shields.io/pypi/l/grafana-wtf.svg
+    :target: https://github.com/panodata/grafana-wtf/blob/main/LICENSE
+
+.. image:: https://img.shields.io/badge/Grafana-6.x%20--%208.x-blue.svg
+    :target: https://github.com/grafana/grafana
+    :alt: Supported Grafana versions
+
+|
+
+###########
+grafana-wtf
+###########
+
+
+*****
+About
+*****
+grafana-wtf - grep through all Grafana entities in the spirit of `git-wtf`_.
+
+.. _git-wtf: http://thrawn01.org/posts/2014/03/03/git-wtf/
+
+.. attention::
+
+    This program can put significant load on your Grafana instance
+    and the underlying database machinery. Handle with care!
+
+
+********
+Synopsis
+********
+Search Grafana API for string "weatherbase".
+::
+
+    grafana-wtf find weatherbase
+
+Display 50 most recent changes across all dashboards.
+::
+
+    grafana-wtf log --number=50
+
+
+***********
+Screenshots
+***********
+
+``grafana-wtf find``
+====================
+.. image:: https://user-images.githubusercontent.com/453543/51694547-5c78fd80-2001-11e9-96ea-3fcc2e0fb016.png
+
+``grafana-wtf log``
+===================
+.. image:: https://user-images.githubusercontent.com/453543/56455736-87ee5880-6362-11e9-8cd2-c356393d09c4.png
+
+
+*****
+Setup
+*****
+
+Install ``grafana-wtf``
+=======================
+::
+
+    pip install grafana-wtf
+
+
+Configure Grafana
+=================
+Please take these steps to create an API key with your Grafana instance:
+
+- Go to ``https://daq.example.org/grafana/org/apikeys``.
+
+- Choose "New API Key".
+
+  - Key name: grafana-wtf
+  - Role: Admin
+
+- From the output ``curl -H "Authorization: Bearer eyJrIjoiWHg...dGJpZCI6MX0=" ...``,
+  please take note of the Bearer token. This is your Grafana API key.
+
+
+*****
+Usage
+*****
+
+Before running ``grafana-wtf``, define URL and access token of your Grafana instance::
+
+    export GRAFANA_URL=https://daq.example.org/grafana/
+    export GRAFANA_TOKEN=eyJrIjoiWHg...dGJpZCI6MX0=
+
+
+Searching for strings
+=====================
+
+Find the string ``weatherbase`` throughout all dashboards and data sources::
+
+    grafana-wtf find weatherbase
+
+.. note::
+
+    ``grafana-wtf`` will cache HTTP responses for 300 seconds by default.
+    When running it with the ``--drop-cache`` option, it will drop its cache upfront.
+
+
+Replacing strings
+=================
+
+Replace all occurrences of ``ldi_v2`` with ``ldi_v3`` within dashboard with
+UID ``_JJ22OZZk``::
+
+    grafana-wtf --select-dashboard=_JJ22OZZk replace ldi_v2 ldi_v3
+
+
+Displaying changes / revision logs
+==================================
+
+Watching out for recent editing activity on any dashboards?::
+
+    # Display 50 most recent changes across all dashboards.
+    grafana-wtf log --number=50
+
+
+Explore data sources
+====================
+
+How to find unused data sources?::
+
+    # Display all data sources and the dashboards using them, as well as unused data sources.
+    grafana-wtf datasource-breakdown --format=yaml
+
+    # Display names of unused datasources as a flat list.
+    grafana-wtf datasource-breakdown --format=json | jq -r '.unused[].datasource.name'
+
+
+
+********
+Examples
+********
+
+For discovering more command line parameters and their arguments, please invoke
+``grafana-wtf --help`` and have a look at `grafana-wtf examples`_.
+
+
+
+***********
+Development
+***********
+::
+
+    git clone https://github.com/panodata/grafana-wtf
+    cd grafana-wtf
+    make test
+
+
+.. _grafana-wtf examples: https://github.com/panodata/grafana-wtf/blob/master/doc/examples.rst
---
+++ packages/grafana-wtf/grafana-wtf-0.11.1.tar.gz/grafana-wtf-0.11.1/grafana_wtf/core.py
@@ -0,0 +1,382 @@
+# -*- coding: utf-8 -*-
+# (c) 2019 Andreas Motl <andreas@hiveeyes.org>
+# License: GNU Affero General Public License, Version 3
+import json
+from pprint import pprint
+
+import colored
+import logging
+import asyncio
+import requests
+import requests_cache
+from tqdm import tqdm
+from munch import Munch, munchify
+from collections import OrderedDict
+from urllib.parse import urlparse, urljoin
+from concurrent.futures.thread import ThreadPoolExecutor
+
+from grafana_wtf.model import DatasourceBreakdownItem
+from grafana_wtf.monkey import monkeypatch_grafana_api
+# Apply monkeypatch to grafana-api
+# https://github.com/m0nhawk/grafana_api/pull/85/files
+monkeypatch_grafana_api()
+
+from grafana_api.grafana_api import GrafanaClientError, GrafanaUnauthorizedError
+from grafana_api.grafana_face import GrafanaFace
+
+from grafana_wtf.util import JsonPathFinder
+
+log = logging.getLogger(__name__)
+
+
+class GrafanaSearch:
+
+    def __init__(self, grafana_url, grafana_token):
+        self.grafana_url = grafana_url
+        self.grafana_token = grafana_token
+
+        self.grafana = None
+        self.data = Munch(datasources=[], dashboard_list=[], dashboards=[])
+
+        self.finder = JsonPathFinder()
+
+        self.taqadum = None
+        self.concurrency = 5
+
+        self.debug = log.getEffectiveLevel() == logging.DEBUG
+        self.progressbar = not self.debug
+
+    def enable_cache(self, expire_after=300, drop_cache=False):
+        if expire_after is None:
+            log.info(f'Setting up response cache to never expire (infinite caching)')
+        else:
+            log.info(f'Setting up response cache to expire after {expire_after} seconds')
+        requests_cache.install_cache(expire_after=expire_after)
+        if drop_cache:
+            self.clear_cache()
+
+        return self
+
+    def clear_cache(self):
+        log.info(f'Clearing cache')
+        requests_cache.clear()
+
+    def enable_concurrency(self, concurrency):
+        self.concurrency = concurrency
+
+    @staticmethod
+    def grafana_client_factory(grafana_url, grafana_token=None):
+        url = urlparse(grafana_url)
+
+        # Grafana API Key auth
+        if grafana_token:
+            auth = grafana_token
+
+        # HTTP basic auth
+        else:
+            username = url.username or 'admin'
+            password = url.password or 'admin'
+            auth = (username, password)
+
+        grafana = GrafanaFace(
+            auth, protocol=url.scheme,
+            host=url.hostname, port=url.port, url_path_prefix=url.path.lstrip('/'))
+
+        return grafana
+
+    def setup(self):
+
+        self.grafana = self.grafana_client_factory(self.grafana_url, grafana_token=self.grafana_token)
+
+        # Configure a larger HTTP request pool.
+        # Todo: Review the pool settings and eventually adjust according to concurrency level or other parameters.
+        # https://urllib3.readthedocs.io/en/latest/advanced-usage.html#customizing-pool-behavior
+        # https://laike9m.com/blog/requests-secret-pool_connections-and-pool_maxsize,89/
+        adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100, max_retries=5, pool_block=True)
+        self.grafana.api.s.mount('http://', adapter)
+        self.grafana.api.s.mount('https://', adapter)
+
+        return self
+
+    def start_progressbar(self, total):
+        if self.progressbar:
+            self.taqadum = tqdm(total=total)
+
+    def search(self, expression):
+        log.info('Searching Grafana at "{}" for expression "{}"'.format(self.grafana_url, expression))
+
+        results = Munch(datasources=[], dashboard_list=[], dashboards=[])
+
+        # Check datasources
+        log.info('Searching data sources')
+        self.search_items(expression, self.data.datasources, results.datasources)
+
+        # Check dashboards
+        log.info('Searching dashboards')
+        self.search_items(expression, self.data.dashboards, results.dashboards)
+
+        return results
+
+    def replace(self, expression, replacement):
+        log.info(f'Replacing "{expression}" by "{replacement}" within Grafana at "{self.grafana_url}"')
+        for dashboard in self.data.dashboards:
+            payload_before = json.dumps(dashboard)
+            payload_after = payload_before.replace(expression, replacement)
+            if payload_before == payload_after:
+                log.info(f'No replacements for dashboard with uid "{dashboard.dashboard.uid}"')
+                continue
+            dashboard_new = json.loads(payload_after)
+            dashboard_new['message'] = f'grafana-wtf: Replaced "{expression}" by "{replacement}"'
+            self.grafana.dashboard.update_dashboard(dashboard=dashboard_new)
+
+    def log(self, dashboard_uid=None):
+        if dashboard_uid:
+            what = 'Grafana dashboard "{}"'.format(dashboard_uid)
+        else:
+            what = 'multiple Grafana dashboards'
+        log.info('Aggregating edit history for {what} at {url}'.format(what=what, url=self.grafana_url))
+
+        entries = []
+        for dashboard_meta in self.data.dashboard_list:
+            if dashboard_uid is not None and dashboard_meta['uid'] != dashboard_uid:
+                continue
+
+            #print(dashboard_meta)
+
+            dashboard_versions = self.get_dashboard_versions(dashboard_meta['id'])
+            for dashboard_revision in dashboard_versions:
+                entry = OrderedDict(
+                    datetime=dashboard_revision['created'],
+                    user=dashboard_revision['createdBy'],
+                    message=dashboard_revision['message'],
+                    folder=dashboard_meta.get('folderTitle'),
+                    title=dashboard_meta['title'],
+                    version=dashboard_revision['version'],
+                    url=urljoin(self.grafana_url, dashboard_meta['url'])
+                )
+                entries.append(entry)
+
+        return entries
+
+    def search_items(self, expression, items, results):
+        for item in items:
+            effective_item = None
+            if expression is None:
+                effective_item = munchify({'meta': {}, 'data': item})
+            else:
+                matches = self.finder.find(expression, item)
+                if matches:
+                    effective_item = munchify({'meta': {'matches': matches}, 'data': item})
+
+            if effective_item:
+                results.append(effective_item)
+
+    def scan(self):
+
+        # TODO: Folders?
+        # folders = self.grafana.folder.get_all_folders()
+        # print(folders)
+
+        self.scan_datasources()
+        self.scan_dashboards()
+
+    def scan_datasources(self):
+        log.info('Scanning datasources')
+        try:
+            self.data.datasources = munchify(self.grafana.datasource.list_datasources())
+            log.info('Found {} data sources'.format(len(self.data.datasources)))
+            return self.data.datasources
+        except GrafanaClientError as ex:
+            message = '{name}: {ex}'.format(name=ex.__class__.__name__, ex=ex)
+            log.error(self.get_red_message(message))
+            if isinstance(ex, GrafanaUnauthorizedError):
+                log.error(self.get_red_message('Please use --grafana-token or GRAFANA_TOKEN '
+                                               'for authenticating with Grafana'))
+
+    @staticmethod
+    def get_red_message(message):
+        return colored.stylize(message, colored.fg("red") + colored.attr("bold"))
+
+    def scan_dashboards(self, dashboard_uids=None):
+
+        log.info('Scanning dashboards')
+        try:
+            if dashboard_uids is not None:
+                for uid in dashboard_uids:
+                    log.info(f'Fetching dashboard by uid {uid}')
+                    try:
+                        dashboard = self.grafana.dashboard.get_dashboard(uid)
+                        self.data.dashboard_list.append(dashboard['dashboard'])
+                    except GrafanaClientError as ex:
+                        self.handle_grafana_error(ex)
+                        continue
+            else:
+                self.data.dashboard_list = self.grafana.search.search_dashboards(limit=5000)
+            log.info('Found {} dashboards'.format(len(self.data.dashboard_list)))
+
+        except GrafanaClientError as ex:
+            self.handle_grafana_error(ex)
+            return
+
+        if self.progressbar:
+            self.start_progressbar(len(self.data.dashboard_list))
+
+        if self.concurrency is None or self.concurrency <= 1:
+            self.fetch_dashboards()
+        else:
+            self.fetch_dashboards_parallel()
+
+        if self.progressbar:
+            self.taqadum.close()
+
+        return self.data.dashboards
+
+    def handle_grafana_error(self, ex):
+        message = '{name}: {ex}'.format(name=ex.__class__.__name__, ex=ex)
+        message = colored.stylize(message, colored.fg("red") + colored.attr("bold"))
+        log.error(self.get_red_message(message))
+        if isinstance(ex, GrafanaUnauthorizedError):
+            log.error(self.get_red_message('Please use --grafana-token or GRAFANA_TOKEN '
+                                           'for authenticating with Grafana'))
+
+    def fetch_dashboard(self, dashboard_info):
+        log.debug(f'Fetching dashboard "{dashboard_info["title"]}" ({dashboard_info["uid"]})')
+        dashboard = self.grafana.dashboard.get_dashboard(dashboard_info['uid'])
+        self.data.dashboards.append(munchify(dashboard))
+        if self.taqadum is not None:
+            self.taqadum.update(1)
+
+    def fetch_dashboards(self):
+        log.info('Fetching dashboards one by one')
+        results = self.data.dashboard_list
+        for dashboard_info in results:
+            self.fetch_dashboard(dashboard_info)
+
+    def fetch_dashboards_parallel(self):
+        # https://hackernoon.com/how-to-run-asynchronous-web-requests-in-parallel-with-python-3-5-without-aiohttp-264dc0f8546
+        loop = asyncio.get_event_loop()
+        future = asyncio.ensure_future(self.execute_parallel())
+        loop.run_until_complete(future)
+
+    async def execute_parallel(self):
+        # https://hackernoon.com/how-to-run-asynchronous-web-requests-in-parallel-with-python-3-5-without-aiohttp-264dc0f8546
+        log.info(f'Fetching dashboards in parallel with {self.concurrency} concurrent requests')
+        with ThreadPoolExecutor(max_workers=self.concurrency) as executor:
+
+            # Set any session parameters here before calling `fetch`
+            loop = asyncio.get_event_loop()
+            # START_TIME = default_timer()
+
+            tasks = []
+            for dashboard_info in self.data.dashboard_list:
+                task = loop.run_in_executor(
+                    executor,
+                    self.fetch_dashboard,
+                    dashboard_info
+                )
+                tasks.append(task)
+
+            # Currently, we are not interested in the responses.
+            #for response in await asyncio.gather(*tasks):
+            #    pass
+
+    def get_dashboard_versions(self, dashboard_id):
+        # https://grafana.com/docs/http_api/dashboard_versions/
+        get_dashboard_versions_path = '/dashboards/id/%s/versions' % dashboard_id
+        r = self.grafana.dashboard.api.GET(get_dashboard_versions_path)
+        return r
+
+    def datasource_breakdown(self):
+
+        # Prepare indexes, mapping dashboards by uid, datasources by name
+        # as well as dashboards to datasources and vice versa.
+        ix = Indexer(engine=self)
+
+        # Compute list of breakdown items, associating datasources with the dashboards that use them.
+        results_used = []
+        results_unused = []
+        for name in sorted(ix.datasource_by_name):
+            datasource = ix.datasource_by_name[name]
+            dashboard_uids = ix.datasource_dashboard_index.get(name, [])
+            dashboards = list(map(ix.dashboard_by_uid.get, dashboard_uids))
+            item = DatasourceBreakdownItem(datasource=datasource, used_in=dashboards, grafana_url=self.grafana_url)
+
+            # Format results in a more compact form, using only a subset of all the attributes.
+            result = item.format_compact()
+
+            if dashboard_uids:
+                results_used.append(result)
+            else:
+                results_unused.append(result)
+
+        response = OrderedDict(
+            used=results_used,
+            unused=results_unused,
+        )
+
+        return response
+
+
+class Indexer:
+
+    def __init__(self, engine: GrafanaSearch):
+        self.engine = engine
+
+        # Prepare index data structures.
+        self.dashboard_by_uid = {}
+        self.datasource_by_name = {}
+        self.dashboard_datasource_index = {}
+        self.datasource_dashboard_index = {}
+
+        # Gather all data.
+        self.dashboards = self.engine.scan_dashboards()
+        self.datasources = self.engine.scan_datasources()
+
+        # Invoke indexer.
+        self.index()
+
+    def index(self):
+        self.index_dashboards()
+        self.index_datasources()
+
+    @staticmethod
+    def collect_datasource_names(element):
+        names = []
+        for node in element:
+            if "datasource" in node and node["datasource"]:
+                names.append(node.datasource)
+        return list(sorted(set(names)))
+
+    def index_dashboards(self):
+
+        self.dashboard_by_uid = {}
+        self.dashboard_datasource_index = {}
+
+        for dashboard in self.dashboards:
+            if dashboard.meta.isFolder:
+                continue
+
+            # Index by uid.
+            uid = dashboard.dashboard.uid
+            self.dashboard_by_uid[uid] = dashboard
+
+            # Map to data source names.
+            dbdata = dashboard.dashboard
+            ds_panels = self.collect_datasource_names(dbdata.get("panels", []))
+            ds_annotations = self.collect_datasource_names(dbdata.get("annotations", {}).get("list", []))
+            ds_templating = self.collect_datasource_names(dbdata.get("templating", {}).get("list", []))
+            self.dashboard_datasource_index[uid] = list(sorted(set(ds_panels + ds_annotations + ds_templating)))
+
+    def index_datasources(self):
+
+        self.datasource_by_name = {}
+        self.datasource_dashboard_index = {}
+
+        for datasource in self.datasources:
+            name = datasource.name
+            self.datasource_by_name[name] = datasource
+
+        for dashboard_uid, datasource_names in self.dashboard_datasource_index.items():
+            for datasource_name in datasource_names:
+                self.datasource_dashboard_index.setdefault(datasource_name, [])
+                self.datasource_dashboard_index[datasource_name].append(dashboard_uid)
