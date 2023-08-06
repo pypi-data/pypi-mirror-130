2546710afd693f1678f0a59d75c05dffaa92479e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:44 +0000

    Add package pydantic-configmanager/0.1.1/pydantic_configmanager-0.1.1-py2.py3-none-any.whl

---
+++ packages/pydantic-configmanager/pydantic_configmanager-0.1.1-py2.py3-none-any.whl/pydantic_configmanager/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Pydantic Configuration Manager."""
+
+from .main import EnvironmentBaseModel, SettingsConfig
+
+__all__ = [
+    'EnvironmentBaseModel',
+    'SettingsConfig'
+]
+
+__version__ = '0.1.1'
