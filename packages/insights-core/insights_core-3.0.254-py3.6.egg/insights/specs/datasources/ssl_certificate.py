9b7c4bfc442bb11a31524957d3b2c1fb520c9cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:25 +0000

    Add package insights-core/3.0.254/insights_core-3.0.254-py3.6.egg

---
+++ packages/insights-core/insights_core-3.0.254-py3.6.egg/insights/specs/datasources/ssl_certificate.py
@@ -0,0 +1,76 @@
+"""
+Custom datasource to get ssl certificate file path.
+"""
+
+from insights.combiners.httpd_conf import HttpdConfTree
+from insights.combiners.nginx_conf import NginxConfTree
+from insights.parsers.mssql_conf import MsSQLConf
+from insights.core.context import HostContext
+from insights.core.dr import SkipComponent
+from insights.core.plugins import datasource
+
+
+@datasource(HttpdConfTree, HostContext)
+def httpd_ssl_certificate_files(broker):
+    """
+    Get the httpd SSL certificate file path configured by "SSLCertificateFile"
+
+    Arguments:
+        broker: the broker object for the current session
+
+    Returns:
+        str: Returns the SSL certificate file path configured by "SSLCertificateFile"
+
+    Raises:
+        SkipComponent: Raised if "SSLCertificateFile" directive isn't found
+    """
+    conf = broker[HttpdConfTree]
+    virtual_hosts = conf.find('VirtualHost')
+    ssl_certs = []
+    for host in virtual_hosts:
+        ssl_cert = ssl_engine = None
+        ssl_engine = host.select('SSLEngine')
+        ssl_cert = host.select('SSLCertificateFile')
+        if ssl_engine and ssl_engine.value and ssl_cert:
+            ssl_certs.append(str(ssl_cert.value))
+    if ssl_certs:
+        return ssl_certs
+    raise SkipComponent
+
+
+@datasource(NginxConfTree, HostContext)
+def nginx_ssl_certificate_files(broker):
+    """
+    Get the nginx SSL certificate file path configured by "ssl_certificate"
+
+    Arguments:
+        broker: the broker object for the current session
+
+    Returns:
+        str: Returns the SSL certificate file path configured by "ssl_certificate"
+
+    Raises:
+        SkipComponent: Raised if "ssl_certificate" directive isn't found
+    """
+    conf = broker[NginxConfTree]
+    ssl_certs = conf.find('ssl_certificate')
+    if ssl_certs:
+        return [str(ssl_cert.value) for ssl_cert in ssl_certs]
+    raise SkipComponent
+
+
+@datasource(MsSQLConf, HostContext)
+def mssql_tls_cert_file(broker):
+    """
+    Get the mssql tls certificate file path configured by "ssl_certificate"
+    Arguments:
+        broker: the broker object for the current session
+    Returns:
+        str: Returns the SSL certificate file path configured by "ssl_certificate"
+    Raises:
+        SkipComponent: Raised if "ssl_certificate" directive isn't found
+    """
+    mssql_conf_content = broker[MsSQLConf]
+    if mssql_conf_content.has_option("network", "tlscert"):
+        return mssql_conf_content.get("network", "tlscert")
+    raise SkipComponent
