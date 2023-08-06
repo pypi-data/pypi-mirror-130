3caa0a10c46e7587669f6a73c3f82361a2574112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:31 +0000

    Add package DNRecipe/0.1/DNRecipe-0.1.tar.gz

---
+++ packages/DNRecipe/DNRecipe-0.1.tar.gz/DNRecipe-0.1/README.md
@@ -0,0 +1,3 @@
+# DATA533lab3
+
+[![Build Status](https://app.travis-ci.com/Neetz78/DATA533_Lab4.svg?token=VxY1FsRx6uitmp8xM6za&branch=main)](https://app.travis-ci.com/Neetz78/DATA533_Lab4)
---
+++ packages/DNRecipe/DNRecipe-0.1.tar.gz/DNRecipe-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DNRecipe',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A python package for food recipes',
+	url='https://github.com/Neetz78/DATA533_Lab4.git',
+    author='Disha D H & Neethu G',
+    author_email='neethu.g0708@gmail.com'
+)
