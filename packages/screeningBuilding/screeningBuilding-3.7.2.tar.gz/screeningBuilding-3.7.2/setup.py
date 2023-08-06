720b9b2fb2c5258b9441672a79eeec99b4974051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:12 +0000

    Add package screeningBuilding/3.7.2/screeningBuilding-3.7.2.tar.gz

---
+++ packages/screeningBuilding/screeningBuilding-3.7.2.tar.gz/screeningBuilding-3.7.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+name="screeningBuilding", # Replace with your own username
+version="3.7.2",
+author="Dorian Drevon",
+author_email="drevondorian@gmail.com",
+description="Utilities package",
+long_description=long_description,
+long_description_content_type="text/markdown",
+# url="https://github.com/pypa/sampleproject",
+# project_urls={
+#     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+# },
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+packages=['screeningBuilding'],
+package_data={'': ['confFiles/*']},
+include_package_data=True,
+install_requires=['dorianUtilsModulaire==3.12.5','odfpy==1.4.1'],
+python_requires=">=3.8"
+)
