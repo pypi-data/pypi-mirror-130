7c42da8f4fdb53d76a796d104ab8463d7d50ffdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:38 +0000

    Add package nib-util/0.0.4/nib-util-0.0.4.tar.gz

---
+++ packages/nib-util/nib-util-0.0.4.tar.gz/nib-util-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nib-util",
+    version="0.0.4",
+    author="NIB-Dev-Team",
+    author_email="NIB_IT_Dev@nib-bahamas.com",
+    description="the site package for nib dev team",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NIB-DEVELOPMENT/NIB_utils.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/NIB-DEVELOPMENT/NIB_utils.git/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/nib-util/nib-util-0.0.4.tar.gz/nib-util-0.0.4/src/NIB_utils/enums/exceptionsMessage.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class RoleExceptions(Enum):
+    INVALID_PERMISSIONS = "User does not have the required permissions" 
