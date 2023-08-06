2b05d01feb5e6b8cb7ebe1e221b3adec6893f51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:04 +0000

    Add package PortsPy/0.1.2/PortsPy-0.1.2.tar.gz

---
+++ packages/PortsPy/PortsPy-0.1.2.tar.gz/PortsPy-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='PortsPy',
+      version='0.1.2',
+      description='Modern Webserver',
+      url='http://github.com/Ehnryu/ports',
+      author='Ehnryu/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['ports'],
+      #entry_points={
+        #'console_scripts': [
+            #'build = buildproj.cli:launch',
+        #],
+    #},
+    #install_requires=[
+        #'tqdm',
+        #],
+    keywords=['webserver', 'ports'],
+    classifiers=["Programming Language :: Python :: 3"],
+      zip_safe=False)
