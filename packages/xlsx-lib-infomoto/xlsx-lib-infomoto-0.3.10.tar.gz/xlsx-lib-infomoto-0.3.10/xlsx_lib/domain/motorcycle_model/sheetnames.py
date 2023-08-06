3e552e4e281afbc5a709dfa50f737129bbc1061a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:12 +0000

    Add package xlsx-lib-infomoto/0.3.10/xlsx-lib-infomoto-0.3.10.tar.gz

---
+++ packages/xlsx-lib-infomoto/xlsx-lib-infomoto-0.3.10.tar.gz/xlsx-lib-infomoto-0.3.10/xlsx_lib/domain/motorcycle_model/sheetnames.py
@@ -0,0 +1,14 @@
+from enum import Enum, auto
+
+
+class Sheetnames(Enum):
+    ELECTRONIC = auto()
+    ENGINE = auto()
+    TIGHTENING_TORQUES = auto()
+    GENERIC_REPLACEMENTS = auto()
+    FRAME = auto()
+    POWER_SUPPLY = auto()
+    DISTRIBUTION = auto()
+
+
+
