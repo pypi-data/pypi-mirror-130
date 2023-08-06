6cd769f6404906c01fb85eddf5d3d4d86191ae11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package nzbn/0.0.3/nzbn-0.0.3.tar.gz

---
+++ packages/nzbn/nzbn-0.0.3.tar.gz/nzbn-0.0.3/nzbn/__init__.py
@@ -0,0 +1,11 @@
+from nzbn.entity import Entity
+from nzbn.classification import Classification
+from nzbn.disposition import Disposition
+from nzbn.entity_status import EntityStatus
+from nzbn.entity_type import EntityType
+from nzbn.trading_name import TradingName
+from nzbn.time import NzbnTime
+
+from nzbn.errors.error import NzbnError
+from nzbn.errors.api import NzbnApiError
+from nzbn.errors.type_error import NzbnTypeError
