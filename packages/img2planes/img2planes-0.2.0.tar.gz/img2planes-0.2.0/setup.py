3010ba546d3d9f4b98917a536522e19747fe7d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:47 +0000

    Add package img2planes/0.2.0/img2planes-0.2.0.tar.gz

---
+++ packages/img2planes/img2planes-0.2.0.tar.gz/img2planes-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='img2planes',
+    version='0.2.0',
+    url='https://github.com/zedr/img2planes',
+    description='Convert any image to an Amiga C bitplane',
+    author='Rigel Di Scala',
+    author_email='zedr@zedr.com',
+    py_modules=['img2planes'],
+    entry_points={
+        'console_scripts': ['img2planes=img2planes:main']
+    },
+    install_requires=['Pillow'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ]
+)
