ae98d03189cb4c4de032f6191f52398a9478fb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:28 +0000

    Add package octadocs/0.2.7/octadocs-0.2.7.tar.gz

---
+++ packages/octadocs/octadocs-0.2.7.tar.gz/octadocs-0.2.7/octadocs_provenance/facets/prov_value.py
@@ -0,0 +1,36 @@
+from dominate.tags import a
+from more_itertools import first
+
+from iolanta.facet import Facet
+from octadocs.iolanta import render, HTML
+
+
+class ProvEntity(Facet):
+    """Render a node with prov:value defined."""
+
+    def render(self):
+        """Render value and link to it."""
+        rows = self.octiron.query(
+            '''
+            SELECT * WHERE {
+                ?node
+                    prov:value ?value ;
+                    prov:wasDerivedFrom ?source .
+            }
+            ''',
+            node=self.iri,
+        )
+
+        try:
+            row = first(rows)
+        except ValueError:
+            return '<prov:value> and <prov:wasDerivedFrom> were not found.'
+
+        return a(
+            render(
+                node=row['value'],
+                octiron=self.octiron,
+            ),
+            href=row['source'],
+            target='_blank',
+        )
