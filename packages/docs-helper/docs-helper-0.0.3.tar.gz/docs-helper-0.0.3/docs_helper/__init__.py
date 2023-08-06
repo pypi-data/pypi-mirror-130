09281c66a308f1fcc4588c3ea225a7794b8c33b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:19 +0000

    Add package docs-helper/0.0.3/docs-helper-0.0.3.tar.gz

---
+++ packages/docs-helper/docs-helper-0.0.3.tar.gz/docs-helper-0.0.3/docs_helper/__init__.py
@@ -0,0 +1,14 @@
+"""
+This is a docstring
+
+`Docs how to write docstring
+<https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html>`_
+"""
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
