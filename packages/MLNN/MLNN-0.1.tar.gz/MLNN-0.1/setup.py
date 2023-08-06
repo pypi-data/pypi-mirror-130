26817cc10208a961d07cf330296d22d6002e5a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:06 +0000

    Add package MLNN/0.1/MLNN-0.1.tar.gz

---
+++ packages/MLNN/MLNN-0.1.tar.gz/MLNN-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='MLNN',
+    version='0.1',    
+    description='Multi-layer neuron network',
+    author='vladgap',
+    author_email='gaponenko.vladimir@gmail.com',
+    packages=['MLNN'],
+    zip_safe=False 
+)
