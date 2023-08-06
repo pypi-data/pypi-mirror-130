71b2983a11904ce35dc2548117e793e0a8955173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:43 +0000

    Add package cs-models/0.0.290/cs-models-0.0.290.tar.gz

---
+++ packages/cs-models/cs-models-0.0.290.tar.gz/cs-models-0.0.290/src/cs_models/resources/CompanyReview/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Boolean,
+)
+from datetime import datetime
+
+from ...database import Base
+
+
+class CompanyReviewModel(Base):
+    __tablename__ = 'company_review'
+
+    id = Column(Integer, primary_key=True)
+    company_sec_id = Column(
+        Integer,
+        ForeignKey('companies_sec.id'),
+        nullable=True,
+    )
+    company_ous_id = Column(
+        Integer,
+        ForeignKey('companies_ous.id'),
+        nullable=True,
+    )
+    note_tag = Column(String(20), nullable=True)
+    news_id = Column(
+        Integer,
+        ForeignKey('newswires.id'),
+        nullable=False,
+    )
+    reviewed = Column(Boolean, nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
