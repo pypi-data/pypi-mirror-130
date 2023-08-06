b8b1b259494993d85c19bab60274f2835e9d36bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:37 +0000

    Add package disgames/2.0.1/disgames-2.0.1.tar.gz

---
+++ packages/disgames/disgames-2.0.1.tar.gz/disgames-2.0.1/disgames/__init__.py
@@ -0,0 +1,15 @@
+from .cog import *
+from .constants import *
+from discord.ext import commands
+from .errors import PathNotFound
+
+def register_commands(bot, *, ignore: list = [], stockfish_path=None):
+    games = [game for game in ALL_GAMES if game not in ignore]
+    class Games(*games):
+        def __init__(self, bot):
+            for cls in games:
+                cls.__init__(self, bot)
+    g = Games(bot)
+    if stockfish_path:
+        g.stockfish_path = stockfish_path
+    bot.add_cog(g)
---
+++ packages/disgames/disgames-2.0.1.tar.gz/disgames-2.0.1/disgames/constants.py
@@ -0,0 +1,15 @@
+from .mixins import *
+
+ALL_GAMES = [
+    TicTacToe,
+    Hangman,
+    MadLib,
+    Chess,
+    Minesweeper,
+    Sokoban,
+    RPS,
+    Connect4,
+    _2048,
+    Checkers,
+    Sudoko
+]
