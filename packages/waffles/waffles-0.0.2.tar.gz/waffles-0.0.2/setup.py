837a33a0ce5f80bb9a42b3cab78e222abc89cd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:53 +0000

    Add package waffles/0.0.2/waffles-0.0.2.tar.gz

---
+++ packages/waffles/waffles-0.0.2.tar.gz/waffles-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['waffles']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.21.1,<0.22.0', 'python-decouple>=3.5,<4.0', 'rich>=10.15.2,<11.0.0']
+
+setup_kwargs = {
+    'name': 'waffles',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Shahnoza Bekbulaeva',
+    'author_email': 'shakhnoza.bekbulaeva@nttdata.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/waffles/waffles-0.0.2.tar.gz/waffles-0.0.2/waffles/sprinkles.py
@@ -0,0 +1,10 @@
+from decouple import config
+
+LIMIT = 16
+LANGUAGE = "en-GB"
+COUNTRY = "gb"
+MAX_PREP_TIME = 30
+MIN_RATING = 3.3
+USER_AGENT = "Shahnozas Bakery"
+
+TOKEN = config("TOKEN")
