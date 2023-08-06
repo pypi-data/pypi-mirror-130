c336bba81b1adcc2bf33f6041cbb6e0c005d478c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:46 +0000

    Add package imath-requests/0.1.24/imath_requests-0.1.24.tar.gz

---
+++ packages/imath-requests/imath_requests-0.1.24.tar.gz/imath_requests-0.1.24/tests/test_server.py
@@ -0,0 +1,20 @@
+"""
+test_server.py
+====================================
+Unit testing for server module
+"""
+
+import unittest
+from imath_requests.server import Server
+
+
+class TestServer(unittest.TestCase):
+    """
+    Unit testing for Server class in requests module.
+
+    """
+    def test_init_server(self):
+        server = Server()
+
+    def test_server_run(self):
+        pass
