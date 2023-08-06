410e6ded9b06d0f8bcb83397c8afac1abe904f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:59 +0000

    Add package pycamdetector/0.5/pycamdetector-0.5.tar.gz

---
+++ packages/pycamdetector/pycamdetector-0.5.tar.gz/pycamdetector-0.5/pycamdetector/__init__.py
@@ -0,0 +1,6 @@
+from pycamdetector.FaceDetectionModule import FaceDetector
+from pycamdetector.HandTrackingModule import HandDetector
+from pycamdetector.FaceMeshModule import FaceMeshDetector
+from pycamdetector.PoseModule import poseDetector
+from pycamdetector.ObjectDetectionModule import ObjectDetector
+from pycamdetector.FPS import FPS
