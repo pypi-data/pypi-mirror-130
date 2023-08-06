69b7591635318d4f242025dd23e0fe83fc4f4c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:23 +0000

    Add package shapecalc/1.0.2/shapecalc-1.0.2.tar.gz

---
+++ packages/shapecalc/shapecalc-1.0.2.tar.gz/shapecalc-1.0.2/src/shapecalc/Cube.py
@@ -0,0 +1,8 @@
+def cube_s(a):
+    S = 6 * a * a
+
+def cube_sl(a):
+    SL = 12 * a
+
+def cube_v(a):
+    V = a ** 3
---
+++ packages/shapecalc/shapecalc-1.0.2.tar.gz/shapecalc-1.0.2/src/shapecalc/Rectangle.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+def rectangle_s(a,b):
+    s=a*b
+
+def rectangle_c(a,b):
+    c=(a+b)*2
+
