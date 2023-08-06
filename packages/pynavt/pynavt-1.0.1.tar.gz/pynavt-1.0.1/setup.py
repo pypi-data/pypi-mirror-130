6d499101d06f422397321df6f045ee457e91dcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:49 +0000

    Add package pynavt/1.0.1/pynavt-1.0.1.tar.gz

---
+++ packages/pynavt/pynavt-1.0.1.tar.gz/pynavt-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynavt',
+    version='1.0.1',
+    author='Alejandro Castillo Valdes',
+    author_email='saturnavt@gmail.com',
+    description='pynavt is a tool to create a clean architecture project and auto generating modules',
+    license='MIT',
+    url='https://github.com/saturnavt/pynavt',
+    packages=find_packages(),
+    py_modules=['pynavt', 'base'],
+    install_requires=[
+        'click',
+        'fastapi',
+        'uvicorn',
+        'pydantic'
+    ],
+    entry_points='''
+        [console_scripts]
+        pynavt=pynavt.__init__:main
+    ''',
+)
