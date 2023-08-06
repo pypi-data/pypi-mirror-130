feeac2c9e693b983f3b15a112decafb20546abe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:28 +0000

    Add package pyportable-installer/4.1.0/pyportable_installer-4.1.0-py3-none-any.whl

---
+++ packages/pyportable-installer/pyportable_installer-4.1.0-py3-none-any.whl/pyportable_installer/main_flow/step1/indexing_paths.py
@@ -0,0 +1,156 @@
+from os.path import basename
+from os.path import exists
+from uuid import uuid1
+
+from lk_logger import lk
+
+from .path_formatter import PathFormatter
+from ...path_model import prj_model
+from ...typehint import *
+
+
+def indexing_paths(conf: TConf, path_fmt: Union[PathFormatter, Callable]):
+    """
+    Args:
+        conf
+        path_fmt:
+            - PathFormatter: map path node from dir_i to dir_o.
+                - the default dir_i is dirname of pyproj_file.
+                - the default dir_o is dist dir from conf['build']['dist_dir'].
+            - Callable: receives one argument as path_i, returns path_o. the
+                typical pattern is: `lambda path_i: path_o`.
+    """
+    placeholders = {
+        'app_name'       : conf['app_name'],
+        'app_name_lower' : conf['app_name'].lower().replace(' ', '_'),
+        'app_name_hyphen': conf['app_name'].lower().replace(' ', '-'),
+        'app_version'    : conf['app_version'],
+    }
+    
+    conf['build']['proj_dir'] = path_fmt(
+        conf['build']['proj_dir']
+    )
+    
+    conf['build']['dist_dir'] = path_fmt(
+        conf['build']['dist_dir'].format(**placeholders)
+    )
+    
+    conf['build']['launcher_name'] = (
+        conf['build']['launcher_name'].format(**placeholders)
+    )
+    
+    conf['build']['icon'] = path_fmt(
+        conf['build']['icon'] or
+        prj_model.python_ico
+    )
+    
+    conf['build']['readme'] = path_fmt(
+        conf['build']['readme']
+    )
+    
+    for t in conf['build']['target']:  # type: TTarget
+        t['file'] = path_fmt(
+            t['file'].format(dist_dir=conf['build']['dist_dir'])
+        )
+    
+    # --------------------------------------------------------------------------
+    
+    # note: `conf['build']['attachments']` and `conf['build']['module_paths']`
+    # may include keyword `dist:xxx`, it requires `path_fmt.dir_o`. so here we
+    # must make sure `path_fmt.dir_o` has been defined.
+    path_fmt.dir_o = conf['build']['dist_dir']
+    
+    temp0 = {}  # type: TAttachments
+    for k, v in conf['build']['attachments'].items():
+        # lk.logt('[D0510]', k, v)
+        k = path_fmt(k)
+        # noinspection PyUnresolvedReferences
+        v = v.split(',')  # type: List[str]
+        if v[-1].startswith('dist:'):
+            assert len(v) > 1, ((k, v), conf['build']['attachments'])
+            path = path_fmt(v[-1].format(name=basename(k)))
+            temp0[k] = {'marks': tuple(v[:-1]), 'path': path}
+        else:
+            temp0[k] = {'marks': tuple(v), 'path': ''}
+    conf['build']['attachments'] = temp0
+    del temp0
+    
+    # noinspection PyTypedDict
+    conf['build']['attachments_exclusions'] = tuple(
+        map(path_fmt, conf['build']['attachments_exclusions'])
+    )
+    
+    # --------------------------------------------------------------------------
+    # build:module_paths
+    
+    module_paths = conf['build']['module_paths']
+    for i, p in enumerate(module_paths):
+        if p.startswith('dist:'):
+            p = path_fmt(p)
+        else:
+            # FIXME: see `pyportable_installer/main_flow/step3/step3_3/create
+            #   _launcher.py > _generate_pylauncher > vars:_ext_paths`
+            # p = src_2_dst(
+            #     path_fmt(p), conf['build']['proj_dir'], path_fmt.dir_o
+            # )
+            p = 'src:' + path_fmt(p)
+        module_paths[i] = p
+    conf['build']['module_paths'] = module_paths
+    del module_paths
+    
+    # # conf['build']['module_paths'] = list(
+    # #     map(path_fmt, conf['build']['module_paths'])
+    # # )
+    
+    # --------------------------------------------------------------------------
+    # build:venv
+    
+    mode = conf['build']['venv']['mode']
+    options = conf['build']['venv']['options'][mode]
+    
+    def _load_requirements(req: Union[str, List[str]]):
+        if isinstance(req, str):
+            if req:
+                # FIXME: path_fmt must be 'abspath'
+                if path_fmt.fmt == 'relpath':
+                    # file = path_fmt.dir_i + '/' + path_fmt
+                    return ['...']
+                from lk_utils.read_and_write import load_list
+                file = path_fmt(req)
+                return [x for x in load_list(file)
+                        if x and not x.startswith('#')]
+            else:
+                return []
+        else:
+            return req
+    
+    if mode == 'depsland':
+        options['venv_name'] = options['venv_name'].format(**placeholders)
+        if options['venv_id'] in ('', '_random'):
+            options['venv_id'] = str(uuid1()).replace('-', '')
+        options['requirements'] = _load_requirements(options['requirements'])
+        options['local'] = path_fmt(options['local'])
+    elif mode == 'embed_python':
+        options['path'] = path_fmt(options['path'])
+    elif mode == 'pip':
+        options['requirements'] = _load_requirements(options['requirements'])
+        options['local'] = path_fmt(options['local'])
+    elif mode == 'source_venv':
+        options['path'] = path_fmt(options['path'])
+    else:
+        raise ValueError(mode)
+    
+    # --------------------------------------------------------------------------
+    # build:compiler
+    
+    name = conf['build']['compiler']['name']
+    options = conf['build']['compiler']['options'][name]
+    if name == 'pyportable_crypto':
+        if options['key'].startswith('path:'):
+            assert options['key'].endswith('pyportable_runtime')
+            options['key'] = 'path:' + (d := path_fmt(options['key'][5:]))
+            assert exists(d)
+            lk.logt('[I0946]', '(experimental feature)',
+                    'use local precompiled pyportable_runtime package', d)
+    
+    return conf
---
+++ packages/pyportable-installer/pyportable_installer-4.1.0-py3-none-any.whl/pyportable_installer/main_flow/step1/init_key_params.py
@@ -0,0 +1,202 @@
+import os
+import re
+from os.path import dirname
+from os.path import exists
+
+from embed_python_manager import EmbedPythonManager
+from lk_logger import lk
+from lk_utils import run_cmd_shell
+from lk_utils.filesniff import normpath
+
+from ...global_conf import gconf
+from ...path_model import prj_model
+from ...typehint import *
+
+
+def init_key_params(conf: TConf, **kwargs):
+    # pyproj_file
+    gconf.pyproj_file = kwargs['pyproj_file']
+    # pyproj_dir
+    gconf.pyproj_dir = kwargs['pyproj_dir']
+    
+    # --------------------------------------------------------------------------
+    # attachments
+    
+    gconf.attachments_exclusions = conf['build']['attachments_exclusions']
+    assert type(gconf.attachments_exclusions) is tuple
+    gconf.attachments_exist_scheme = conf['build']['attachments_exist_scheme']
+    
+    # --------------------------------------------------------------------------
+    
+    pyversion = conf['build']['venv']['python_version']
+    assert pyversion, '`conf["build"]["venv"]["python_version"]` not defined'
+    pyversion = 'python' + pyversion.replace('.', '')
+    gconf.target_pyversion = pyversion
+    
+    # --------------------------------------------------------------------------
+    
+    name = conf['build']['compiler']['name']  # type: TCompilerName
+    mode = conf['build']['venv']['mode']  # type: TVenvMode
+    enable_venv = conf['build']['venv']['enable_venv']  # type: bool
+    
+    is_full_python_required = False
+    if not enable_venv:
+        is_full_python_required = True
+    elif name in ('cython', 'mypyc', 'nuitka'):
+        is_full_python_required = True
+    elif name == 'pyportable_crypto':
+        key = conf['build']['compiler']['options'][name]['key']
+        if key != '_trial' and not key.startswith('path:'):
+            is_full_python_required = True
+    
+    is_embed_python_required = False
+    if (
+            (name in ('pyarmor', 'pyc')) or
+            (enable_venv and mode == 'pip') or
+            (enable_venv and mode == 'source_venv' and
+             conf['build']['venv']['options']['source_venv']['copy_venv'])
+    ):
+        is_embed_python_required = True
+    
+    if name == 'pyportable_crypto':
+        key = conf['build']['compiler']['options'][name]['key']
+        if key.startswith('path:') and pyversion != 'python39':
+            is_embed_python_required = True
+    
+    if name in ('pyarmor', 'pyc') or \
+            (enable_venv and mode in ('pip', 'source_venv')):
+        is_embed_python_required = True
+    
+    is_pip_required = False
+    if enable_venv and mode == 'pip':
+        is_pip_required = True
+    
+    lk.logt('[I5239]',
+            is_full_python_required,
+            is_embed_python_required,
+            is_pip_required)
+    
+    # --------------------------------------------------------------------------
+    
+    if is_full_python_required:
+        # noinspection PyTypedDict
+        options = conf['build']['compiler']['options'][name]
+        if options['python_path'] in ('_auto_detect', 'auto_detect', ''):
+            gconf.full_python = _get_full_python(pyversion)
+        else:
+            if (p := options['python_path']).endswith('python.exe'):
+                lk.logt(
+                    '[W2320]',
+                    'please do not pass "{}" path to `{}`, use "{}" instead. '
+                    'the exe-path support may be removed in the future.'.format(
+                        p, 'pyproject.json > build:compiler:options:{}:'
+                           'python_path'.format(name), dirname(p)
+                    )
+                )
+                gconf.full_python = p
+            else:
+                gconf.full_python = p + '/python.exe'
+        lk.loga(gconf.full_python)
+        assert exists(gconf.full_python)
+    
+    # --------------------------------------------------------------------------
+    
+    if is_embed_python_required:
+        if is_pip_required:
+            gconf.embed_python = _get_embed_python(pyversion, True)
+        else:
+            gconf.embed_python = _get_embed_python_from_local(pyversion) or \
+                                 _get_embed_python(pyversion, False)
+        assert exists(gconf.embed_python)
+
+
+def _get_full_python(pyversion):
+    def where_python_installed(pyversion: str):  # pyversion: e.g. 'python39'
+        """
+        PS: 'full python' means a full featured python, i.e. the regular python
+            installed in your computer.
+        
+        There're two ways to search for full python:
+            1. Use `py -{pyversion} ...`
+               This is introduced since Python 3.3. See https://docs.python.org
+               /3/using/windows.html#launcher for more information.
+               Note that there're several inadequacies:
+               1. It supports only Python 3.3+.
+               2. If user has installed a Python version in user-only position,
+                  it doesn't work.
+            2. Look for the most frequently used positions. Include system
+               environment variables.
+
+        Returns:
+            The full python dir. For example: 'C:/Program Files/Python38'
+        """
+        if pyversion.startswith('python3'):
+            try:
+                ret = run_cmd_shell(
+                    'py -{} -c "import sys;print(sys.executable)"'.format(
+                        '3' + '.' + pyversion.replace('python3', '')
+                    )
+                )
+                return normpath(ret).replace('/python.exe', '')
+            except:
+                pass
+        
+        for v in filter(None, os.getenv('PATH').lower().split(';')):
+            if re.search(rf'[/\\]{pyversion}[/\\]?$', v):
+                return normpath(v).rstrip('/')
+        
+        available_paths = (
+            f'{os.path.expanduser("~")}'
+            + '/appdata/local/programs/python/{pyversion}',
+            f'c:/program files/{pyversion}',
+            f'c:/program files (x86)/{pyversion}',
+            f'c:/program files (x86)/{pyversion}',
+            f'd:/program files/{pyversion}',
+            f'd:/program files (x86)/{pyversion}',
+            f'd:/program files (x86)/{pyversion}',
+            f'e:/program files/{pyversion}',
+            f'e:/program files (x86)/{pyversion}',
+            f'e:/program files (x86)/{pyversion}',
+        )
+        
+        for p in available_paths:
+            if os.path.exists(p):
+                return p
+        
+        raise FileNotFoundError(
+            'Auto detection failed finding avaiable executable python in your '
+            'computer. Please make sure you have installed the required Python '
+            f'(version {pyversion[6:]}) in your computer and fill the path in '
+            f'your "~/pyproject.json" file.'
+        )
+    
+    try:
+        return where_python_installed(pyversion) + '/python.exe'
+    except FileNotFoundError as e:
+        lk.logt("[E5951]", 'Cannot find installed python in your computer. '
+                           'Please pass in your python path manually (for '
+                           'example "C:\\Program Files\\Python38") ...')
+        path = input(f'Input path here ({pyversion}): ')
+        if not exists(path):
+            raise e
+        else:
+            return normpath(path) + '/python.exe'
+
+
+def _get_embed_python_from_local(pyversion):
+    if pyversion == 'python39' and \
+            exists(f'{prj_model.prj_root}/venv/python39._pth.bak'):
+        return f'{prj_model.prj_root}/venv/python.exe'
+    else:
+        return ''
+
+
+def _get_embed_python(pyversion, add_pip_suits: bool):
+    manager = EmbedPythonManager(pyversion)
+    manager.change_source('npm_taobao_org.yml')
+    manager.deploy(
+        add_pip_suits=add_pip_suits,
+        add_pip_scripts=False,
+        add_tk_suits=False  # FIXME: check tkinter requirement
+    )
+    return manager.python
