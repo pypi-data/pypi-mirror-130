3f26b586e5982586b86cc636dbdc1beb5566d046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package dbt-firebolt/0.21.4/dbt-firebolt-0.21.4.tar.gz

---
+++ packages/dbt-firebolt/dbt-firebolt-0.21.4.tar.gz/dbt-firebolt-0.21.4/dbt/adapters/firebolt/__init__.py
@@ -0,0 +1,15 @@
+from dbt.adapters.base import AdapterPlugin
+
+from dbt.adapters.firebolt.connections import (
+    FireboltConnectionManager,
+    FireboltCredentials,
+)
+from dbt.adapters.firebolt.impl import FireboltAdapter
+from dbt.adapters.firebolt.relation import FireboltRelation
+from dbt.include import firebolt
+
+Plugin = AdapterPlugin(
+    adapter=FireboltAdapter,
+    credentials=FireboltCredentials,
+    include_path=firebolt.PACKAGE_PATH,
+)
