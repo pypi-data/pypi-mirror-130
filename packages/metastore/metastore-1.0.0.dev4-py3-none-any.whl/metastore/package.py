da4ee44bd9530cd21a4ba1161c091b7e4172340b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:46 +0000

    Add package metastore/1.0.0.dev4/metastore-1.0.0.dev4-py3-none-any.whl

---
+++ packages/metastore/metastore-1.0.0.dev4-py3-none-any.whl/metastore/package.py
@@ -0,0 +1,22 @@
+'''
+Package description information.
+'''
+
+__title__ = 'Metastore'
+__version__ = '1.0.0.dev4'
+__description__ = 'Metastore Python SDK.'
+__author__ = 'Metastore Developers'
+__email__ = 'metastoredevelopers@gmail.com'
+__license__ = 'BSD-3-Clause'
+__copyright__ = 'Copyright (c) 2022, Metastore Developers. All rights reserved.'
+
+
+__all__ = [
+    '__title__',
+    '__version__',
+    '__description__',
+    '__author__',
+    '__email__',
+    '__license__',
+    '__copyright__'
+]
