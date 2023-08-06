478cc203fd198dff1433d8dd19c70db40f103fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:56 +0000

    Add package LineDream/0.1.11/LineDream-0.1.11.tar.gz

---
+++ packages/LineDream/LineDream-0.1.11.tar.gz/LineDream-0.1.11/LineDream/primitives/Line.py
@@ -0,0 +1,17 @@
+from .BaseShape import BaseShape
+
+from typing import Tuple
+class Line(BaseShape):
+	def __init__(self, verticies:[Tuple]=None, **kwargs):
+		super().__init__(**kwargs)
+
+		if verticies:
+
+			for (x,y) in verticies:
+				self.add_vertex(x,y,0)
+
+
+
+
+# if __name__ == '__main__':
+# 	p = Path([(1,2),(2,5) ])
