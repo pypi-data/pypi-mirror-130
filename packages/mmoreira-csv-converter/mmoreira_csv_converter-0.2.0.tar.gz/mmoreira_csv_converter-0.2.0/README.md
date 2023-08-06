797b678773b2f307ee903503e68592388e0213a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:00 +0000

    Add package mmoreira-csv-converter/0.2.0/mmoreira_csv_converter-0.2.0.tar.gz

---
+++ packages/mmoreira-csv-converter/mmoreira_csv_converter-0.2.0.tar.gz/mmoreira_csv_converter-0.2.0/README.md
@@ -0,0 +1,10 @@
+Project description
+csv to json
+json to csv
+
+License
+Only learning purpose. (Trabalho de programação da disciplina Python. PUC-MG)
+Versão 0.2.0
+
+Autor
+Marcelo Moreira G Silva
---
+++ packages/mmoreira-csv-converter/mmoreira_csv_converter-0.2.0.tar.gz/mmoreira_csv_converter-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mmoreira_csv_converter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0', 'flake8>=4.0.1,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['csv2json = mmoreira_csv_converter.csv2json:converter']}
+
+setup_kwargs = {
+    'name': 'mmoreira-csv-converter',
+    'version': '0.2.0',
+    'description': 'Convert csv to json or convert json to csv. Publishing only for learning purpose at PUC-MG. DS Class',
+    'long_description': 'Project description\ncsv to json\njson to csv\n\nLicense\nOnly learning purpose. (Trabalho de programação da disciplina Python. PUC-MG)\nVersão 0.2.0\n\nAutor\nMarcelo Moreira G Silva',
+    'author': 'Marcelo Moreira G Silva',
+    'author_email': 'mqestatistico@gamil.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
