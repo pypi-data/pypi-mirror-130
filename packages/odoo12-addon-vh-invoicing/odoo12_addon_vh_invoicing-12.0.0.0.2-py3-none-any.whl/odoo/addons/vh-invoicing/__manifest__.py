11e111e9c3f5b4b5e39b2942d0dd243ff4256e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:32 +0000

    Add package odoo12-addon-vh-invoicing/12.0.0.0.2/odoo12_addon_vh_invoicing-12.0.0.0.2-py3-none-any.whl

---
+++ packages/odoo12-addon-vh-invoicing/odoo12_addon_vh_invoicing-12.0.0.0.2-py3-none-any.whl/odoo/addons/vh-invoicing/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vertical-habitatge-invoicing",
+
+    'summary': """
+        Invoicing process for habitatge.""",
+
+    'author': "Coopdevs",
+    'website': "https://gitlab.com/coopdevs/vertical-habitatge",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'vertical-habitatge',
+    'version': '12.0.0.0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+    ],
+}
