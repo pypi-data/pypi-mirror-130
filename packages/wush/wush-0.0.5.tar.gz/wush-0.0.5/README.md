3dc510c02d860acc5bfd8c04078e4ea459f39daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:55 +0000

    Add package wush/0.0.5/wush-0.0.5.tar.gz

---
+++ packages/wush/wush-0.0.5.tar.gz/wush-0.0.5/README.md
@@ -0,0 +1,8 @@
+# wapi
+
+wxnacy api 请求客户端
+
+
+## 更新历史
+
+[HISTORY](docs/HISTORY.md)
---
+++ packages/wush/wush-0.0.5.tar.gz/wush-0.0.5/wush/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy@gmail.com
+"""
+
+"""
+
+from .wush import Wapi
+from .common import functions
+
+__all__ = ['Wapi', ]
