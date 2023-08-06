7a205a33cc86cca98e04119a9865042944ce9c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:17 +0000

    Add package mouzz/0.0.1/mouzz-0.0.1.tar.gz

---
+++ packages/mouzz/mouzz-0.0.1.tar.gz/mouzz-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='mouzz',
+    version='0.0.1',
+    author='prim',
+    author_email='1210102@qq.com',
+    url='https://github.com/prim',
+    description=u'mouzz',
+    packages=['mouzz'],
+    install_requires=["mouse", "keyboard", "psutil", "pywin32"],
+    entry_points={
+        'console_scripts': [
+            'mouzz=mouzz:main',
+        ]
+    }
+)
