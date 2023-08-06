1687b182494dc2f36441e379e0ae84a652fd93fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:58 +0000

    Add package TextStega/0.1.3/TextStega-0.1.3.tar.gz

---
+++ packages/TextStega/TextStega-0.1.3.tar.gz/TextStega-0.1.3/TextStega/__init__.py
@@ -0,0 +1,16 @@
+"""
+This module implements Text Steganography
+Author: Atharva Vidwans
+Github: https://github.com/Atharva-Vidwans
+LinkedIn: https://www.linkedin.com/in/atharva-vidwans-62739b169/
+Medium: https://medium.com/@atharvavidwans
+Website: https://atharva-vidwans.github.io/Website/ 
+
+"""
+
+__author__ = "Atharva Vidwans"
+__email__ = "atharvavidwans@gmail.com"
+__status__ = "stable"
+__version__= "0.1.3" 
+
+from TextStega.stg import TextStega 
