2d755efac59429382ad0955afdfbe815bb3cc571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:43 +0000

    Add package pywtdlib/0.0.4/pywtdlib-0.0.4.tar.gz

---
+++ packages/pywtdlib/pywtdlib-0.0.4.tar.gz/pywtdlib-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="pywtdlib",
+    version="0.0.4",
+    author="alvhix",
+    author_email="alvhix@gmail.com",
+    description="A simple Python TDLib wrapper",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alvhix/pywtdlib",
+    project_urls={
+        "Bug Tracker": "https://github.com/alvhix/pywtdlib/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=["python-dotenv"],
+    python_requires=">=3.6",
+)
