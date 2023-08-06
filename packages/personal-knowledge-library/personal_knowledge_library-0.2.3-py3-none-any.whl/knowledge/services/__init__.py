ca26d490a575382e8053cd567acbd88e831ea3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:15 +0000

    Add package personal-knowledge-library/0.2.3/personal_knowledge_library-0.2.3-py3-none-any.whl

---
+++ packages/personal-knowledge-library/personal_knowledge_library-0.2.3-py3-none-any.whl/knowledge/services/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2021 Wacom. All rights reserved.
+
+__all__ = ['base', 'graph', 'ontology']
+
+from knowledge.services import base
+from knowledge.services import graph
+from knowledge.services import ontology
