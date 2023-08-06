c61b7fca709cf7054d509b2ee8422a2a32c2e158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:28 +0000

    Add package UW-RestClients-Catalyst/1.1.0/UW-RestClients-Catalyst-1.1.0.tar.gz

---
+++ packages/UW-RestClients-Catalyst/UW-RestClients-Catalyst-1.1.0.tar.gz/UW-RestClients-Catalyst-1.1.0/uw_catalyst/util.py
@@ -0,0 +1,8 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from commonconf import override_settings
+
+
+fdao_catalyst_override = override_settings(
+    RESTCLIENTS_CATALYST_DAO_CLASS='Mock')
