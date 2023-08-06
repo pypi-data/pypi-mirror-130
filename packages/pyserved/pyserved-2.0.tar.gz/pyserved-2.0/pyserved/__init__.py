6497926a3b993ac7b9721d8baf0c3bd28856884a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:21 +0000

    Add package pyserved/2.0/pyserved-2.0.tar.gz

---
+++ packages/pyserved/pyserved-2.0.tar.gz/pyserved-2.0/pyserved/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+Pyserved module
+
+1.1 -> Added support for all OS
+1.2 -> Removed cli and just made a python package.
+MAJOR UPDATE
+2.0 -> Bought back cli and added support for ALL codec files. 
+
+Shaurya Pratap Singh 2021
+"""
+
+__version__ = '2.0'
+__author__ = 'Shaurya Pratap Singh'
+__message__ = 'Hail dogecoin! Hail hydra!'
