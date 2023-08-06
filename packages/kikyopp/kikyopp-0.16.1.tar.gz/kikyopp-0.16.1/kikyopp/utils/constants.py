3563d089c10a71d8add5df1fe3e52152e3cff671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:50 +0000

    Add package kikyopp/0.16.1/kikyopp-0.16.1.tar.gz

---
+++ packages/kikyopp/kikyopp-0.16.1.tar.gz/kikyopp-0.16.1/kikyopp/utils/constants.py
@@ -0,0 +1,5 @@
+YURI_API_HOST = 'http://yuri.app.kdsec.org'
+TIKA_HOST = 'http://tika.app.kdsec.org'
+PADDLEOCR_HOST = 'http://paddleocr.app.kdsec.org'
+PDF_CONVERTER_HOST = 'http://pdf-converter.app.kdsec.org'
+FILE_CONTENT_LIMIT = 5000
