d054589b64da77ef3e6b69ac32655139a31d93dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:43 +0000

    Add package apollo-tylerhm/0.0.3/apollo-tylerhm-0.0.3.tar.gz

---
+++ packages/apollo-tylerhm/apollo-tylerhm-0.0.3.tar.gz/apollo-tylerhm-0.0.3/apollo/verdict.py
@@ -0,0 +1,21 @@
+class Verdict:
+    def __init__(self, short_name, name, return_code):
+        self.short_name = short_name
+        self.name = name
+        self.return_code = return_code
+    def to_string(self):
+        return self.short_name + ': ' + self.name
+
+Correct = Verdict('AC', 'All Correct', 0)
+WrongAnswer = Verdict('WA', 'Wrong Answer', 1)
+PresentationError = Verdict('PE', 'PresentationError', 2)
+
+class Response:
+    def __init__(self, verdict, message=''):
+        self.verdict = verdict
+        self.message = message
+    def to_string(self):
+        ret = self.verdict.to_string()
+        if self.message != '':
+            ret = ret + '\n' + self.message
+        return ret
