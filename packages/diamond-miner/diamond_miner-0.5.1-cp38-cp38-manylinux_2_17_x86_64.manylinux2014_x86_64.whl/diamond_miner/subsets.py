146d2c42046972d8bacf718331a3a47a55d91965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:21 +0000

    Add package diamond-miner/0.5.1/diamond_miner-0.5.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/diamond-miner/diamond_miner-0.5.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/diamond_miner/subsets.py
@@ -0,0 +1,83 @@
+from ipaddress import IPv6Network, ip_network
+from typing import Dict, List, Union
+
+from diamond_miner.queries import (
+    CountFlowsPerPrefix,
+    CountLinksPerPrefix,
+    CountProbesPerPrefix,
+    CountResultsPerPrefix,
+    FlowsQuery,
+    LinksQuery,
+    ProbesQuery,
+    ResultsQuery,
+)
+from diamond_miner.utilities import common_parameters
+
+Counts = Dict[IPv6Network, int]
+
+
+def subsets_for(
+    query: Union[FlowsQuery, LinksQuery, ProbesQuery, ResultsQuery],
+    url: str,
+    measurement_id: str,
+    max_items_per_subset: int = 8_000_000,
+) -> List[IPv6Network]:
+    if isinstance(query, FlowsQuery):
+        count_query = CountFlowsPerPrefix(**common_parameters(query, FlowsQuery))
+    elif isinstance(query, LinksQuery):
+        count_query = CountLinksPerPrefix(**common_parameters(query, LinksQuery))  # type: ignore
+    elif isinstance(query, ProbesQuery):
+        count_query = CountProbesPerPrefix(**common_parameters(query, ProbesQuery))  # type: ignore
+    elif isinstance(query, ResultsQuery):
+        count_query = CountResultsPerPrefix(**common_parameters(query, ResultsQuery))  # type: ignore
+    else:
+        raise NotImplementedError
+    counts = {
+        addr_to_network(
+            row["prefix"], count_query.prefix_len_v4, count_query.prefix_len_v6
+        ): row["count"]
+        for row in count_query.execute_iter(url, measurement_id)
+    }
+    return split(counts, max_items_per_subset)
+
+
+def split(counts: Counts, max_items_per_subset: int) -> List[IPv6Network]:
+    """
+    Return the IP networks such that there are no more than `max_items_per_subset`
+    per network.
+
+    :param counts: Number of items per prefix in the database table.
+    :param max_items_per_subset: Maximum number of items per network.
+    """
+    candidates = [(ip_network("::/0"), n_items(counts, ip_network("::/0")))]
+    subsets = []
+
+    while candidates:
+        candidate, n_replies = candidates.pop()
+        if max_items_per_subset >= n_replies > 0:
+            subsets.append(candidate)
+        elif n_replies > 0:
+            a, b = tuple(candidate.subnets(prefixlen_diff=1))
+            n_items_a = n_items(counts, a)
+            n_items_b = n_items(counts, b)
+            if n_items_a + n_items_b == 0:
+                subsets.append(candidate)
+            else:
+                candidates.append((a, n_items_a))
+                candidates.append((b, n_items_b))
+
+    return sorted(subsets)
+
+
+def addr_to_network(addr: str, prefix_len_v4: int, prefix_len_v6: int) -> IPv6Network:
+    if addr.startswith("::ffff:"):
+        return IPv6Network(f"{addr}/{96+prefix_len_v4}")
+    return IPv6Network(f"{addr}/{prefix_len_v6}")
+
+
+def n_items(counts: Counts, subset: IPv6Network) -> int:
+    total = 0
+    for network, count_ in counts.items():
+        if network.subnet_of(subset):
+            total += count_
+    return total
