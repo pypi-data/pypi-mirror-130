67e6bbb80288476230fb41fbf28c28881905c23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package baizhanmath2guozhe/1.0/baizhanmath2guozhe-1.0.tar.gz

---
+++ packages/baizhanmath2guozhe/baizhanmath2guozhe-1.0.tar.gz/baizhanmath2guozhe-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="baizhanmath2guozhe",
+    version='1.0',
+    description='这是第一个对外发布的模块，测试用',
+    author='austinzheguo',
+    author_email='18701938090@163.com',
+    py_modules=['baizhanmath2guozhe.demo1', 'baizhanmath2guozhe.demo2']
+)
