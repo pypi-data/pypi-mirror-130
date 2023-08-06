877f9c07a7f09abc4cdda7ab7c8bf371c36624a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:03 +0000

    Add package nonebot-plugin-asoulcnki/0.1.1/nonebot_plugin_asoulcnki-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-asoulcnki/nonebot_plugin_asoulcnki-0.1.1.tar.gz/nonebot_plugin_asoulcnki-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_asoulcnki']
+
+package_data = \
+{'': ['*'], 'nonebot_plugin_asoulcnki': ['templates/*']}
+
+install_requires = \
+['Jinja2>=3.0.0,<4.0.0',
+ 'httpx>=0.19.0,<0.20.0',
+ 'nonebot-adapter-cqhttp>=2.0.0-alpha.15,<3.0.0',
+ 'nonebot2>=2.0.0-alpha.15,<3.0.0',
+ 'playwright>=1.9.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-asoulcnki',
+    'version': '0.1.1',
+    'description': 'ASoulCnki plugin for Nonebot2',
+    'long_description': None,
+    'author': 'meetwq',
+    'author_email': 'meetwq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
