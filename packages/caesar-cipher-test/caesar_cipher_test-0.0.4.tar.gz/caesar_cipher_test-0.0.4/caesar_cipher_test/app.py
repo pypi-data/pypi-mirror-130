dbfd643831139528ba949907c2546dea8e3a944e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:19 +0000

    Add package caesar-cipher-test/0.0.4/caesar_cipher_test-0.0.4.tar.gz

---
+++ packages/caesar-cipher-test/caesar_cipher_test-0.0.4.tar.gz/caesar_cipher_test-0.0.4/caesar_cipher_test/app.py
@@ -0,0 +1,41 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      #position of cipher text 
+      # % 26 for char more zan z in the alphabet 
+      new_position = (position + shift_amount) % 26
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  #terminate when the user not write in the above format encode or decode
+  if direction == 'encode' or direction == 'decode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    # %26 for shift value more than 26
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  else:
+      print('Wrong option.\n')
+  restart = input("Type 'y/Y' if you want to go again. Otherwise type 'n/N'.\n").lower()
+  if restart == "n":
+    should_end = True
+    print("Goodbye")
+    
+
+
---
+++ packages/caesar-cipher-test/caesar_cipher_test-0.0.4.tar.gz/caesar_cipher_test-0.0.4/setup.py
@@ -0,0 +1,35 @@
+"""
+MIT License
+
+Copyright (c) 2021 Berhanu Dagnew
+"""
+ 
+from setuptools import setup, find_packages
+# long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+# this_directory = Path(__file__).parent
+long_description = open('README.md').read()
+ 
+# See note below for more information about classifiers
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: POSIX :: Linux',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='caesar_cipher_test',
+  version='0.0.4',
+  description='A caesar cipher app and simple python calculator',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='',  # the URL of your package's home page e.g. github link
+  author='Berhanu Dagnew',
+  author_email='babipanda.learn@gmail.com',
+  license='MIT', # note the American spelling
+  classifiers=classifiers,
+  keywords='', # used when people are searching for a module, keywords separated with a space
+  packages=find_packages(),
+  install_requires=[''] # a list of other Python modules which this module depends on.  For example RPi.GPIO
+)
