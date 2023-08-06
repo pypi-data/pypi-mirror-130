3e95781ee9bd8cd4e94a55f5f0d9d389b24ce8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:21 +0000

    Add package gosu/1.1.6/gosu-1.1.6.tar.gz

---
+++ packages/gosu/gosu-1.1.6.tar.gz/gosu-1.1.6/tests/test_basic.py
@@ -0,0 +1,28 @@
+import os
+
+from gosu.cmds.ci import get_version, git_num_changes
+from gosu.cmds.python import _get_pkg_pyfiles, _manage_env_vars, env_from_sourcing
+
+
+def test_get_env_from_sourcing():
+    e = env_from_sourcing(".venv/bin/activate")
+    assert "PWD" in e
+
+
+def test_get_env_vars():
+    os.chdir("tests")
+    e = _manage_env_vars()
+    assert e["test"] == "1"
+
+
+def test_get_pkg_pyfiles():
+    e = _get_pkg_pyfiles()
+    assert len(e) > 0
+
+
+def test_get_version():
+    assert get_version() != ""
+
+
+def test_git_num_changes():
+    assert git_num_changes() != ""
