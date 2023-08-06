09be4ced098285d4edb99c883e149ad3aefc5403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:11 +0000

    Add package expressmoney/0.50.4/expressmoney-0.50.4.tar.gz

---
+++ packages/expressmoney/expressmoney-0.50.4.tar.gz/expressmoney-0.50.4/expressmoney/__init__.py
@@ -0,0 +1,3 @@
+from . import django
+from . import django2
+from . import django3
---
+++ packages/expressmoney/expressmoney-0.50.4.tar.gz/expressmoney-0.50.4/setup.py
@@ -0,0 +1,21 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-0.50.1.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney',
+    packages=setuptools.find_packages(),
+    version='0.50.4',
+    description='SDK ExpressMoney',
+    author='Sergey Zherebtsov',
+    author_email='sergey@expressmoney.com',
+    url='https://github.com/zsergey85/',
+    install_requires=('google-cloud-secret-manager', 'google-cloud-error-reporting', 'google-cloud-pubsub',
+                      'google-cloud-tasks', 'google-cloud-storage'),
+    download_url='https://github.com/zsergey85/',
+    keywords=('expressmoney',),
+    classifiers=(),
+    python_requires='>=3.7',
+)
