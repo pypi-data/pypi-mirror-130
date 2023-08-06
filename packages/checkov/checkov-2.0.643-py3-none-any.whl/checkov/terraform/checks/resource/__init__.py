6f197c82953b3cf90a2ad55b9132235dd2b60ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:59 +0000

    Add package checkov/2.0.643/checkov-2.0.643-py3-none-any.whl

---
+++ packages/checkov/checkov-2.0.643-py3-none-any.whl/checkov/terraform/checks/resource/__init__.py
@@ -0,0 +1,7 @@
+from checkov.terraform.checks.resource.aws import *
+from checkov.terraform.checks.resource.gcp import *
+from checkov.terraform.checks.resource.azure import *
+from checkov.terraform.checks.resource.github import *
+from checkov.terraform.checks.resource.linode import *
+from checkov.terraform.checks.resource.oci import *
+from checkov.terraform.checks.resource.openstack import *
