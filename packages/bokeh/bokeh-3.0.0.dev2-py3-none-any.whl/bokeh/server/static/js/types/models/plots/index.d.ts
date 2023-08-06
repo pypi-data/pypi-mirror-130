104228b104f58595e196b9d57c41a2447c044fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:38 +0000

    Add package bokeh/3.0.0.dev2/bokeh-3.0.0.dev2-py3-none-any.whl

---
+++ packages/bokeh/bokeh-3.0.0.dev2-py3-none-any.whl/bokeh/server/static/js/types/models/plots/index.d.ts
@@ -0,0 +1,6 @@
+export { MapOptions } from "./gmap_plot";
+export { GMapOptions } from "./gmap_plot";
+export { GMapPlot } from "./gmap_plot";
+export { Plot } from "./plot";
+export { Figure } from "./figure";
+//# sourceMappingURL=index.d.ts.map
