d97d5ec0b6aa1af2cebc24d1959439c2462e57f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:17 +0000

    Add package aiogram-pagination/0.1.7/aiogram-pagination-0.1.7.tar.gz

---
+++ packages/aiogram-pagination/aiogram-pagination-0.1.7.tar.gz/aiogram-pagination-0.1.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="aiogram-pagination",
+    version="0.1.7",
+    packages=find_packages(exclude=('tests', 'tests.*', 'examples.*', 'docs',)),
+    author="altcode",
+    author_email="cosmosx1328@gmail.com",
+    description="Module for deep pagination in an aiogram bot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/altroncode/aiogram-pagination",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries'
+
+    ],
+    include_package_data=False,
+    install_requires=[
+        'aiogram>=2.16',
+        'bestconfig>=1.3.5',
+    ]
+)
