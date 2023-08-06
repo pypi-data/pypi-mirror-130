fe3faf47ca04aca87e45a9ec4a2b4b316b8e0316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:03 +0000

    Add package cannyfier/0.2.5/cannyfier-0.2.5.tar.gz

---
+++ packages/cannyfier/cannyfier-0.2.5.tar.gz/cannyfier-0.2.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cannyfier',
+    version='0.2.5',
+    author='Pascual Martinez',
+    author_email='pascual.martinezzapata@gmail.com',
+    description='Show canny on any part of the screen. Better to use with video',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['cannyfier'],
+    install_requires=['numpy', 'opencv-python', 'mss', 'Pillow']
+)
