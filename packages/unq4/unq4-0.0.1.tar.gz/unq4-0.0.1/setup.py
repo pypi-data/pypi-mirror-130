47352381f68fb4d6485363e75b5ac5e654d6130e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:55 +0000

    Add package unq4/0.0.1/unq4-0.0.1.tar.gz

---
+++ packages/unq4/unq4-0.0.1.tar.gz/unq4-0.0.1/setup.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+from distutils.core import setup
+
+setup(
+    name="unq4",
+    version="0.0.1",
+
+    description="飞机大战",
+
+    author='纪念',
+    author_email="285151481@qq.com",
+    py_modules=['__init__','pd_unq1']
+
+)
