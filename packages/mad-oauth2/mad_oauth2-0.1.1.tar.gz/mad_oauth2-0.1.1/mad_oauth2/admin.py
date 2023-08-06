7da5373b9113b87e91dd66bcffcb011842b11ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:17 +0000

    Add package mad-oauth2/0.1.1/mad_oauth2-0.1.1.tar.gz

---
+++ packages/mad-oauth2/mad_oauth2-0.1.1.tar.gz/mad_oauth2-0.1.1/mad_oauth2/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from oauth2_provider.admin import ApplicationAdmin
+
+from mad_oauth2.models import Application, Scope, Throttle
+
+# Register your models here.
+
+@admin.register(Application)
+class ApplicationAdminClass(ApplicationAdmin):
+    filter_horizontal = ('allowed_scopes',)
+    readonly_fields = ['id']
+    list_display = ['id', 'name', 'client_type', 'authorization_grant_type', 'created_at']
+
+
+
+@admin.register(Scope)
+class ScopeAdminClass(admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = ['id', 'name', 'key', 'throttling',]
+
+
+@admin.register(Throttle)
+class ThrottleAdminClass(admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = ['id', 'name', 'throttling', ]
