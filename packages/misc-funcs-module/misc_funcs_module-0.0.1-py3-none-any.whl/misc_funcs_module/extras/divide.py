a65164a190eb5317032873f87672777235c461fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:05 +0000

    Add package misc-funcs-module/0.0.1/misc_funcs_module-0.0.1-py3-none-any.whl

---
+++ packages/misc-funcs-module/misc_funcs_module-0.0.1-py3-none-any.whl/misc_funcs_module/extras/divide.py
@@ -0,0 +1,6 @@
+def div(n,num):
+    """function that takes two two numbers and returns the result of the two"""
+    res = n / num
+    return res
+if __name__ == '__main__':
+    div()
---
+++ packages/misc-funcs-module/misc_funcs_module-0.0.1-py3-none-any.whl/misc_funcs_module/subtract.py
@@ -0,0 +1,6 @@
+def sub(n,num):
+    """function that takes two two numbers and returns the difference of the two"""
+    diff = n - num
+    return diff
+if __name__ == '__main__':
+    sub()
