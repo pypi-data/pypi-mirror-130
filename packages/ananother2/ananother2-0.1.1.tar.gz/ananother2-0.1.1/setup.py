b1ec3af0fa12a9fa9cbb9c66dee6e354af199770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:21 +0000

    Add package ananother2/0.1.1/ananother2-0.1.1.tar.gz

---
+++ packages/ananother2/ananother2-0.1.1.tar.gz/ananother2-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='ananother2',
+    version='0.1.1',
+    description='An another',
+    url='https://github.com/shuds13/pyexample',
+    author='Pasha',
+    author_email='swordingo@gmail.com',
+    license='BSD 2-clause',
+    packages=['ananother2'],
+    install_requires=['numpy>=1.20.2',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
