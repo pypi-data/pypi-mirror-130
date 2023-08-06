c458630b26d4b4e800f04f2474aa1adf48cd8984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package dapr-httpx/0.1.8/dapr_httpx-0.1.8.tar.gz

---
+++ packages/dapr-httpx/dapr_httpx-0.1.8.tar.gz/dapr_httpx-0.1.8/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dapr_httpx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.21.1,<0.22.0']
+
+setup_kwargs = {
+    'name': 'dapr-httpx',
+    'version': '0.1.8',
+    'description': '✨dapr ➕ ✨httpx is awesome',
+    'long_description': None,
+    'author': 'Ben',
+    'author_email': 'moon791017@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
