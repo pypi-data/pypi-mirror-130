9040986fe0fe6dd9ca5c309fdd486bc4426052ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:17 +0000

    Add package json-enhanced/0.1.74/json-enhanced-0.1.74.tar.gz

---
+++ packages/json-enhanced/json-enhanced-0.1.74.tar.gz/json-enhanced-0.1.74/jsonutils/config/queries.py
@@ -0,0 +1,5 @@
+include_parents = False
+recursive_queries = True
+clever_parsing = True
+query_exceptions = True
+native_types = False  # if True, the result of get method, or first, last, etc will be a python object instead of a jsonnode
