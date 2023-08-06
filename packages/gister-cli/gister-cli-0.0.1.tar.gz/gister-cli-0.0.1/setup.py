1f91ea45d78c38c5cea2fa8356f19d31ecf2a59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:30 +0000

    Add package gister-cli/0.0.1/gister-cli-0.0.1.tar.gz

---
+++ packages/gister-cli/gister-cli-0.0.1.tar.gz/gister-cli-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+# packages = ["gister"],
+# url="https://github.com/pypa/sampleproject",
+#project_urls={
+#    "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+#},
+setup(
+    name="gister-cli",
+    version="0.0.1",
+    author="Josh Andres",
+    author_email="josh.an.bas@gmail.com",
+    description="A awesome cli project.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'importlib; python_version == "3.6"',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gister=gister.cli:main'
+        ],
+    },
+)
