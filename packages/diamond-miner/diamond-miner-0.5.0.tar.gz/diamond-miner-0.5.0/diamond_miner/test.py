56913b26cbb5b79a6b8ed7b0b66e6c0bc7bb8ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:49 +0000

    Add package diamond-miner/0.5.0/diamond-miner-0.5.0.tar.gz

---
+++ packages/diamond-miner/diamond-miner-0.5.0.tar.gz/diamond-miner-0.5.0/diamond_miner/test.py
@@ -0,0 +1,10 @@
+url = "http://localhost:8123"
+
+
+def create_tables(url: str, measurement_id: str) -> None:
+    # Avoid circular imports.
+    from diamond_miner.queries.create_tables import CreateTables
+    from diamond_miner.queries.drop_tables import DropTables
+
+    DropTables().execute(url, measurement_id)
+    CreateTables().execute(url, measurement_id)
