ff5483940e070049c8ee5ed1d957879ce09833f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:29 +0000

    Add package plangym/0.0.14/plangym-0.0.14.tar.gz

---
+++ packages/plangym/plangym-0.0.14.tar.gz/plangym-0.0.14/tests/test_retro.py
@@ -0,0 +1,43 @@
+from typing import Union
+
+import pytest
+
+from plangym.parallel import ParallelEnvironment
+from plangym.retro import RetroEnvironment
+from tests.api_tests import batch_size, display, TestBaseEnvironment, TestGymEnvironment
+
+
+def retro_airstrike():
+    return RetroEnvironment(name="Airstriker-Genesis", obs_type="ram")
+
+
+def retro_sonic():
+    from plangym.wrappers.retro_wrappers import SonicDiscretizer
+
+    return RetroEnvironment(
+        name="SonicTheHedgehog-Genesis",
+        state="GreenHillZone.Act3",
+        wrappers=[SonicDiscretizer],
+    )
+
+
+# retro.make("Airstriker-Genesis")
+def parallel_retro():
+    return ParallelEnvironment(
+        name="Airstriker-Genesis",
+        env_class=RetroEnvironment,
+        n_workers=2,
+        delay_init=False,
+    )
+
+
+environments = [retro_airstrike, retro_sonic, parallel_retro]
+
+
+@pytest.fixture(params=environments, scope="class")
+def env(request) -> Union[RetroEnvironment, ParallelEnvironment]:
+    env_ = request.param()
+    if env_.delay_init and env_.gym_env is None:
+        env_.init_env()
+    yield env_
+    env_.close()
