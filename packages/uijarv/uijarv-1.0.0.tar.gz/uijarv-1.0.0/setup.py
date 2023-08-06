f1db016b9bc9165bb59f26bb52f21e71cc9a3d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package uijarv/1.0.0/uijarv-1.0.0.tar.gz

---
+++ packages/uijarv/uijarv-1.0.0.tar.gz/uijarv-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Installs my jarvis code on your system'
+LONG_DESCRIPTION = 'A package to install Jarvis on your system'
+
+# Setting up
+setup(
+    name="uijarv",
+    version=VERSION,
+    author="Aditya Garg",
+    author_email="adityagarg1165@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyttsx3','speechrecognition','wikipedia','Pyqt5','Pyqt5-tools','pyjokes','pyautogui'],
+    keywords=['Jarvis', 'jarviswithgui'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
