ebbf68d72c88bb4a58ce2bef6766c83e52988248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package django-cleavejs/0.1.1/django-cleavejs-0.1.1.tar.gz

---
+++ packages/django-cleavejs/django-cleavejs-0.1.1.tar.gz/django-cleavejs-0.1.1/example/dj_cleavejs_example/forms.py
@@ -0,0 +1,14 @@
+from django.forms import CharField, Form
+
+from dj_cleavejs import *
+
+
+class TestForm(Form):
+    windows_xp_serial = CharField(
+        widget=CleaveWidget(blocks=[5, 5, 5, 5, 5], delimiter="-", uppercase=True)
+    )
+    windows_95_serial = CharField(
+        widget=CleaveWidget(blocks=[3, 7], delimiter="-", numericOnly=True)
+    )
+
+    credit_card = CharField(widget=CreditCard())
