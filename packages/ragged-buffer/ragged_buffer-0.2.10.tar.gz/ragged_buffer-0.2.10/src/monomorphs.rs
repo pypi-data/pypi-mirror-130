5276f4f063598580d3c51f0db7e0de93e833df10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package ragged-buffer/0.2.10/ragged_buffer-0.2.10.tar.gz

---
+++ packages/ragged-buffer/ragged_buffer-0.2.10.tar.gz/ragged_buffer-0.2.10/src/monomorphs.rs
@@ -0,0 +1,14 @@
+use numpy::PyReadonlyArray1;
+use pyo3::FromPyObject;
+
+mod f32;
+mod i64;
+
+pub use self::f32::RaggedBufferF32;
+pub use self::i64::RaggedBufferI64;
+
+#[derive(FromPyObject)]
+pub enum IndicesOrInt<'a> {
+    Indices(PyReadonlyArray1<'a, i64>),
+    Int(usize),
+}
