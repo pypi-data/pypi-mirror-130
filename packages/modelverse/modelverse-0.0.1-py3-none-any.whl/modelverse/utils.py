a83eecc0b471733fd5340cd1623d5b075497aa6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:32 +0000

    Add package modelverse/0.0.1/modelverse-0.0.1-py3-none-any.whl

---
+++ packages/modelverse/modelverse-0.0.1-py3-none-any.whl/modelverse/utils.py
@@ -0,0 +1,12 @@
+def hw(x=1):
+    """
+    Hello world function.
+
+    Args:
+        x (int): num times to print
+
+    Returns:
+        None
+
+    """
+    print("Welcome to the modelverse!" * x)
