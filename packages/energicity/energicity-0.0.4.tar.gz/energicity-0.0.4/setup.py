28c0c386ad07b000b2e0ba1dec086938ab6cd84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:30 +0000

    Add package energicity/0.0.4/energicity-0.0.4.tar.gz

---
+++ packages/energicity/energicity-0.0.4.tar.gz/energicity-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='energicity',
+    version='0.0.4',
+    description="EnergiCity main python package, it's a toolkit",
+    py_modules=["hello", "powerleone"],
+    package_dir={"":"src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.com/EnergiCity/energicity-py-package',
+    author="Ahmed Ali",
+    author_email='ahmed.elsir.khalfalla@gmail.com'
+)
