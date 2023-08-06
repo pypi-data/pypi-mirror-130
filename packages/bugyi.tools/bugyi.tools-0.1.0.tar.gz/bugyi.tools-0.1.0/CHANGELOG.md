2329f4479997c3bb3e82012655b84473f088db12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:00 +0000

    Add package bugyi.tools/0.1.0/bugyi.tools-0.1.0.tar.gz

---
+++ packages/bugyi.tools/bugyi.tools-0.1.0.tar.gz/bugyi.tools-0.1.0/CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog for `bugyi.tools`
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog], and this project adheres to
+[Semantic Versioning].
+
+[Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
+[Semantic Versioning]: https://semver.org/
+
+
+## [Unreleased](https://github.com/bbugyi200/python-tools/compare/0.1.0...HEAD)
+
+No notable changes have been made.
+
+
+## [0.1.0](https://github.com/bbugyi200/python-tools/releases/tag/0.1.0) - 2021-12-11
+
+### Added
+
+* Add the `shlib` tool to dynamically source bash libraries.
+
+### Miscellaneous
+
+* First release.
---
+++ packages/bugyi.tools/bugyi.tools-0.1.0.tar.gz/bugyi.tools-0.1.0/src/bugyi/tools/shlib.py
@@ -0,0 +1,50 @@
+"""Used to dynamically source bash libraries.
+
+Examples:
+    # Source default bash library...
+    source $(shlib)
+
+    # Source "foo.sh" bash library...
+    source $(shlib foo)
+"""
+
+from importlib.resources import read_text
+from typing import Sequence
+
+import clap
+from pydantic.dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Arguments(clap.Arguments):
+    """Command-line arguments."""
+
+    library_name: str
+
+
+def parse_cli_args(argv: Sequence[str]) -> Arguments:
+    """Parses command-line arguments."""
+    parser = clap.Parser()
+    parser.add_argument(
+        "library_name",
+        default="bugyi",
+        nargs="?",
+        help=(
+            "The basename (i.e. no extension) of the bash library you want to"
+            " use."
+        ),
+    )
+
+    args = parser.parse_args(argv[1:])
+    kwargs = vars(args)
+
+    return Arguments(**kwargs)
+
+
+def run(args: Arguments) -> int:
+    """This function acts as this tool's main entry point."""
+    print(read_text("bugyi.tools.data.shlib", args.library_name + ".sh"))
+    return 0
+
+
+main = clap.main_factory(parse_cli_args, run)
