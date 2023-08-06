9b72ce39e2b506bb62632445686318a2e75cd98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:23 +0000

    Add package pywebvue/2.5.0/pywebvue-2.5.0.tar.gz

---
+++ packages/pywebvue/pywebvue-2.5.0.tar.gz/pywebvue-2.5.0/src/pywebvue/www/js/chunk-74835bae.ca9bbe94.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-74835bae"],{5363:function(n,w,o){}}]);
+//# sourceMappingURL=chunk-74835bae.ca9bbe94.js.map
---
+++ packages/pywebvue/pywebvue-2.5.0.tar.gz/pywebvue-2.5.0/src/pywebvue/www/js/chunk-771ac683.2979b96b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-771ac683"],{d4b8:function(n,w,c){}}]);
+//# sourceMappingURL=chunk-771ac683.2979b96b.js.map
