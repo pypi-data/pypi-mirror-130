dfde717d643f32b5ee1be18eace2583b72473085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package app-image-generator/0.2.14/app-image-generator-0.2.14.tar.gz

---
+++ packages/app-image-generator/app-image-generator-0.2.14.tar.gz/app-image-generator-0.2.14/app_image_generator/plugins/slack.py
@@ -0,0 +1,36 @@
+import os
+from slacker import Slacker
+from app_image_generator.plugins.base import BasePlugin
+
+
+class SlackPlugin(BasePlugin):
+    def __init__(self):
+        self.slacker = Slacker(os.environ.get("SLACK_TOKEN"))
+        self.channel = os.environ.get("SLACK_CHANNEL")
+
+    def build_succeeded(self, app, output):
+        self.slacker.chat.post_message(self.channel or '#dev', 'New %s images available:' % app, username='app-image-generator', attachments=[
+            {
+                "fields": [
+                    {
+                        "title": image_name,
+                        "value": image_id,
+                        "short": True
+                    } for image_name, image_id in self.get_images_from_output(output).items()
+                ],
+            }
+        ])
+
+    def build_failed(self, app, output):
+        self.slacker.chat.post_message(self.channel or '#dev', 'Building %s failed!!!!' % app, username='app-image-generator', attachments=[
+            {
+                "fields": [
+                    {
+                        "title": image_name,
+                        "value": image_id,
+                        "short": True
+                    } for image_name, image_id in self.get_images_from_output(output).items()
+                ],
+                "color": "#F35A00"
+            }
+        ])
