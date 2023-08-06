e18e1ee19d4ced0ba60b1a93a281e1fd867a7cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:19 +0000

    Add package search-me/2.1/search_me-2.1-py3-none-any.whl

---
+++ packages/search-me/search_me-2.1-py3-none-any.whl/search_me/__init__.py
@@ -0,0 +1,8 @@
+from .api import Searx, Google, Rambler
+
+__author__ = "Michael R. Kisel"
+__license__ = "MIT"
+__version__ = "2.1"
+__maintainer__ = "Michael R. Kisel"
+__email__ = "deploy-me@yandex.ru"
+__status__ = "Stable"
