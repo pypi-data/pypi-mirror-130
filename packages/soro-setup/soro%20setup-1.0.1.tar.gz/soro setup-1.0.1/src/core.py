cf6a23e84f5df34a6fa0bdc02e4eb5917131b3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:18 +0000

    Add package soro-setup/1.0.1/soro%20setup-1.0.1.tar.gz

---
+++ packages/soro-setup/soro%20setup-1.0.1.tar.gz/soro setup-1.0.1/src/core.py
@@ -0,0 +1,15 @@
+
+def get_message():
+    return "Hello World!"
+
+
+def print_message():
+    print(get_message())
+
+class Hello():
+    def __init__(self):
+        self.name = "Andrii"
+
+    def get_name(self):
+        return self.name
+
