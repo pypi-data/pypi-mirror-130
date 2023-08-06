100e59d85a14db0ce3afa8d800db28da64169e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package sitzungsdienst/1.7.0/sitzungsdienst-1.7.0-py3-none-any.whl

---
+++ packages/sitzungsdienst/sitzungsdienst-1.7.0-py3-none-any.whl/sitzungsdienst/__init__.py
@@ -0,0 +1,10 @@
+from .sta import Sitzungsdienst
+from .utils import data2calendar, dump_csv, dump_json, dump_ics
+
+__all__ = [
+    'Sitzungsdienst',
+    'data2calendar',
+    'dump_csv',
+    'dump_json',
+    'dump_ics',
+]
