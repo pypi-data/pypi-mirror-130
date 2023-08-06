7da99f26e605786eea91329cfd9f54d315730981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:43 +0000

    Add package khang-simple-pkg-hello-world/1.0/khang_simple_pkg_hello_world-1.0.tar.gz

---
+++ packages/khang-simple-pkg-hello-world/khang_simple_pkg_hello_world-1.0.tar.gz/khang_simple_pkg_hello_world-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='khang_simple_pkg_hello_world',
+    version='1.0',
+    license='MIT',
+    author="Khang",
+    author_email='',
+    package_dir={'': 'src'},
+    url='',
+    packages=['khangsimplepkghello'],
+    keywords='simple hello',
+    install_requires=[
+      ],
+
+)
