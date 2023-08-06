dcd9d880993a96fb788e479aaba5b7e46f5b84fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:17 +0000

    Add package Sectumsempra/0.1.3/Sectumsempra-0.1.3.tar.gz

---
+++ packages/Sectumsempra/Sectumsempra-0.1.3.tar.gz/Sectumsempra-0.1.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Sectumsempra',
+    version='0.1.3',
+    description=(
+        'utils for nlp'
+    ),
+    author='liyuhao',
+    author_email='1241225413@qq.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    url='https://github.com/hgliyuhao',
+    install_requires=[
+        'xlrd==1.2.0',
+        'xlwt',
+        'xlutils',
+        'numpy'
+    ],
+)
