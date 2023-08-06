23d6f396a39f4b4251fc96cd843f9f775a4d461a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package test-package-pigeon/0.0.1/test_package_pigeon-0.0.1.tar.gz

---
+++ packages/test-package-pigeon/test_package_pigeon-0.0.1.tar.gz/test_package_pigeon-0.0.1/test_package_pigeon/__init__.py
@@ -0,0 +1,10 @@
+from .module import *
+
+__copyright__    = 'Copyright (C) 2021 Yudai Nagano'
+__version__      = '0.0.1'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Yudai Nagano'
+__author_email__ = 'alchemic4s@gmail.com'
+__url__          = 'http://github.com/account/repository'
+
+__all__ = [ClassA, func_a]
