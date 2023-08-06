5b229a5df9f20f85f1327c4becc28f6792f1f8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:36 +0000

    Add package uproot/4.1.9/uproot-4.1.9.tar.gz

---
+++ packages/uproot/uproot-4.1.9.tar.gz/uproot-4.1.9/tests/test_0066-fix-http-fallback-freeze.py
@@ -0,0 +1,17 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import numpy
+import pytest
+
+import uproot
+
+
+@pytest.mark.network
+def test():
+    with uproot.open(
+        {"http://scikit-hep.org/uproot3/examples/HZZ.root": "events"}
+    ) as t:
+        t["MET_px"].array(library="np")
+        t["MET_py"].array(library="np")
---
+++ packages/uproot/uproot-4.1.9.tar.gz/uproot-4.1.9/tests/test_0123-atlas-issues.py
@@ -0,0 +1,17 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest
+import skhep_testdata
+
+import uproot
+
+
+def test_version():
+    assert uproot.classname_decode(
+        uproot.classname_encode("xAOD::MissingETAuxAssociationMap_v2")
+    ) == ("xAOD::MissingETAuxAssociationMap_v2", None)
+    assert uproot.classname_decode(
+        uproot.classname_encode("xAOD::MissingETAuxAssociationMap_v2", 9)
+    ) == ("xAOD::MissingETAuxAssociationMap_v2", 9)
