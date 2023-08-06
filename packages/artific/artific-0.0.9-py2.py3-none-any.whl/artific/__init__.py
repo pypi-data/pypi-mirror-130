cd9db0be7bb01f0038ea61a0bd18fc4200e7232a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:21 +0000

    Add package artific/0.0.9/artific-0.0.9-py2.py3-none-any.whl

---
+++ packages/artific/artific-0.0.9-py2.py3-none-any.whl/artific/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for artific."""
+
+__author__ = """Nipun Waas"""
+__email__ = 'waasnipun@gmail.com'
+__version__ = '0.0.8'
+
+from artific.artific import BubbleSort as BubbleSort
