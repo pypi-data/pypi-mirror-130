af85a82642da676a5608d7f698101a97bc683a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:07 +0000

    Add package katsdpsigproc/1.4.2/katsdpsigproc-1.4.2.tar.gz

---
+++ packages/katsdpsigproc/katsdpsigproc-1.4.2.tar.gz/katsdpsigproc-1.4.2/katsdpsigproc/asyncio/__init__.py
@@ -0,0 +1,4 @@
+"""Utilities for working with :mod:`asyncio`."""
+
+# Automatically added by katversion
+__version__ = '1.4.2'
---
+++ packages/katsdpsigproc/katsdpsigproc-1.4.2.tar.gz/katsdpsigproc-1.4.2/katsdpsigproc/rfi/test/__init__.py
@@ -0,0 +1,4 @@
+"""Unit tests for :mod:`katsdpsigproc.rfi`."""
+
+# Automatically added by katversion
+__version__ = '1.4.2'
