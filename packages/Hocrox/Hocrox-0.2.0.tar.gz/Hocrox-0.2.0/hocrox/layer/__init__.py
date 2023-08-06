d177afd50f3afa15517816aaed5898573133d06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:03 +0000

    Add package Hocrox/0.2.0/Hocrox-0.2.0.tar.gz

---
+++ packages/Hocrox/Hocrox-0.2.0.tar.gz/Hocrox-0.2.0/hocrox/layer/__init__.py
@@ -0,0 +1,24 @@
+"""Layers are the fundamental blocks of Hocrox image preprocessing and augmentation library.
+
+In Hocrox, each layer basically means a function that will be performed on the images to preprocess or/and augment.
+
+Currently, in Hocrox, there are several layers. The complete list is below.
+
+- Preprocessing
+    - Crop
+    - Grayscale
+    - Horizontal Flip
+    - Padding
+    - Resize
+    - Rotate
+    - Save
+    - Vertical Flip
+- Augmentation
+    - RandomFlip
+    - RandomRotate
+"""
+
+from . import preprocessing
+from . import augmentation
+from .read import Read
+from .save import Save
