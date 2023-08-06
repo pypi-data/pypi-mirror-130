036c3f83c1b3ab02abec41d8913ff7f01f0bf7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:22 +0000

    Add package yet-another-ini/1.0/yet_another_ini-1.0.tar.gz

---
+++ packages/yet-another-ini/yet_another_ini-1.0.tar.gz/yet_another_ini-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name="yet_another_ini",
+    version="1.0",
+    license="gpl-3.0",
+    url="https://github.com/megahomyak/yet_another_ini",
+    download_url=(
+        "https://github.com/megahomyak/yet_another_ini"
+        "/archive/refs/tags/1.0.tar.gz"
+    )
+)
