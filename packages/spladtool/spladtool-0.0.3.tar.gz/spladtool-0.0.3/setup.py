9bbc35aa0dea8ae94a3b98288014af90bfa8d7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:29 +0000

    Add package spladtool/0.0.3/spladtool-0.0.3.tar.gz

---
+++ packages/spladtool/spladtool-0.0.3.tar.gz/spladtool-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name = "spladtool",
+    version = "0.0.3",
+    author = "Raymond Jow, Rye Julson, Shihan Lin, Yuanbiao Wang",
+    author_email = "rjow@college.harvard.edu",
+    description = "The Simple Pytorch-Like Auto Differentiation Toolkit is an automatic differentiation package for calculating gradients of a function in forward and reverse mode.",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/cs107-rysr/cs107-FinalProject",
+    license = "MIT",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires = ">=3.7",
+    install_requires = ["numpy"],
+)
