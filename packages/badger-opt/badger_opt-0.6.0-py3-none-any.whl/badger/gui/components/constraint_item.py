4791da3aa431ad454ed319d242c7321292997e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:00 +0000

    Add package badger-opt/0.6.0/badger_opt-0.6.0-py3-none-any.whl

---
+++ packages/badger-opt/badger_opt-0.6.0-py3-none-any.whl/badger/gui/components/constraint_item.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget, QDoubleSpinBox, QAbstractSpinBox
+from PyQt5.QtWidgets import QComboBox, QStyledItemDelegate
+
+
+def constraint_item(options, remove_item, name=None, relation=0, threshold=0, decimals=4):
+    # relation: 0 for >, 1 for <, 2 for =
+    widget = QWidget()
+    hbox = QHBoxLayout(widget)
+    # hbox.setContentsMargins(0, 0, 0, 0)
+    # hbox.setSpacing(0)
+    widget.cb_obs = cb_obs = QComboBox()
+    cb_obs.setItemDelegate(QStyledItemDelegate())
+    cb_obs.addItems(options)
+    try:
+        idx = options.index(name)
+    except:
+        idx = 0
+    cb_obs.setCurrentIndex(idx)
+
+    widget.cb_rel = cb_rel = QComboBox()
+    cb_rel.setItemDelegate(QStyledItemDelegate())
+    cb_rel.addItems(['>', '<', '='])
+    cb_rel.setFixedWidth(64)
+    cb_rel.setCurrentIndex(relation)
+
+    widget.sb = sb = QDoubleSpinBox()
+    sb.setDecimals(decimals)
+    default_value = threshold
+    lb = default_value - 1e3
+    ub = default_value + 1e3
+    sb.setRange(lb, ub)
+    sb.setStepType(QAbstractSpinBox.AdaptiveDecimalStepType)
+    sb.setValue(default_value)
+
+    widget.btn_del = btn_del = QPushButton('Remove')
+    btn_del.setFixedSize(72, 24)
+
+    hbox.addWidget(cb_obs, 1)
+    hbox.addWidget(cb_rel)
+    hbox.addWidget(sb, 1)
+    hbox.addWidget(btn_del)
+
+    btn_del.clicked.connect(remove_item)
+
+    return widget
