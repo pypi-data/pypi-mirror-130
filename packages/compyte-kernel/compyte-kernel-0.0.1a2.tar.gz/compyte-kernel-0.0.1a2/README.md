f4a81e46246c8cb80a7e2c51bee5398f379bb0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:27 +0000

    Add package compyte-kernel/0.0.1a2/compyte-kernel-0.0.1a2.tar.gz

---
+++ packages/compyte-kernel/compyte-kernel-0.0.1a2.tar.gz/compyte-kernel-0.0.1a2/README.md
@@ -0,0 +1,14 @@
+# compyte-kernel
+
+VERY UNSTABLE! 
+
+## Installation
+`$ pip install compyte-kernel `
+
+### Example 
+`>>> import compyte_kernel.lib as ck`
+
+`>>> ck.execute(2, 5135, 107)`
+
+
+
---
+++ packages/compyte-kernel/compyte-kernel-0.0.1a2.tar.gz/compyte-kernel-0.0.1a2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="compyte-kernel",
+    version="0.0.1-alpha2",
+    author="AutomaCoin Devs",
+    author_email="alessio.proietti@protonmail.com",
+    description="A compute kernel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/automacoin/compyte-kernel",
+    project_urls={
+        "Bug Tracker": "https://github.com/automacoin/compyte-kernel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
