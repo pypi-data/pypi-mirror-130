3c4b686e7679a18da1eb0c77e7d55fde796eb18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:29 +0000

    Add package pdm/1.11.1/pdm-1.11.1-py3-none-any.whl

---
+++ packages/pdm/pdm-1.11.1-py3-none-any.whl/pdm/_types.py
@@ -0,0 +1,40 @@
+import sys
+from typing import Dict, List, NamedTuple, Tuple, Union
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import Distribution
+    from typing import Literal, Protocol, TypedDict
+else:
+    from importlib_metadata import Distribution
+    from typing_extensions import Literal, Protocol, TypedDict
+
+
+class Source(TypedDict, total=False):
+    url: str
+    verify_ssl: bool
+    name: str
+    type: Union[Literal["index"], Literal["find_links"]]
+
+
+RequirementDict = Union[str, Dict[str, Union[bool, str]]]
+CandidateInfo = Tuple[List[str], str, str]
+
+
+class Package(NamedTuple):
+    name: str
+    version: str
+    summary: str
+
+
+SearchResult = List[Package]
+
+__all__ = (
+    "Literal",
+    "Source",
+    "RequirementDict",
+    "CandidateInfo",
+    "Distribution",
+    "Package",
+    "SearchResult",
+    "Protocol",
+)
