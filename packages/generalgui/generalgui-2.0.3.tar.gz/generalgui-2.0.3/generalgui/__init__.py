19b149d6b740f9d4ca7b46e150dbaf66737c6fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:44 +0000

    Add package generalgui/2.0.3/generalgui-2.0.3.tar.gz

---
+++ packages/generalgui/generalgui-2.0.3.tar.gz/generalgui-2.0.3/generalgui/__init__.py
@@ -0,0 +1,11 @@
+from generalgui.elements.page import Page
+from generalgui.elements.label import Label
+from generalgui.elements.button import Button
+from generalgui.elements.checkbutton import Checkbutton
+from generalgui.elements.entry import Entry
+from generalgui.elements.subelements.password import Password
+
+from generalgui.pages.plot import Plot
+
+class MethodGrouper(Plot, Page, Label, Button, Checkbutton, Password, Entry):
+    pass
