97fa85223e4ab24111a58411a82a7993bb98c1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:10 +0000

    Add package business-rules-harishbohara/0.0.1/business-rules-harishbohara-0.0.1.tar.gz

---
+++ packages/business-rules-harishbohara/business-rules-harishbohara-0.0.1.tar.gz/business-rules-harishbohara-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import setuptools
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+description = 'Python DSL for setting up business intelligence rules that can be configured without code'
+
+setuptools.setup(
+    name='business-rules-harishbohara',
+    version="0.0.1",
+    description='{0}\n\n{1}'.format(description, history),
+    author='Harish Bohara',
+    author_email='harish.bohara@gmail.com',
+    url='https://github.com/harishb2k/business-rules',
+    packages=['business_rules'],
+    package_dir={"": "."},
+    license='MIT'
+)
