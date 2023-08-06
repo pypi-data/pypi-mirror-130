005f0636305e1a22f9fc8a1ee1a24321477cff38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:17 +0000

    Add package qpvdi/0.0.6/qpvdi-0.0.6-py3-none-any.whl

---
+++ packages/qpvdi/qpvdi-0.0.6-py3-none-any.whl/qpvdi/__init__.py
@@ -0,0 +1,9 @@
+from .injector import component as component
+from .injector import inject as inject
+from .injector import get as get
+from .injector import single as single
+from .injector import factory as factory
+from .injector import auto_single as auto_single
+from .injector import auto_factory as auto_factory
+
+__version__ = "0.0.6"
