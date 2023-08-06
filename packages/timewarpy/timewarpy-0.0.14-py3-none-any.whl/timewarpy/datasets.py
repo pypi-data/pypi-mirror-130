111210d17cdc26d67f4dad0828ffe51b63d7c48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:36 +0000

    Add package timewarpy/0.0.14/timewarpy-0.0.14-py3-none-any.whl

---
+++ packages/timewarpy/timewarpy-0.0.14-py3-none-any.whl/timewarpy/datasets.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from timewarpy import sample_data
+
+
+def load_energy_data():
+    """Loads a multi-variate time series dataset from the timewarpy library
+    for energy usage forecasting.
+
+    More information can be found [here](https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction)
+
+    Returns:
+        pandas.DataFrame: time series dataset
+    """
+    return pd.read_csv(sample_data.__path__[0] + '/energydata_complete.csv')
