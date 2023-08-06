2f75caebfabb36b44cb10a257d8c8eb4cf1d55d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:03 +0000

    Add package jommerce/0.1.0/jommerce-0.1.0.tar.gz

---
+++ packages/jommerce/jommerce-0.1.0.tar.gz/jommerce-0.1.0/jommerce/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = SECRETS.get("ALLOWED_HOSTS", [])
+
+
+STATIC_ROOT = SECRETS["STATIC_ROOT"]
+MEDIA_ROOT = SECRETS["MEDIA_ROOT"]
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
