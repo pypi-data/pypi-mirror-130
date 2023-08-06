994b8143d18fa3d87cb718e1e78d6e4184171aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:44 +0000

    Add package PyMyORM/1.0.7/PyMyORM-1.0.7-py3-none-any.whl

---
+++ packages/PyMyORM/PyMyORM-1.0.7-py3-none-any.whl/tests/my_update.py
@@ -0,0 +1,20 @@
+from pymyorm.database import Database
+from config import db
+from models.user import User
+
+
+def main():
+
+    Database().connect(**db)
+
+    # # case 1
+    # one = User.find().where(name='lily').one()
+    # one.money = 500
+    # one.save()
+
+    # case 2
+    User.find().where(name='lily').update(money=180)
+
+
+if __name__ == '__main__':
+    main()
