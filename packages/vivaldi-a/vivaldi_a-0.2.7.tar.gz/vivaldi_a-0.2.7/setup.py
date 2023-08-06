1142fc672b5f7abf7088fb4349a1f604eb6e403e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:13 +0000

    Add package vivaldi-a/0.2.7/vivaldi_a-0.2.7.tar.gz

---
+++ packages/vivaldi-a/vivaldi_a-0.2.7.tar.gz/vivaldi_a-0.2.7/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="vivaldi_a",
+    version="0.2.7",
+    description="Visualization Validation and Diagnostics for Atmospheric chemistry",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Rebecca Buchholz;Duseong Jo",
+    author_email="cdswk@ucar.edu",
+    url='https://ncar.github.io/CAM-chem/',
+    packages=find_packages(),
+    keywords=['atmospheric chemistry', 'cesm', 'cam-chem', 'air quality', 'emission'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'xarray',
+        'cartopy',
+        'cftime',
+        'ESMPy>=8.1.0',
+        'netCDF4' ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Utilities"
+    ]
+)
