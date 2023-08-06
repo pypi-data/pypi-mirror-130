ed276ecee791c6639cfd3cd77e3f124e0d2f777d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package batoid/0.3.7/batoid-0.3.7.tar.gz

---
+++ packages/batoid/batoid-0.3.7.tar.gz/batoid-0.3.7/src/bicubic.cpp
@@ -0,0 +1,68 @@
+#include <new>
+#include "bicubic.h"
+
+
+namespace batoid {
+
+    #if defined(BATOID_GPU)
+        #pragma omp declare target
+    #endif
+
+    Bicubic::Bicubic(
+        const Table* table
+    ) :
+        Surface(), _table(table)
+    {}
+
+    Bicubic::~Bicubic() {}  // don't own _table
+
+    double Bicubic::sag(double x, double y) const {
+        return _table->eval(x, y);
+    }
+
+    void Bicubic::normal(
+        double x, double y,
+        double& nx, double& ny, double& nz
+    ) const {
+        double dxdz, dydz;
+        _table->grad(x, y, dxdz, dydz);
+        if (std::isnan(dxdz)) {
+            nx = NAN;
+            ny = NAN;
+            nz = NAN;
+            return;
+        }
+
+        // This works
+        double norm = dxdz*dxdz;
+        norm += dydz*dydz;
+        norm += 1;
+        norm = 1/std::sqrt(norm);
+
+        nx = -dxdz*norm;
+        ny = -dydz*norm;
+        nz = norm;
+    }
+
+    #if defined(BATOID_GPU)
+        #pragma omp end declare target
+    #endif
+
+    const Surface* Bicubic::getDevPtr() const {
+        #if defined(BATOID_GPU)
+            if (!_devPtr) {
+                Surface* ptr;
+                const Table* tableDevPtr = _table->getDevPtr();
+
+                #pragma omp target map(from:ptr) is_device_ptr(tableDevPtr)
+                {
+                    ptr = new Bicubic(tableDevPtr);
+                }
+                _devPtr = ptr;
+            }
+            return _devPtr;
+        #else
+            return this;
+        #endif
+    }
+}
