b79e82feb6322ed1c0e73a99a7f8802d18c39512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:08 +0000

    Add package aoc-helper/1.2.14/aoc_helper-1.2.14.tar.gz

---
+++ packages/aoc-helper/aoc_helper-1.2.14.tar.gz/aoc_helper-1.2.14/aoc_helper/__init__.py
@@ -0,0 +1,13 @@
+from .interface import fetch, lazy_submit, submit
+from .utils import (
+    chunk,
+    chunk_default,
+    extract_ints,
+    frange,
+    irange,
+    iter,
+    list,
+    map,
+    range,
+    tail_call,
+)
