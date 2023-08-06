6ea02b83dc7189d80b77974e5c684d7799781bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:31 +0000

    Add package flask-cdc-devlibx/0.0.3/flask-cdc-devlibx-0.0.3.tar.gz

---
+++ packages/flask-cdc-devlibx/flask-cdc-devlibx-0.0.3.tar.gz/flask-cdc-devlibx-0.0.3/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import setuptools
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+description = 'Python DSL for setting up Flask app CDC'
+
+setuptools.setup(
+    name='flask-cdc-devlibx',
+    version="0.0.3",
+    description='{0}\n\n{1}'.format(description, history),
+    author='devlibx',
+    author_email='devlibxgithub@gmail.com',
+    url='https://github.com/devlibx/flask-cdc',
+    packages=['flask_cdc'],
+    package_dir={"": "."},
+    license='MIT',
+    install_requires=['kafka-python']
+)
