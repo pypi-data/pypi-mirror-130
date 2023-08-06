4b1888e47de7f7e9ef46e7c57c1b262481deeba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:25 +0000

    Add package kubernetes-validate/1.22.1rc1/kubernetes-validate-1.22.1rc1.tar.gz

---
+++ packages/kubernetes-validate/kubernetes-validate-1.22.1rc1.tar.gz/kubernetes-validate-1.22.1rc1/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+import sys
+
+
+sys.path.insert(0, os.path.abspath('lib'))
+
+
+setup(
+    name='kubernetes-validate',
+    description=('validates kubernetes resource definitions against schemas'),
+    keywords='kubernetes, schema, validate, validator',
+    author='Will Thames',
+    url='https://github.com/willthames/kubernetes-validate',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        '': ['kubernetes-json-schema/*-local/*.json', 'kubernetes-json-schema/*-local-strict/*.json']
+    },
+    zip_safe=False,
+    install_requires=['PyYAML', 'jsonschema'],
+    entry_points={
+        'console_scripts': [
+             'kubernetes-validate=kubernetes_validate.__main__:main'
+        ]
+    },
+    license='Apache',
+    test_suite="test"
+)
