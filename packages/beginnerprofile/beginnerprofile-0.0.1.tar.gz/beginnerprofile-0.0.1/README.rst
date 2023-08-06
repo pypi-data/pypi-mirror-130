5210ee2683340b3c77e614f56d08464f8e14ff3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:21 +0000

    Add package beginnerprofile/0.0.1/beginnerprofile-0.0.1.tar.gz

---
+++ packages/beginnerprofile/beginnerprofile-0.0.1.tar.gz/beginnerprofile-0.0.1/README.rst
@@ -0,0 +1,34 @@
+(Beginner Profile) Example upload package to Pypi.org
+=====================================================
+
+PyPi: https://pypi.org/project/beginnerprofile/
+
+This package show profile of me.
+
+How to install
+~~~~~~~~~~~~~~
+
+Open CMD / Terminal
+
+.. code:: python
+
+   pip install beginnerprofile
+
+How to use this package
+~~~~~~~~~~~~~~~~~~~~~~~
+
+-  Open IDLE then input…
+
+.. code:: python
+
+   my = Profile('Ton')
+   my.company = 'goodday'
+   my.hobby = ['Running','Learning','Travelling']
+   print(my.name)
+   my.show_email()
+   my.show_myart()
+   my.show_hobby()
+
+developed by: Kerksak Mattayangkul Linkedin:
+
+https://www.linkedin.com/in/kerksak-mattayangkul-5080821b0/
---
+++ packages/beginnerprofile/beginnerprofile-0.0.1.tar.gz/beginnerprofile-0.0.1/beginnerprofile/myprofile.py
@@ -0,0 +1,67 @@
+# myprofile.py
+
+class Profile:
+	'''
+
+	Example:
+
+	my = Profile('Ton')
+	my.company = 'goodday'
+	my.hobby = ['Running','Learning','Travelling']
+	print(my.name)
+	my.show_email()
+	my.show_myart()
+	my.show_hobby()
+
+	'''
+
+	def __init__(self,name):
+		self.name = name
+		self.company = ''
+		self.hobby = []
+		self.art = '''
+		
+             ,-._
+           _.-'  '--.
+         .'      _  -`\_
+        / .----.`_.'----'
+        ;/     `
+ Whale   /_;
+
+		'''
+
+	def show_email(self):
+		if self.company != '':
+			print('{}@{}.com'.format(self.name.lower(),self.company))
+		else: 
+			print('{}@gmail.com'.format(self.name.lower()))
+
+	def show_myart(self):
+		
+		print(self.art)
+
+
+	def show_hobby(self):
+		if len(self.hobby) !=0:
+			print('-----my hobby----')
+			for i,h in enumerate (self.hobby,start=1):
+				print(i,h)
+			print('------------------')
+
+		else:
+			print('No hobby')
+
+
+
+if __name__ == '__main__':
+	my = Profile('Ton')
+	my.company = 'goodday'
+	my.hobby = ['Running','Learning','Travelling']
+	print(my.name)
+	my.show_email()
+	my.show_myart()
+	my.show_hobby()
+
+	# help(my)
+
+
