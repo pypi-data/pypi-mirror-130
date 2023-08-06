dc8f55f9b5376c216f00371b4e3253f54a121489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:45 +0000

    Add package treex/0.6.5/treex-0.6.5-py3-none-any.whl

---
+++ packages/treex/treex-0.6.5-py3-none-any.whl/treex/metrics/__init__.py
@@ -0,0 +1,12 @@
+from .accuracy import Accuracy
+from .loss_and_logs import LossAndLogs
+from .losses import AuxLosses, Losses
+from .mean import Mean
+from .mean_absolute_error import MeanAbsoluteError
+from .mean_square_error import MeanSquareError
+from .metric import Metric
+from .metrics import AuxMetrics, Metrics
+from .reduce import Reduce, Reduction
+
+MAE = MeanAbsoluteError
+MSE = MeanSquareError
