923a72daeb134d0ff843ad6c19ca5d40bf0c0cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:42 +0000

    Add package one-table/1.0.2/one_table-1.0.2.tar.gz

---
+++ packages/one-table/one_table-1.0.2.tar.gz/one_table-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2'
+DESCRIPTION = 'Python dynamoDB one table'
+LONG_DESCRIPTION = 'Python dynamoDB one table'
+
+setup(
+    name="one_table",
+    version=VERSION,
+    author="Gerswin Pineda",
+    author_email="g3rswin@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["cerberus"],
+    requires_python='>=3.6',
+    keywords=['python', 'dynamodb'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
