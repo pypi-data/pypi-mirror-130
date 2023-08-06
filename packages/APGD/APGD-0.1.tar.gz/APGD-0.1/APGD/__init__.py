9f43e92886a23c31f0f6800e516962134b5119db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:59 +0000

    Add package APGD/0.1/APGD-0.1.tar.gz

---
+++ packages/APGD/APGD-0.1.tar.gz/APGD-0.1/APGD/__init__.py
@@ -0,0 +1,11 @@
+'''
+Tools for solving the penalized regression models, such as HuberNet, HuberLasso,
+Huber Elastic Net, Elastic Net, Lasso, Network regularization, using Accelerated
+Proximal Gradient Descent (APGD) algorithm.
+'''
+
+
+from __future__ import absolute_import
+from .APGD import *
+
+name = 'APGD'
