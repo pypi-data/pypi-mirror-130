7f0df3672a95ae0fdc6155ed1de137c19b7c40e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:42 +0000

    Add package latestearthquakenewsindonesia/0.4/latestearthquakenewsindonesia-0.4.tar.gz

---
+++ packages/latestearthquakenewsindonesia/latestearthquakenewsindonesia-0.4.tar.gz/latestearthquakenewsindonesia-0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latestearthquakenewsindonesia",
+    version="0.4",
+    author="Firman Arya P",
+    author_email="firmanap22@gmail.com",
+    description="his package will get the latest earthquake news from Indonesia BMKG Meteorological, "
+                "Climatological And Geophysical Agency",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakarta-remote-work/Latest-Earthquake-News",
+    project_urls={
+
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    # package_dir={"": "src"},
+    packages=setuptools.find_packages(where=""),
+    python_requires=">=3.6",
+)
