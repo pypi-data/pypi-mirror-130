d69d34b68ee4fa3487bf91d338cfb30e5aff45b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:38 +0000

    Add package dbt-superset-lineage/0.2.0/dbt_superset_lineage-0.2.0-py3-none-any.whl

---
+++ packages/dbt-superset-lineage/dbt_superset_lineage-0.2.0-py3-none-any.whl/dbt_superset_lineage/__init__.py
@@ -0,0 +1,40 @@
+import typer
+from .pull_dashboards import main as pull_dashboards_main
+from .push_descriptions import main as push_descriptions_main
+
+app = typer.Typer()
+
+
+@app.command()
+def pull_dashboards(dbt_project_dir: str = typer.Option('.', help=""),
+                    exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
+                                                       help="If you change this, the path needs to be added"
+                                                            "to source-paths in dbt_project.yml."),
+                    dbt_db_name: str = typer.Option(None, help=""),
+                    superset_url: str = typer.Argument(..., help=""),
+                    superset_db_id: int = typer.Option(None, help=""),
+                    sql_dialect: str = typer.Option('ansi', help=""),
+                    superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
+                    superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
+
+    pull_dashboards_main(dbt_project_dir, exposures_path, dbt_db_name,
+                         superset_url, superset_db_id, sql_dialect,
+                         superset_access_token, superset_refresh_token)
+
+
+@app.command()
+def push_descriptions(dbt_project_dir: str = typer.Option('.', help=""),
+                      dbt_db_name: str = typer.Option(None, help=""),
+                      superset_url: str = typer.Argument(..., help=""),
+                      superset_db_id: int = typer.Option(None, help=""),
+                      superset_refresh_columns: bool = typer.Option(False, help=""),
+                      superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
+                      superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
+
+    push_descriptions_main(dbt_project_dir, dbt_db_name,
+                           superset_url, superset_db_id, superset_refresh_columns,
+                           superset_access_token, superset_refresh_token)
+
+
+if __name__ == '__main__':
+    app()
