2dae4e3143cf1b7ffc40e9e4e0f11cf77a815ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package strs/0.2.0/strs-0.2.0.tar.gz

---
+++ packages/strs/strs-0.2.0.tar.gz/strs-0.2.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+requirements: list[str] = \
+  Path('requirements.txt') \
+    .read_text() \
+    .splitlines()
+
+pkgs: list[str] = find_packages(
+  # include=[
+  #  'strs',
+  #  'strs.*',
+  # ]
+)
+
+
+setup(
+  name="strs",
+  version="0.2.0",
+  description="🧵 Easy string tools for the shell",
+  url="https://github.com/alexdelorenzo/strs",
+  author="Alex DeLorenzo",
+  license="AGPL 3.0",
+  packages=pkgs,
+  zip_safe=True,
+  install_requires=requirements,
+  python_requires='>=3.10',
+  entry_points={
+    "console_scripts": [
+      "strs = strs.__main__:_main",
+      "str = strs.__main__:_main",
+    ]
+  }
+)
