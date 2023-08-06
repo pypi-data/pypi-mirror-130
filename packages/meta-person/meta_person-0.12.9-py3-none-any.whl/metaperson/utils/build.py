f5e2e8bf95ec3e95c64d976a8dbbd55cbcb0f382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:25 +0000

    Add package meta-person/0.12.9/meta_person-0.12.9-py3-none-any.whl

---
+++ packages/meta-person/meta_person-0.12.9-py3-none-any.whl/metaperson/utils/build.py
@@ -0,0 +1,13 @@
+import importlib
+import os
+import sys
+
+
+def get_exp_by_file(exp_file):
+    try:
+        sys.path.append(os.path.dirname(exp_file))
+        current_exp = importlib.import_module(os.path.basename(exp_file).split(".")[0])
+        exp = current_exp.Exp()
+    except Exception:
+        raise ImportError("{} doesn't contains class named 'Exp'".format(exp_file))
+    return exp
