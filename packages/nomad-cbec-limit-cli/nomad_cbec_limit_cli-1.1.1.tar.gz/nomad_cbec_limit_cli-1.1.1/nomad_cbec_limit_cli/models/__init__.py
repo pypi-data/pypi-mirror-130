2c5b26e980af14a3f1fae2ae22e6f7138e2f38d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:06 +0000

    Add package nomad-cbec-limit-cli/1.1.1/nomad_cbec_limit_cli-1.1.1.tar.gz

---
+++ packages/nomad-cbec-limit-cli/nomad_cbec_limit_cli-1.1.1.tar.gz/nomad_cbec_limit_cli-1.1.1/nomad_cbec_limit_cli/models/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from nomad_cbec_limit_cli.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from nomad_cbec_limit_cli.model.api_response import ApiResponse
+from nomad_cbec_limit_cli.model.api_response_data import ApiResponseData
