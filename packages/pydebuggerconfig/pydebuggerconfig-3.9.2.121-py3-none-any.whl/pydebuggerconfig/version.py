d9f8883f2fd3afd338589ba5bf468351309015d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:42 +0000

    Add package pydebuggerconfig/3.9.2.121/pydebuggerconfig-3.9.2.121-py3-none-any.whl

---
+++ packages/pydebuggerconfig/pydebuggerconfig-3.9.2.121-py3-none-any.whl/pydebuggerconfig/version.py
@@ -0,0 +1,4 @@
+""" This file was generated when pydebuggerconfig was built """
+VERSION = '3.9.2.121'
+COMMIT_ID = '2a39bfb2bddcd7f0be0d4623a30ab931f0dc917b'
+BUILD_DATE = '2021-12-09 17:52:27 +0000'
