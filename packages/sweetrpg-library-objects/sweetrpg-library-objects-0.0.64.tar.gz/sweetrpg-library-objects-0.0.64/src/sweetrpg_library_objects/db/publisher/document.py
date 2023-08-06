2d6f26ac54f515444765567b202dc2bcaa57718f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:24 +0000

    Add package sweetrpg-library-objects/0.0.64/sweetrpg-library-objects-0.0.64.tar.gz

---
+++ packages/sweetrpg-library-objects/sweetrpg-library-objects-0.0.64.tar.gz/sweetrpg-library-objects-0.0.64/src/sweetrpg_library_objects/db/publisher/document.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""
+"""
+
+from datetime import datetime
+from mongoengine import Document, fields
+from pymongo import ASCENDING
+
+
+class PublisherDocument(Document):
+    """A mapping object to convert MongoDB data to a Publisher object."""
+
+    meta = {
+        "indexes": [
+            {"name": "publisher_name", "fields": ["name"]},
+        ],
+        "db_alias": "default",
+        "collection": "publishers",
+        "strict": False,
+    }
+
+    # basic properties
+    name = fields.StringField(min_length=1, max_length=200, required=True)
+
+    # relations
+    tags = fields.ListField(field=fields.ReferenceField("TagDocument"))
+
+    # audit properties
+    created_at = fields.DateTimeField(default=datetime.utcnow, required=True)
+    created_by = fields.StringField(default="system", required=True)
+    updated_at = fields.DateTimeField(default=datetime.utcnow, required=True)
+    updated_by = fields.StringField(default="system", required=True)
+    deleted_at = fields.DateTimeField(null=True)
+    deleted_by = fields.StringField(null=True)
