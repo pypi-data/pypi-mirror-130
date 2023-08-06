9d720df2af557707f8061627a65a57db4514dd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:40 +0000

    Add package kaiostocks/0.0.6/kaiostocks-0.0.6.tar.gz

---
+++ packages/kaiostocks/kaiostocks-0.0.6.tar.gz/kaiostocks-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+__project__ = "kaiostocks"
+__version__ = "0.0.6"
+__description__ = "this library treats stocks data"
+__requires__ = ["pandas", "ta", "numpy"]
+__author__ = "Kaio"
+__author_email__ = "akaio@crimson.ua.edu"
+
+setup(
+    name = __project__,
+    version = __version__,
+    url = "https://github.com/kaiomarques93/kaiostocks",
+    description = __description__,
+    packages = ['kaiostocks'],
+    #packages=find_packages(where="./kaiostocks"),
+    #package_dir= {'', 'kaiostocks'},
+    author = __author__,
+    author_email = __author_email__,
+    install_requires = __requires__,
+)
