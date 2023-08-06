3bb047663a89b47bcfdb0b58157c36b5f12ec97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:13 +0000

    Add package meerschaum/0.5.1.dev3/meerschaum-0.5.1.dev3-py3-none-any.whl

---
+++ packages/meerschaum/meerschaum-0.5.1.dev3-py3-none-any.whl/meerschaum/api/_events.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Declare FastAPI events in this module (startup, shutdown, etc.).
+"""
+
+import sys, os, time
+from meerschaum.api import app, get_api_connector, get_uvicorn_config, debug
+from meerschaum.utils.debug import dprint
+from meerschaum.utils.misc import retry_connect
+from meerschaum.config._paths import API_UVICORN_CONFIG_PATH
+
+@app.on_event("startup")
+async def startup():
+    conn = get_api_connector()
+    try:
+        connected = retry_connect(
+            get_api_connector(),
+            workers = get_uvicorn_config().get('workers', None),
+            debug = debug
+        )
+    except Exception as e:
+        print(e)
+        connected = False
+    if not connected:
+        await shutdown()
+        os._exit(1)
+
+@app.on_event("shutdown")
+async def shutdown():
+    try:
+        if API_UVICORN_CONFIG_PATH.exists():
+            os.remove(API_UVICORN_CONFIG_PATH)
+    except Exception as e:
+        pass
+        print(e)
+    if debug:
+        dprint("Closing connection...")
+    if get_api_connector().type == 'sql':
+        try:
+            await get_api_connector().db.disconnect()
+        except Exception as e:
+            pass
