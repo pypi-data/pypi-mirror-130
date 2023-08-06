9473e7e1d07417a41d42dc20c33ed63f8403d791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package flinit/0.2.2/flinit-0.2.2.tar.gz

---
+++ packages/flinit/flinit-0.2.2.tar.gz/flinit-0.2.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+
+def getRequirements():
+    with open('requirements.txt') as f:
+        content = f.read()
+        requirements = content.split('\n')
+    return requirements
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='flinit',
+    version='0.2.2',
+    author='Tanmoy Sen Gupta',
+    author_email='tanmoysps@gmail.com',
+    url='https://github.com/TanmoySG/flinit',
+    description='Flask App Initializer.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+            'console_scripts': [
+                'flinit = flinit.flinit:main'
+            ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    keywords=['flask', 'python', 'project'],
+    install_requires=getRequirements(),
+    zip_safe=False
+)
