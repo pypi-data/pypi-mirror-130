557d9cb1348d81fbe85dc24357c736021d8bb947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:29 +0000

    Add package joker-flasky/0.4.7/joker-flasky-0.4.7.tar.gz

---
+++ packages/joker-flasky/joker-flasky-0.4.7.tar.gz/joker-flasky-0.4.7/joker/flasky/security.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Deprecated. This module will be removed on ver 0.6.0.
+"""
+
+from joker.flasky.auth import HashedPassword
+
+__all__ = []
+__deprecated__ = [HashedPassword]
