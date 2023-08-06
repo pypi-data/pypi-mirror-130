470fed75f2faa9fddd81c1a30ee85f1b01537ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package ananotherone/0.1.5/ananotherone-0.1.5.tar.gz

---
+++ packages/ananotherone/ananotherone-0.1.5.tar.gz/ananotherone-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='ananotherone',
+    version='0.1.5',
+    description='An another one',
+    url='https://github.com/shuds13/pyexample',
+    author='Pasha',
+    author_email='swordingo@gmail.com',
+    license='BSD 2-clause',
+    packages=['ananotherone'],
+    install_requires=['numpy==0.1.0',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
