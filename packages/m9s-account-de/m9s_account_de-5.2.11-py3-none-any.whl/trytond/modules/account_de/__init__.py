07ca41aebae388623a1efe4f5434e3dc9a1a7ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:50 +0000

    Add package m9s-account-de/5.2.11/m9s_account_de-5.2.11-py3-none-any.whl

---
+++ packages/m9s-account-de/m9s_account_de-5.2.11-py3-none-any.whl/trytond/modules/account_de/__init__.py
@@ -0,0 +1,21 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+from . import configuration, currency, party, tax
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        configuration.Configuration,
+        configuration.ConfigurationDefaultTaxRule,
+        party.Party,
+        tax.AdvanceTurnoverTaxReturnContext,
+        tax.TaxCode,
+        currency.Currency,
+        module='account_de', type_='model')
+    Pool.register(
+        tax.AdvanceTurnoverTaxReturnReport,
+        module='account_de', type_='report')
