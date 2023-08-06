69fc40196ccf31fcada3098f95564218b383c91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:51 +0000

    Add package mysqlconn/1.0.0/mysqlconn-1.0.0-py3.7.egg

---
+++ packages/mysqlconn/mysqlconn-1.0.0-py3.7.egg/myorm/orm.py
@@ -0,0 +1,4 @@
+# noinspection PyUnresolvedReferences
+from myorm.factory import MySQLSessionFactoryBuild as Builder
+# noinspection PyUnresolvedReferences
+from superorm.orm import parse_config_from_file, parse_config_from_string
