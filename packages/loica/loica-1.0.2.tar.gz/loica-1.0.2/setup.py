08df2255bd582652f102f2587c03203cabc65d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:52 +0000

    Add package loica/1.0.2/loica-1.0.2.tar.gz

---
+++ packages/loica/loica-1.0.2.tar.gz/loica-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as ld:
+    long_description = ld.read()
+
+setup(name='loica',
+    version='1.0.2',
+    author='Gonzalo Vidal',
+    author_email='gsvidal@uc.cl',
+    description='LOICA: Logical Operators for Integrated Cell Algorithms',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RudgeLab/LOICA',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    install_requires=[        
+        'numpy', 
+	    'networkx',
+	    'pandas',
+    	'sbol3',
+    	'tyto',
+   	    'tdqm',
+        'scipy'   
+        ],
+    setup_requires=[
+        'numpy',
+	    'networkx',
+	    'pandas',
+    	'sbol3',
+    	'tyto',
+    	'tdqm',
+        'scipy'
+        ],
+    packages=find_packages(),
+    python_requires='>=3'
+)
