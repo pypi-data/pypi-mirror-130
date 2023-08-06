8c7b3a28415c669d2a887f0f1eb37ffec4fb5bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:22 +0000

    Add package VividHues/4.0.0/VividHues-4.0.0.tar.gz

---
+++ packages/VividHues/VividHues-4.0.0.tar.gz/VividHues-4.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+print("\033[93m" + "Installing VividHues..." + "\033[0m")
+
+setup(
+    name='VividHues',  # needed to silence warnings (and to be a worthwhile package)
+    url='https://github.com/KennyOliver/VividHues',
+    author='Kenneth Oliver',
+    author_email='kenny_oliver@icloud.com',
+    packages=['VividHues'],  # needed to actually package something
+    # install_requires=['numpy'], #needed for dependencies
+    version='4.0.0',
+    license='AGPL',  # license can be anything you like
+    description='VividHues: super lightweight package for coloured strings in Python!',
+    long_description=open('README.md').read(),  # you need a readme eventually (there will be a warning)
+    long_description_content_type='text/markdown',
+)
+
+print("\033[32m" + "\033[1m" + "Success!!! Have colourful fun! ;D" + "\033[0m")
