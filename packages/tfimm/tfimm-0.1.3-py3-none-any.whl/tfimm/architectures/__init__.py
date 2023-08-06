59d10f5a4b1e79ffcd20a278863a16077ae19eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:56 +0000

    Add package tfimm/0.1.3/tfimm-0.1.3-py3-none-any.whl

---
+++ packages/tfimm/tfimm-0.1.3-py3-none-any.whl/tfimm/architectures/__init__.py
@@ -0,0 +1,6 @@
+from .cait import *  # noqa: F401
+from .convmixer import *  # noqa: F401
+from .mlp_mixer import *  # noqa: F401
+from .resnet import *  # noqa: F401
+from .swin import *  # noqa: F401
+from .vit import *  # noqa: F401
