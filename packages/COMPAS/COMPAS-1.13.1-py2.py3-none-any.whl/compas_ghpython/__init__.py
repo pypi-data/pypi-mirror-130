b68fd196cbc197c4e1f9d3b4532a5a667dcc59f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:03 +0000

    Add package COMPAS/1.13.1/COMPAS-1.13.1-py2.py3-none-any.whl

---
+++ packages/COMPAS/COMPAS-1.13.1-py2.py3-none-any.whl/compas_ghpython/__init__.py
@@ -0,0 +1,84 @@
+"""
+********************************************************************************
+compas_ghpython
+********************************************************************************
+
+.. currentmodule:: compas_ghpython
+
+.. toctree::
+    :maxdepth: 1
+
+    compas_ghpython.artists
+    compas_ghpython.utilities
+
+"""
+import os
+import compas
+import compas_rhino
+
+__version__ = '1.13.1'
+
+if compas.is_rhino():
+    from .utilities import *  # noqa: F401 F403
+
+__all__ = [
+    'get_grasshopper_managedplugin_path',
+    'get_grasshopper_library_path',
+    'get_grasshopper_userobjects_path'
+]
+__all_plugins__ = [
+    'compas_ghpython.install',
+    'compas_ghpython.uninstall',
+    'compas_ghpython.artists',
+]
+
+
+# =============================================================================
+# General Helpers
+# =============================================================================
+
+
+def _get_grasshopper_special_folder(version, folder_name):
+    grasshopper = compas_rhino._get_rhino_grasshopperplugin_path(version)
+    return os.path.join(grasshopper, folder_name)
+
+
+# =============================================================================
+# Managed Plugin
+# =============================================================================
+
+
+def get_grasshopper_managedplugin_path(version):
+    version = compas_rhino._check_rhino_version(version)
+    managedplugins = compas_rhino._get_rhino_managedplugins_path(version)
+
+    if compas.WINDOWS:
+        gh_managedplugin_path = os.path.join(managedplugins, 'Grasshopper')
+
+    elif compas.OSX:
+        gh_managedplugin_path = os.path.join(managedplugins, 'GrasshopperPlugin.rhp')
+
+    if not os.path.exists(gh_managedplugin_path):
+        raise Exception("The Grasshopper (managed) Plug-in folder does not exist in this location: {}".format(gh_managedplugin_path))
+
+    return gh_managedplugin_path
+
+
+# =============================================================================
+# GH Plugin Libraries path
+# =============================================================================
+
+
+def get_grasshopper_library_path(version):
+    """Retrieve Grasshopper's library (components) path"""
+    return _get_grasshopper_special_folder(version, 'Libraries')
+
+
+# =============================================================================
+# GH Plugin UserObjects path
+# =============================================================================
+
+
+def get_grasshopper_userobjects_path(version):
+    """Retrieve Grasshopper's user objects path"""
+    return _get_grasshopper_special_folder(version, 'UserObjects')
