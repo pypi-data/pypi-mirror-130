145bc85a3d320a6513c2100e04e6e5bf40134064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:17 +0000

    Add package classrad/0.0.6/classrad-0.0.6.tar.gz

---
+++ packages/classrad/classrad-0.0.6.tar.gz/classrad-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Load packages from requirements.txt
+with open("requirements.txt") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+setup(
+    name = 'classrad',
+    packages = ['classrad'],
+    install_requires=required_packages,
+)
