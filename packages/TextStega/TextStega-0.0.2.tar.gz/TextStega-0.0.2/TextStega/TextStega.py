de17dc42555e5e63dbf22553099c31051b47e95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:44 +0000

    Add package TextStega/0.0.2/TextStega-0.0.2.tar.gz

---
+++ packages/TextStega/TextStega-0.0.2.tar.gz/TextStega-0.0.2/TextStega/TextStega.py
@@ -0,0 +1,133 @@
+"""
+This module implements text steganography
+Author: Atharva Vidwans
+Github: https://github.com/Atharva-Vidwans
+Linkedin: https://www.linkedin.com/in/atharva-vidwans-62739b169/
+Medium: https://medium.com/@atharvavidwans
+Website: https://atharva-vidwans.github.io/Website/
+"""
+
+__author__ = "Atharva Vidwans"
+__email__ = "atharvavidwans@gmail.com"
+
+import matplotlib.pyplot as plt
+import math
+import cv2 as cv 
+import numpy as np
+# Stop command is by ~
+
+class stg:
+
+     """
+    Methods
+    -------
+    
+    Public:
+        
+        decode: Encodes the text to the image
+    
+        encode: Decodes the text from the image
+    
+    """
+    def decode(path):    
+        '''
+        Input parameter 
+        path(str) : Path of the image from which the encoded message is to be decoded
+
+        Output
+        text(str) : Decoded message from the image  
+
+        Example
+        msg = decode(path)
+
+        Decode function decodes all the characters till it encounters ~(tilda) character.  
+        '''
+
+        img = cv.imread(path)
+
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img_str = np.reshape(img, (np.product(img.shape)))
+
+        text = ""
+
+        for i in range(int(len(img_str)/8)):
+            bit = img_str[8*i:8*(i+1)]
+            #print(bit.shape)
+            #print(bit)
+            bit = np.remainder(bit, 2)
+            #print(bit.shape)
+            #print(bit)
+            bit = ''.join(map(str, bit))
+            #print(bit)
+
+            ascii_character = chr(int(bit, 2))
+            #print(ascii_character)
+            if ascii_character == '~' :
+                break
+            else:
+                text = text + ascii_character
+            i+=8
+        return text
+
+
+    def encode(path, text):
+        '''
+        Input parameter: 
+        path(str) : Path of the image to which the message is to be encoded
+        text(str) : Message to be encoded
+
+        Output:
+        final_image(numpy array) :  New image on which the message is encoded
+
+        Example:
+        img = encode(path, text)
+
+        Stop character is ~(tilda). Encode function automatically adds this character at the end of the user input string. No need to specifically add this character. 
+
+        '''
+
+        img = cv.imread(path)
+
+        limit = math.floor((img.shape[0]*img.shape[1]*img.shape[2]-8)/8)
+
+        if len(text) <= (img.shape[0]*img.shape[1]*img.shape[2]-8)/8 :
+
+            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            img_str = np.reshape(img, (np.product(img.shape)))
+
+            bit_str=""
+            a = ' '.join(format(ord(x), 'b') for x in text)
+            LL = list(a.split(' '))
+            for i, bit in enumerate(LL):
+                bit_str = bit_str + bit.zfill(8)
+
+            bit_str = bit_str + "01111110"
+            #print(bit_str)
+
+            for i in range(len(bit_str)):
+                #print(bit_str[i],img_str[i])
+
+                if bit_str[i] == '0':
+                    if img_str[i]%2 != 0:
+
+                        img_str[i] = img_str[i] + 1
+                    #print(bit_str[i],img_str[i])
+                else:
+                    if img_str[i]%2 == 0:
+
+                        img_str[i] = img_str[i] + 1
+                    #print(bit_str[i],img_str[i])
+
+            final_image = np.reshape(img_str , img.shape)
+
+            cv.imwrite("Encoded_image.png", cv.cvtColor(final_image, cv.COLOR_RGB2BGR))
+            print(r"Encoded image is saved as 'Encoded_image.png'")
+            return final_image
+
+        else:
+            print("Try again with higher resolution image!\nNo.of characters to encode exceed maximum capacity of the image\n")
+            print("\nMaximum Characters allowed are : ",limit)
+
+
+
+    
