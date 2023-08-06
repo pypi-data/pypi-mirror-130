ba14e5c1dbff419ac2c595ef872a21a1c19f059a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:07 +0000

    Add package rgov/0.6.7/rgov-0.6.7.tar.gz

---
+++ packages/rgov/rgov-0.6.7.tar.gz/rgov-0.6.7/rgov/search.py
@@ -0,0 +1,34 @@
+import sqlite3
+from contextlib import closing
+from typing import Generator
+
+from rgov import locations
+
+
+def search(
+    search_terms: list, descriptions=False
+) -> Generator[tuple[str, str], None, None]:
+    if not isinstance(search_terms, list):
+        raise ValueError("arg: search_terms not a list")
+
+    query = [f"%{term.lower()}%" for term in search_terms]
+
+    num_search_terms = len(query)
+
+    if descriptions == True:
+        column = "description"
+    else:
+        column = "name"
+
+    separator = f" AND {column} LIKE "
+
+    sql_statement = f"""SELECT *
+                     FROM campgrounds 
+                     WHERE ({column} LIKE {separator.join(['?' for _ in query])}) 
+                     ORDER BY name;"""
+
+    with closing(sqlite3.connect(locations.FACILITIES_DB)) as con, con, closing(
+        con.cursor()
+    ) as cur:
+        for row in cur.execute(sql_statement, query):
+            yield row[1].title(), row[0]
