69045a19dfc4beb253a3e461da8c147ad9618d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:07 +0000

    Add package milwrap/0.1.3/milwrap-0.1.3-py3-none-any.whl

---
+++ packages/milwrap/milwrap-0.1.3-py3-none-any.whl/milwrap/__init__.py
@@ -0,0 +1,7 @@
+"""
+milwrap
+~~~~~~
+
+The milwrap package - multiple instane meta-learner that can use any supervised-learning algorithms.
+"""
+from milwrap.countbase import MilCountBasedMultiClassLearner
