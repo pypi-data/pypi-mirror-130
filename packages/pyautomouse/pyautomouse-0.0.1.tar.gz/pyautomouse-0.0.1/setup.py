00d644f3db6ffd9d262e510237e05b06b85e295b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:20 +0000

    Add package pyautomouse/0.0.1/pyautomouse-0.0.1.tar.gz

---
+++ packages/pyautomouse/pyautomouse-0.0.1.tar.gz/pyautomouse-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyautomouse",
+    version="0.0.1",
+    author="xiongtianshuo",
+    author_email="seoul1k@163.com",
+    url='https://github.com/seoul2k/pyautomouse',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    project_urls={
+        "Bug Tracker": "https://github.com/seoul2k/pyautomouse/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
