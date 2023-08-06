c62ba052d9871fd23bc7fecd802b019e80312864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:13 +0000

    Add package VioKiP/0.0.1/VioKiP-0.0.1.tar.gz

---
+++ packages/VioKiP/VioKiP-0.0.1.tar.gz/VioKiP-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+    name="VioKiP",
+    version=VERSION,
+    author="Violetta Kishkurno",
+    author_email="<kishkurno34@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
