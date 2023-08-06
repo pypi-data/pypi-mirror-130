2154d2078581b8655955c25f217645c2719ddd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package ad-AHJZ/1.5/ad_AHJZ-1.5.tar.gz

---
+++ packages/ad-AHJZ/ad_AHJZ-1.5.tar.gz/ad_AHJZ-1.5/ad_AHJZ/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 15:14:15 2021
+
+@author: aditimemani
+"""
+from ad_AHJZ.val_derv import val_derv
+from ad_AHJZ.forward_mode import combine_vector_inputs, forward_mode
+from ad_AHJZ.optimizers import Optimizer
