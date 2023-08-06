6d11855320fd620daa4f29609e3c977bd048d946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:47 +0000

    Add package autodiff-107/0.0.1/autodiff_107-0.0.1-py3-none-any.whl

---
+++ packages/autodiff-107/autodiff_107-0.0.1-py3-none-any.whl/autodiff_107/diff/__init__.py
@@ -0,0 +1,5 @@
+from .Node import Node as Node
+from .Node import Var as Var
+from .Node import variable
+from .nodemath import numpy
+
---
+++ packages/autodiff-107/autodiff_107-0.0.1-py3-none-any.whl/tests/test_gradient_descent.py
@@ -0,0 +1,22 @@
+from autodiff_107.diff import Node
+from autodiff_107.optim import gradient_descent
+# from Node import Var
+# from Node import variable
+# from nodemath import numpy
+from autodiff_107.diff import numpy as np
+import pytest
+
+
+def test_gradient_descent_1():
+	# find min of (x-3)**2+3, which occurs at 3 with value 3
+	x0 = 1
+	f = lambda x: (x-3)**2+1
+	tol = 1e-3
+	alpha=.9
+	max_iter=20000
+	mini = gradient_descent(f, x0,alpha, tol, max_iter)
+	#print("The minimum is achieved at {} with function value {}".format(mini, f(mini)))
+	assert np.abs(mini-3)<tol
+
+test_gradient_descent_1()
+
