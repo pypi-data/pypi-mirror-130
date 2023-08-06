5dea9463512457255ce0cacf2d657df2105461e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:52 +0000

    Add package pyoverload/1.1.1/pyoverload-1.1.1.tar.gz

---
+++ packages/pyoverload/pyoverload-1.1.1.tar.gz/pyoverload-1.1.1/pyoverload/__init__.py
@@ -0,0 +1,15 @@
+#! python3.8 -u
+#  -*- coding: utf-8 -*-
+
+##############################
+## Project PyZMyc
+## Package pyoverload
+## Author: Yuncheng Zhou
+## Create: 2021-12
+## Version: 1.1.1
+## Contact: bertiezhou@163.com
+## Keywords: overload
+## Description: 'pyoverload' overloads the functions by simply using typehints and adding decorator '@overload'.
+##############################
+
+from .override import *
