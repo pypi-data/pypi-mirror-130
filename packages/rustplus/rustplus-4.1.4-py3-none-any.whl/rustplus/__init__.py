0e5fa5d2c7c280fb37543581f49c3b6561a32054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package rustplus/4.1.4/rustplus-4.1.4-py3-none-any.whl

---
+++ packages/rustplus/rustplus-4.1.4-py3-none-any.whl/rustplus/__init__.py
@@ -0,0 +1,12 @@
+r"""
+RustPlus, An API wrapper for interfacing with the Rust+ App API
+"""
+
+from .api import RustSocket
+from .exceptions import ClientError, ImageError, ServerNotResponsiveError, ClientNotConnectedError, PrefixNotDefinedError, CommandsNotEnabledError, RustSocketDestroyedError
+from .commands import CommandOptions, Command
+
+__name__ = "rustplus"
+__author__ = "olijefferson"
+__version__ = "4.1.4"
+__support__ = "Discord: https://discord.gg/nQqJe8qvP8"
