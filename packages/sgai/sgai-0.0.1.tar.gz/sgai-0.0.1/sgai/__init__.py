c3a4d263b23c5c7f0f8ed9f885c31cea2d190ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:43 +0000

    Add package sgai/0.0.1/sgai-0.0.1.tar.gz

---
+++ packages/sgai/sgai-0.0.1.tar.gz/sgai-0.0.1/sgai/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Simple Game AI."""
+
+__author__ = """Jan Bronicki"""
+__email__ = 'janbronicki@gmail.com'
+__version__ = '0.0.1'
---
+++ packages/sgai/sgai-0.0.1.tar.gz/sgai-0.0.1/sgai/sgai.py
@@ -0,0 +1,6 @@
+"""Main module."""
+
+
+def hi_jan():
+    return "hi, jan"
+
