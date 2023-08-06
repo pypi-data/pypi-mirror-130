6017a80e3c34ad38a62aa3c52e88e229d7762e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:27 +0000

    Add package messenger_server_project_december/0.0.1/messenger_server_project_december-0.0.1.tar.gz

---
+++ packages/messenger_server_project_december/messenger_server_project_december-0.0.1.tar.gz/messenger_server_project_december-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="messenger_server_project_december",
+      version="0.0.1",
+      description="messenger_server_project_december",
+      author="Vladimir",
+      author_email="iv.iv@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
