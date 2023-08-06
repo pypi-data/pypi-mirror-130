af73fd14c06ba6cd1e67b1851522f47198905381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:23 +0000

    Add package ExoJAX/1.0.0/exojax-1.0.0-py3.8.egg

---
+++ packages/ExoJAX/exojax-1.0.0-py3.8.egg/exojax/spec/__init__.py
@@ -0,0 +1,38 @@
+__all__ = []
+
+__version__ = "1.0.0"
+__uri__ = ""
+__author__ = "Hajime Kawahara"
+__email__ = "divrot@gmail.com"
+__license__ = ""
+__description__ = "auto-differentiable spectral modules in exojax"
+
+from exojax.spec.hitran import (
+    SijT,
+    doppler_sigma,
+    gamma_natural,
+    normalized_doppler_sigma
+)
+
+from exojax.spec.autospec import (
+    AutoXS,
+    AutoRT,
+)
+
+
+
+from exojax.spec.opacity import (
+    xsection,
+)
+
+from exojax.spec.lpf import (
+    hjert,
+    voigt,
+    voigtone,
+    vvoigt,
+)
+
+
+from exojax.spec.make_numatrix import (
+    make_numatrix0,
+)
