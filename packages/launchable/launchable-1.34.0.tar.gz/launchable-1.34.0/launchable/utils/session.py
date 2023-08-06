f0f6d1361e748130be08dd8e1e549dc8c391eaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package launchable/1.34.0/launchable-1.34.0.tar.gz

---
+++ packages/launchable/launchable-1.34.0.tar.gz/launchable-1.34.0/launchable/utils/session.py
@@ -0,0 +1,110 @@
+import os
+import sys
+from pathlib import Path
+from typing import Optional
+import datetime
+import hashlib
+from .logger import Logger
+
+SESSION_DIR_KEY = 'LAUNCHABLE_SESSION_DIR'
+DEFAULT_SESSION_DIR = '~/.config/launchable/sessions/'
+
+
+def _session_file_dir() -> Path:
+    return Path(os.environ.get(SESSION_DIR_KEY) or DEFAULT_SESSION_DIR).expanduser()
+
+
+def _session_file_path(build_name: str) -> Path:
+    return _session_file_dir() / (hashlib.sha1("{}:{}".format(build_name, _get_session_id()).encode()).hexdigest() + ".txt")
+
+
+def _get_session_id() -> str:
+    # CircleCI changes unix session id each steps, so set non change variable
+    # https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
+    if os.environ.get("CIRCLECI") is not None:
+        id = os.environ.get("CIRCLE_WORKFLOW_ID")
+        if id is not None:
+            return id
+        Logger().warning("CIRCLECI environment variable is set but not CIRCLE_WORKFLOW_ID")
+
+    # Jenkins pipeline also launches every process as a new session, so better to scope this to a particular build.
+    # For freestyle job types that do not do this, session IDs do not distinguish different builds, so that is a problem, too.
+    #
+    # See: https://github.com/jenkinsci/lib-durable-task/blob/6e020747205cb5aca5af757bc0d2a302c42bdb79/src/cmd/bash/durable_task_monitor.go#L41
+    # See: https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#using-environment-variables
+    if os.environ.get("JENKINS_URL") is not None:
+        id = os.environ.get("BUILD_URL")
+        if id is not None:
+            return id
+        Logger().warning("JENKINS_URL environment variable is set but not BUILD_URL")
+
+    if sys.platform == "win32":
+        import wmi  # type: ignore
+        c = wmi.WMI()
+        wql = "Associators of {{Win32_Process='{}'}} Where Resultclass = Win32_LogonSession Assocclass = Win32_SessionProcess".format(
+            os.getpid())
+        res = c.query(wql)
+        return str(res[0].LogonId)
+    else:
+        return str(os.getsid(os.getpid()))
+
+
+def read_session(build_name: str) -> Optional[str]:
+    f = _session_file_path(build_name)
+    try:
+        if not f.exists():
+            return None
+        return f.read_text()
+    except Exception as e:
+        raise Exception("Can't read {}".format(f)) from e
+
+
+def write_session(build_name: str, session_id: str) -> None:
+    try:
+        if not _session_file_dir().exists():
+            _session_file_dir().mkdir(parents=True, exist_ok=True)
+
+        _session_file_path(build_name).write_text(session_id)
+    except Exception as e:
+        raise Exception("Can't write to {}. Perhaps set the {} environment variable to specify an alternative writable path?".format(
+            _session_file_path(build_name), SESSION_DIR_KEY)) from e
+
+
+def remove_session(build_name: str) -> None:
+    """
+    Call it after closing a session
+    """
+    if _session_file_path(build_name).exists():
+        _session_file_path(build_name).unlink()
+
+
+def clean_session_files(days_ago: int = 0) -> None:
+    """
+    Call it each build start
+    """
+    if _session_file_dir().exists():
+        for child in _session_file_dir().iterdir():
+            file_created = datetime.datetime.fromtimestamp(
+                child.stat().st_mtime)
+
+            if sys.platform == "win32":
+                # Windows sometimes misses to delete session files at Unit Test
+                microseconds = -10
+            else:
+                microseconds = 0
+
+            clean_date = datetime.datetime.now() - datetime.timedelta(days=days_ago,
+                                                                      microseconds=microseconds)
+            if file_created < clean_date:
+                child.unlink()
+
+
+def parse_session(session: str):
+    try:
+        # session format:
+        # builds/<build name>/test_sessions/<test session id>
+        _, build_name, _, session_id = session.split("/")
+    except Exception as e:
+        raise Exception("Can't parse session: {}".format(session)) from e
+
+    return build_name, session_id
---
+++ packages/launchable/launchable-1.34.0.tar.gz/launchable-1.34.0/tests/test_session.py
@@ -0,0 +1,61 @@
+import tempfile
+from unittest import TestCase, mock
+from launchable.utils.session import SESSION_DIR_KEY, write_session, read_session, remove_session, clean_session_files, _get_session_id, parse_session
+import os
+import shutil
+
+
+class SessionTestClass(TestCase):
+    build_name = '123'
+    session_id = '/intake/organizations/launchableinc/workspaces/mothership/builds/123/test_sessions/13'
+
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
+        os.environ[SESSION_DIR_KEY] = self.dir
+
+    def tearDown(self):
+        clean_session_files()
+        del os.environ[SESSION_DIR_KEY]
+        shutil.rmtree(self.dir)
+
+    def test_write_read_remove(self):
+        write_session(self.build_name, self.session_id)
+        self.assertEqual(read_session(self.build_name), self.session_id)
+
+        remove_session(self.build_name)
+        self.assertEqual(read_session(self.build_name), None)
+
+    def test_other_build(self):
+        write_session(self.build_name, self.session_id)
+
+        next_build_name = '124'
+        next_session_id = '/intake/organizations/launchableinc/workspaces/mothership/builds/123/test_sessions/14'
+        write_session(next_build_name, next_session_id)
+
+        self.assertEqual(read_session(next_build_name), next_session_id)
+        self.assertEqual(read_session(self.build_name), self.session_id)
+
+    def test_read_before_write(self):
+        self.assertEqual(read_session(self.build_name), None)
+
+    def test_different_pid(self):
+        # TODO
+        pass
+
+    def test_get_session_id(self):
+        id = _get_session_id()
+
+        with mock.patch.dict(os.environ, {"CIRCLECI": "TRUE", "CIRCLE_WORKFLOW_ID": "abc-123"}):
+            id_in_circleci = _get_session_id()
+
+            self.assertEqual(id_in_circleci, "abc-123")
+            self.assertNotEqual(id, id_in_circleci)
+
+    def test_parse_session(self):
+        session = "builds/build-name/test_sessions/123"
+        build_name, session_id = parse_session(session)
+        self.assertEqual(build_name, "build-name")
+        self.assertEqual(session_id, "123")
+
+        with self.assertRaises(Exception):
+            parse_session("hoge/fuga")
