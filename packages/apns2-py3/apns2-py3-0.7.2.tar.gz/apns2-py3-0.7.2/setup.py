8d36c7902c1be26e23a6cc00af855331d8a2cfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:25 +0000

    Add package apns2-py3/0.7.2/apns2-py3-0.7.2.tar.gz

---
+++ packages/apns2-py3/apns2-py3-0.7.2.tar.gz/apns2-py3-0.7.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='apns2-py3',
+    version='0.7.2',
+    packages=['apns2'],
+    install_requires=[
+        'hyper>=0.7',
+        'PyJWT>2.0.0',
+        'cryptography>=1.7.2',
+    ],
+    extras_require={
+        "tests": [
+            'freezegun',
+            'pytest',
+        ],
+    },
+    url='https://github.com/ramananda-kairi/PyApns2-py3',
+    license='MIT',
+    author='Ramananda Kairi',
+    author_email='ramananda.kairi@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'
+)
