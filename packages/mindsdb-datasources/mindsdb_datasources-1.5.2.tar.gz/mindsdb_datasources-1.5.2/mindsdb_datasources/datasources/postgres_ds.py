ccda81a40d48143bc6be4f803e9bafa624674d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:49 +0000

    Add package mindsdb-datasources/1.5.2/mindsdb_datasources-1.5.2.tar.gz

---
+++ packages/mindsdb-datasources/mindsdb_datasources-1.5.2.tar.gz/mindsdb_datasources-1.5.2/mindsdb_datasources/datasources/postgres_ds.py
@@ -0,0 +1,45 @@
+import os
+
+import pandas as pd
+import pg8000
+
+from mindsdb_datasources.datasources.data_source import SQLDataSource
+
+
+class PostgresDS(SQLDataSource):
+    def __init__(self, query, database='postgres', host='localhost',
+                 port=5432, user='postgres', password=''):
+        super().__init__(query)
+        self.database = database
+        self.host = host
+        self.port = int(port)
+        self.user = user
+        self.password = password
+
+    def query(self, q):
+        additional_args = {}
+        if 'cockroachlabs.cloud' in self.host:
+            additional_args['ssl_context'] = True
+        con = pg8000.connect(
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            **additional_args
+        )
+
+        df = pd.read_sql(q, con=con)
+        con.close()
+
+        df.columns = [x if isinstance(x, str) else x.decode('utf-8') for x in df.columns]
+        for col_name in df.columns:
+            try:
+                df[col_name] = df[col_name].apply(lambda x: x if isinstance(x, str) else x.decode('utf-8'))
+            except Exception:
+                pass
+
+        return df, self._make_colmap(df)
+
+    def name(self):
+        return 'Postgres - {}'.format(self._query)
