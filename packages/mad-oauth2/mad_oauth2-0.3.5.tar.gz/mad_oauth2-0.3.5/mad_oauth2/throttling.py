53b1e24f1bb53cba929ef75efb0d8e2a12192864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:27 +0000

    Add package mad-oauth2/0.3.5/mad_oauth2-0.3.5.tar.gz

---
+++ packages/mad-oauth2/mad_oauth2-0.3.5.tar.gz/mad_oauth2-0.3.5/mad_oauth2/throttling.py
@@ -0,0 +1,35 @@
+from mad_oauth2.models import Throttle
+from mad_oauth2.settings import oauth2_settings
+from rest_framework.throttling import ScopedRateThrottle
+from mad_webhooks.application import getApplicationDataFromRequest
+
+class ThrottleClass():
+    def __init__(self):
+        self.rate = dict(Throttle.objects.values_list('scope', 'rate'))
+
+    def getThrottlingRates(self):
+        return self.rate
+
+
+def getThrottling():
+    throttling = oauth2_settings.THROTTLE_CLASS()
+    return throttling.getThrottlingRates()
+
+
+class BaseScopedRateThrottle(ScopedRateThrottle):
+    THROTTLE_RATES = getThrottling()
+
+    def __init__(self):
+        super(BaseScopedRateThrottle, self).__init__()
+
+    def get_cache_key(self, request, view):
+        if request.user is not None and request.user.is_authenticated:
+            ident = request.user.pk
+        else:
+            app_data = getApplicationDataFromRequest(self.request)
+            ident = app_data['application'].id
+
+        return self.cache_format % {
+            'scope': self.scope,
+            'ident': ident
+        }
