e0bac3b3e63480b1cbd7a7f1486f37c7b1b4253b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:13 +0000

    Add package amstrax/1.0.2/amstrax-1.0.2.tar.gz

---
+++ packages/amstrax/amstrax-1.0.2.tar.gz/amstrax-1.0.2/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+# Get requirements from requirements.txt, stripping the version tags
+with open('requirements.txt') as f:
+    requires = [
+        r.split('/')[-1] if r.startswith('git+') else r
+        for r in f.read().splitlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
+with open('HISTORY.md') as file:
+    history = file.read()
+
+setuptools.setup(name='amstrax',
+                 version='1.0.2',
+                 description='strax for XAMS data',
+                 author='Nikhef',
+                 url='https://github.com/XAMS-nikhef/amstrax',
+                 long_description=readme + '\n\n' + history,
+                 long_description_content_type="text/markdown",
+                 setup_requires=['pytest-runner'],
+                 install_requires=requires,
+                 tests_require=requires + [
+                     'pytest',
+                     'hypothesis',
+                     'boltons',
+                     'flake8',
+                 ],
+                 python_requires=">=3.6",
+                 packages=setuptools.find_packages(),
+                 scripts=[],
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: BSD License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.6',
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: Implementation :: CPython',
+                     'Topic :: Scientific/Engineering :: Physics',
+                 ],
+                 zip_safe=False)
