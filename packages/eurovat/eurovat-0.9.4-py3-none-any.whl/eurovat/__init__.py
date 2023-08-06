d78f391b52e30f7bb050e35dd3eff1e0971c9f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:48 +0000

    Add package eurovat/0.9.4/eurovat-0.9.4-py3-none-any.whl

---
+++ packages/eurovat/eurovat-0.9.4-py3-none-any.whl/eurovat/__init__.py
@@ -0,0 +1,10 @@
+from eurovat.registry import VatRuleRegistry
+from eurovat.cache import Cache, FilesystemCache
+from eurovat.rate import VatRate
+from eurovat.states import states
+from eurovat.cn_code import CnCode
+from eurovat.vat_number import VatNumber
+
+from eurovat.functions import get_vat_rate
+
+__version__ = "0.9.4"
