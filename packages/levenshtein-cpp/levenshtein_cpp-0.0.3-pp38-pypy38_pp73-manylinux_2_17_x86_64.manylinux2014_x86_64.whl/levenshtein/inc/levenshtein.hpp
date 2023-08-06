474ed9330814d449e6c849daf95689ac3c90739a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:48 +0000

    Add package levenshtein-cpp/0.0.3/levenshtein_cpp-0.0.3-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/levenshtein-cpp/levenshtein_cpp-0.0.3-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/levenshtein/inc/levenshtein.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+namespace lev {
+static constexpr unsigned VERSION_MAJOR = 0;
+static constexpr unsigned VERSION_MINOR = 0;
+static constexpr unsigned VERSION_PATCH = 3;
+
+int distance(std::string a, std::string b);
+
+float ratio(std::string a, std::string b);
+} // namespace lev
