e03393ca02e04029519c5b386a9d2adee26af683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:22 +0000

    Add package aria2-rpc-client/0.4.0/aria2_rpc_client-0.4.0.tar.gz

---
+++ packages/aria2-rpc-client/aria2_rpc_client-0.4.0.tar.gz/aria2_rpc_client-0.4.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aria2_rpc_client']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'aria2-rpc-client',
+    'version': '0.4.0',
+    'description': 'Aria2 RPC Client',
+    'long_description': None,
+    'author': 'Özkan ŞEN',
+    'author_email': 'ozkansen@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ozkansen/aria2_rpc_client',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
