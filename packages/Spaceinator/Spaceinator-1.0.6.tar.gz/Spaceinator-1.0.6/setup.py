9a9d6dde3adbe99c86ef7adff5fcc6d42f9b8b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:21 +0000

    Add package Spaceinator/1.0.6/Spaceinator-1.0.6.tar.gz

---
+++ packages/Spaceinator/Spaceinator-1.0.6.tar.gz/Spaceinator-1.0.6/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="Spaceinator",
+    packages=find_packages(exclude="test"),
+    version="1.0.6",
+    license='gpl-3.0',
+    author="John",
+    email="Tagmeh@gmail.com",
+    description="A simple application to press the spacebar within a user-specified timeframe.",
+    url="https://gitlab.com/Tagmeh/spaceinator",
+    keywords=['stay logged in'],
+    long_description=read("README.md"),
+    python_requires='>=3.7'
+)
+
+"""
+Build:
+py -m build
+
+Upload:
+py -m twine upload --repository pypi dist/*
+"""
