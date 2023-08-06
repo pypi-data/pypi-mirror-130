aa5af25d221941e3b119ff6437c13d195ab12c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:37 +0000

    Add package ordr/0.0.1/ordr-0.0.1.tar.gz

---
+++ packages/ordr/ordr-0.0.1.tar.gz/ordr-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name='ordr',
+  version='0.0.1',
+  author='Francesco Boffa',
+  author_email='francesco@ordr.menu',
+  packages=['ordr'],
+  url='https://ordr.menu',
+  license='MIT',
+  description='Python SDK for the ORDR API',
+  install_requires=[
+      "pytest"
+  ],
+)
