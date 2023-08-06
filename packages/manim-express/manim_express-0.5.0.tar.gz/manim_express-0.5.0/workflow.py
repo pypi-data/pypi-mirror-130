7aa968b0db29706b879a620bc296c1819a922b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:36 +0000

    Add package manim-express/0.5.0/manim_express-0.5.0.tar.gz

---
+++ packages/manim-express/manim_express-0.5.0.tar.gz/manim_express-0.5.0/workflow.py
@@ -0,0 +1,15 @@
+from sparrow.version_ops import VersionControl
+from tools import update_requirements, update_setup_cfg
+
+pkgname = "manim_express"
+pkgdir = pkgname
+vc = VersionControl(pkgname, pkgdir)
+# vc.update_version()
+
+update_setup_cfg()
+# To keep manimlib latest.
+update_requirements()
+# vc.update_readme(readme_path="./README.md", license="MIT", version="0.5.0")
+# vc.update_readme(readme_path="./README_zh.md", license="MIT", version="0.5.0")
+# os.system("yapf -i -r ./manim_express")
+vc.upload_pypi()
