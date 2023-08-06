504d67c3c3a2d589f806eb3d20675979e02ac496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:30 +0000

    Add package chaosmagpy/0.8/chaosmagpy-0.8-py3-none-any.whl

---
+++ packages/chaosmagpy/chaosmagpy-0.8-py3-none-any.whl/chaosmagpy/__init__.py
@@ -0,0 +1,13 @@
+from .chaos import (CHAOS, load_CHAOS_matfile, load_CHAOS_shcfile,
+                    load_CovObs_txtfile, load_gufm1_txtfile)
+from .config_utils import basicConfig
+from .model_utils import synth_values
+
+
+__all__ = [
+    "CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile",
+    "load_CovObs_txtfile", "load_gufm1_txtfile",
+    "basicConfig", "synth_values"
+]
+
+__version__ = "0.8"
