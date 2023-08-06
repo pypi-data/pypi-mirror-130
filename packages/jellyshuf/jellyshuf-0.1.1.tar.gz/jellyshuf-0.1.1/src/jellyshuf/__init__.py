1364990b953b8c6e512567d021fdd79d35c9b07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:33 +0000

    Add package jellyshuf/0.1.1/jellyshuf-0.1.1.tar.gz

---
+++ packages/jellyshuf/jellyshuf-0.1.1.tar.gz/jellyshuf-0.1.1/src/jellyshuf/__init__.py
@@ -0,0 +1,115 @@
+#future
+from __future__ import annotations
+
+# standard library
+import logging
+from typing import Union, Mapping, List
+from sys import argv
+import itertools 
+
+# pip installable
+import musicpd
+
+
+from jellyshuf import jellyfin
+
+""" Parts of this file are modified from mopidy-jellyfin. See LICENSE-mopidy-jellyfin.
+"""
+
+JSON = Union[str, int, float, bool, None, Mapping[str, 'JSON'], List['JSON']]
+logger = logging.getLogger(__name__)
+helpstr = '''
+USAGE: jellyshuf <FLAGS> TYPE NUMBER
+    
+TYPE is one of artists, albums, songs 
+    
+FLAGS: 
+    --stdout        Instead of adding retrieved paths to mpd queue, emits them to stdout (line separated)
+    --random|-r     Set mpd to random mode after adding new items
+    --start|-s      Start mpd after adding new items
+    --clear|-c      Clear mpd queue before adding items
+    --config        Run config (overwriting existing info if necessary) then exit
+    --help|-h       Display this message and exit
+'''
+
+def cli():
+    add_to_mpd = True
+    start_mpd = False
+    set_mpd_random = False
+    mpd_clear = False
+    args = argv[1:] # discard binary/file name
+    
+    if len(args) < 2:
+        print("ERROR: Not enough args") 
+        print(helpstr)
+        return
+
+    while args[0].startswith('-'): 
+        flag = args.pop(0).casefold()
+        if flag == '--stdout': 
+            add_to_mpd = False 
+        elif flag == '--config': 
+            jf = jellyfin.CliClient(overwrite=True)
+            return 
+        elif flag  == '--help': 
+            print(helpstr)
+            return
+        elif flag == '--start':
+            start_mpd = True 
+        elif flag == '--random': 
+            set_mpd_random = True 
+        elif flag == '--clear':
+            mpd_clear = True
+        else:
+            flag = flag[1:]
+            while flag != '':
+                if flag == 'h': 
+                    print(helpstr)
+                    return
+                elif flag == 's':
+                    start_mpd = True 
+                elif flag == 'r':
+                    set_mpd_random = True
+                elif flag == 'h': 
+                    print(helpstr)
+                    return
+                elif flag == 'c': 
+                    mpd_clear = True
+                flag = flag[1:]
+
+    mpd = musicpd.MPDClient()
+    mpd.connect()
+    jf = jellyfin.CliClient()
+
+    if args[0].casefold() == 'albums'.casefold(): 
+        gen = jf.shuf_all_albums()
+    elif args[0].casefold() == 'artists'.casefold():
+        gen = jf.shuf_all_artists()
+    elif args[0].casefold() == 'songs'.casefold(): 
+        gen = jf.shuf_all_songs()
+    else:
+        print("ERROR: Invalid argument for type") 
+        print(helpstr)
+        return
+    
+    if mpd_clear:
+        mpd.clear()
+
+    if add_to_mpd:
+        for path in itertools.islice(gen, int(args[1])):
+            try: 
+                mpd.add(path)
+                print(f'Added {path}')
+            except musicpd.CommandError as e: 
+                print('Failed to add {}'.format(path))
+                print(str(e))
+    else: 
+        for path in itertools.islice(gen, int(args[1])):
+            print('{}'.format(path))
+
+    if set_mpd_random: 
+        mpd.random()
+    if start_mpd: 
+        mpd.play()
+
+    mpd.disconnect()
---
+++ packages/jellyshuf/jellyshuf-0.1.1.tar.gz/jellyshuf-0.1.1/src/jellyshuf/settings.py
@@ -0,0 +1,114 @@
+from getpass import getpass
+from os import stat
+import uuid 
+import socket 
+import logging 
+import json 
+from typing import Union, Mapping, List, Dict
+from pathlib import Path
+from appdirs import AppDirs
+import datetime
+
+JSON = Union[str, int, float, bool, None, Mapping[str, 'JSON'], List['JSON']]
+logger = logging.getLogger(__name__)
+
+class CliSettingsManager():
+    def __init__(self) -> None:
+        # preset constants
+        self.APPNAME = 'jellyshuf'
+        self.APPVER = '0.1.1' 
+        self.AUTHOR = 'def'
+        self.CLIENT_UUID = str(uuid.uuid4())
+        self.HOSTNAME = socket.gethostname()
+        self.MPD_PATH_PREFIX = 'Jellyfin/Music' # do not include trailing slash
+        self.TRANSLATE_MPD_PATH = {
+            ord('/'): None
+        }
+        self.DATEFMT = '%d/%m/%Y'
+        self.CONFIG_KEYS = ['url', 'user', 'pass', 'view', 'cache'] # pass should be stored in keyring! eventually! maybe ..
+
+        dirs = AppDirs(self.APPNAME, self.AUTHOR)
+        self.userpath = Path(dirs.user_config_dir).joinpath("config.json")
+        self.cachepath = Path(dirs.user_cache_dir).joinpath('jfcache.json')
+        
+        if self.userpath.is_file():
+            with open(self.userpath, 'r') as cf: 
+                try:    
+                    self.user =  json.load(cf)
+                except json.decoder.JSONDecodeError: 
+                    self.cache = {}
+                    self.cachepath.unlink() 
+        else: 
+            self.user = {} 
+    
+        if self.cachepath.is_file():
+            with open(self.cachepath, 'r') as f: 
+                try: 
+                    self.cache = json.load(f)
+                except json.decoder.JSONDecodeError: 
+                    self.cache = {}
+                    self.cachepath.unlink()
+        else: 
+            self.cache = {}
+
+    def get_settings_cli(self, overwrite=False) -> None: 
+        def bool_to_str(s: str) -> bool: 
+            return s.lower() in ("yes", 'y', "true", "t", "1")
+        
+        if overwrite or self.user.get('url') is None: 
+            url = str(input('Enter server url (including proto and port (if non-standard): '))
+            if url.endswith('/'): 
+                url = url[:-1]
+            self.user['url'] = url
+
+        if overwrite or self.user.get('user') is None: 
+            self.user['user'] = input('Enter jellyfin username: ')
+
+        if overwrite or self.user.get('pass') is None: 
+            self.user['pass'] = getpass('Enter jellyfin password: ')
+
+        if overwrite or self.user.get('cache') is None: 
+            self.user['cache'] = bool_to_str(input('Do you want to cache results from jellyfin api (yes/no): '))
+        
+        if self.user['cache'] and (overwrite or self.user.get('cachedays') is None): 
+            self.user['cachedays'] = int(input('How many days do you want to cache jellyfin data for (integer): '))
+            
+    def get_view_cli(self, views: List[Dict[str, str]], overwrite=False):
+        try: 
+            view = self.user['ViewId']
+            if overwrite or view not in [e['Id'] for e in views]: 
+                raise KeyError 
+        except KeyError: 
+            print("Please select a view.")
+            for i, view in enumerate(views): 
+                print('    {}: {}'.format(i, view['Name']))
+            self.user['ViewId'] = views[int(input("Enter view index: "))]['Id']
+    
+    @staticmethod
+    def _touch_file(path: Path):
+        if not path.exists(): 
+            path.parent.mkdir(parents=True, exist_ok=True) 
+            path.touch()
+
+    def get_cache(self, key: str) -> Union[dict, None]: 
+        try: 
+            cache = self.cache[key]
+        except KeyError: 
+            return None 
+        if (datetime.datetime.today() - datetime.datetime.strptime(cache['date'], self.DATEFMT)).days < self.user['cachedays']: 
+            return cache['data']
+        return None
+
+    def save_cache(self, key: str, data: dict) -> None: 
+        self._touch_file(self.cachepath)
+        self.cache[key] = {
+            'date': datetime.date.today().strftime(self.DATEFMT),
+            'data': data
+        }
+        with open(self.cachepath, 'w') as f: 
+            json.dump(self.cache, f)
+
+    def save_config(self) -> None:
+        self._touch_file(self.userpath)
+        with open(self.userpath, 'w') as f: 
+            json.dump(self.user, f)
