6234d703cba59a62b40a9339f989c1ae941616c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:20 +0000

    Add package qq.py/0.2.1/qq.py-0.2.1-py3-none-any.whl

---
+++ packages/qq.py/qq.py-0.2.1-py3-none-any.whl/qq/__init__.py
@@ -0,0 +1,37 @@
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+import logging
+from typing import NamedTuple
+from .client import *
+from .user import *
+from .channel import *
+from .guild import *
+from .flags import *
+from .member import *
+from .message import *
+from .asset import *
+from .role import *
+from .file import *
+from .colour import *
+from .object import *
+from . import utils, abc
+from .enum import *
+from .shard import *
+from .error import *
+from .embeds import *
+from .mention import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=0, minor=2, micro=1, releaselevel='alpha', serial=0)
+__title__ = 'qq'
+__author__ = 'Foxwhite'
+__license__ = 'MIT'
+__version__ = f'0.2.1'
