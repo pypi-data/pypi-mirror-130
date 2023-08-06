97328d65c15b5eda42f51158c7ac56503894bc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:48 +0000

    Add package evalml/0.39.0/evalml-0.39.0.tar.gz

---
+++ packages/evalml/evalml-0.39.0.tar.gz/evalml-0.39.0/evalml/pipelines/components/transformers/preprocessing/featuretools.py
@@ -0,0 +1,80 @@
+"""Featuretools DFS component that generates features for the input features."""
+from featuretools import EntitySet, calculate_feature_matrix, dfs
+
+from evalml.pipelines.components.transformers.transformer import Transformer
+from evalml.utils import infer_feature_types
+
+
+class DFSTransformer(Transformer):
+    """Featuretools DFS component that generates features for the input features.
+
+    Args:
+        index (string): The name of the column that contains the indices. If no column with this name exists,
+            then featuretools.EntitySet() creates a column with this name to serve as the index column. Defaults to 'index'.
+        random_seed (int): Seed for the random number generator. Defaults to 0.
+    """
+
+    name = "DFS Transformer"
+    hyperparameter_ranges = {}
+    """{}"""
+
+    def __init__(self, index="index", random_seed=0, **kwargs):
+        parameters = {"index": index}
+        if not isinstance(index, str):
+            raise TypeError(f"Index provided must be string, got {type(index)}")
+
+        self.index = index
+        self.features = None
+        parameters.update(kwargs)
+        super().__init__(parameters=parameters, random_seed=random_seed)
+
+    def _make_entity_set(self, X):
+        """Helper method that creates and returns the entity set given the input data."""
+        ft_es = EntitySet()
+        # TODO: This delete was introduced for compatibility with Featuretools 1.0.0.  This should
+        # be removed after Featuretools handles unnamed dataframes being passed to this function.
+        del X.ww
+        if self.index not in X.columns:
+            es = ft_es.add_dataframe(
+                dataframe=X, dataframe_name="X", index=self.index, make_index=True
+            )
+        else:
+            es = ft_es.add_dataframe(dataframe=X, dataframe_name="X", index=self.index)
+        return es
+
+    def fit(self, X, y=None):
+        """Fits the DFSTransformer Transformer component.
+
+        Args:
+            X (pd.DataFrame, np.array): The input data to transform, of shape [n_samples, n_features].
+            y (pd.Series): The target training data of length [n_samples].
+
+        Returns:
+            self
+        """
+        X_ww = infer_feature_types(X)
+        X_ww = X_ww.ww.rename({col: str(col) for col in X_ww.columns})
+        es = self._make_entity_set(X_ww)
+        self.features = dfs(
+            entityset=es, target_dataframe_name="X", features_only=True, max_depth=1
+        )
+        return self
+
+    def transform(self, X, y=None):
+        """Computes the feature matrix for the input X using featuretools' dfs algorithm.
+
+        Args:
+            X (pd.DataFrame or np.ndarray): The input training data to transform. Has shape [n_samples, n_features]
+            y (pd.Series, optional): Ignored.
+
+        Returns:
+            pd.DataFrame: Feature matrix
+        """
+        X_ww = infer_feature_types(X)
+        X_ww = X_ww.ww.rename({col: str(col) for col in X_ww.columns})
+        es = self._make_entity_set(X_ww)
+
+        feature_matrix = calculate_feature_matrix(features=self.features, entityset=es)
+        typed_columns = set(X_ww.columns).intersection(set(feature_matrix.columns))
+        feature_matrix.ww.init(schema=X_ww.ww.schema._get_subset_schema(typed_columns))
+        return feature_matrix
---
+++ packages/evalml/evalml-0.39.0.tar.gz/evalml-0.39.0/evalml/tests/automl_tests/test_search.py
@@ -0,0 +1,152 @@
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+
+from evalml.automl import AutoMLSearch, search
+from evalml.automl.automl_algorithm import DefaultAlgorithm
+from evalml.utils import infer_feature_types
+
+
+@patch("evalml.data_checks.default_data_checks.DefaultDataChecks.validate")
+@patch("evalml.automl.AutoMLSearch.search")
+def test_search(mock_automl_search, mock_data_checks_validate, X_y_binary):
+    X, y = X_y_binary
+    # this doesn't exactly match the data check results schema but its enough to trigger the error in search()
+    data_check_results_expected = {"warnings": ["Warning 1", "Warning 2"]}
+    mock_data_checks_validate.return_value = data_check_results_expected
+    automl, data_check_results = search(X_train=X, y_train=y, problem_type="binary")
+    assert isinstance(automl, AutoMLSearch)
+    assert data_check_results is data_check_results_expected
+    mock_data_checks_validate.assert_called_once()
+    data, target = (
+        mock_data_checks_validate.call_args[0][0],
+        mock_data_checks_validate.call_args[1]["y"],
+    )
+    pd.testing.assert_frame_equal(data, infer_feature_types(X))
+    pd.testing.assert_series_equal(target, infer_feature_types(y))
+    mock_automl_search.assert_called_once()
+
+
+@patch("evalml.data_checks.default_data_checks.DefaultDataChecks.validate")
+@patch("evalml.automl.AutoMLSearch.search")
+def test_search_data_check_error(
+    mock_automl_search, mock_data_checks_validate, X_y_binary
+):
+    X, y = X_y_binary
+    # this doesn't exactly match the data check results schema but its enough to trigger the error in search()
+    data_check_results_expected = {"errors": ["Error 1", "Error 2"]}
+    mock_data_checks_validate.return_value = data_check_results_expected
+    automl, data_check_results = search(X_train=X, y_train=y, problem_type="binary")
+    assert automl is None
+    assert data_check_results == data_check_results_expected
+    mock_data_checks_validate.assert_called_once()
+    data, target = (
+        mock_data_checks_validate.call_args[0][0],
+        mock_data_checks_validate.call_args[1]["y"],
+    )
+    pd.testing.assert_frame_equal(data, infer_feature_types(X))
+    pd.testing.assert_series_equal(target, infer_feature_types(y))
+
+
+@pytest.mark.parametrize(
+    "problem_config", [None, "missing_date_index", "has_date_index"]
+)
+def test_search_data_check_error_timeseries(problem_config):
+    X, y = pd.DataFrame({"features": range(30)}), pd.Series(range(30))
+    problem_configuration = None
+
+    dates = pd.date_range("2021-01-01", periods=29).append(
+        pd.date_range("2021-01-31", periods=1)
+    )
+    X["dates"] = dates
+
+    if problem_config == "missing_date_index":
+        problem_configuration = {"gap": 4}
+        with pytest.raises(
+            ValueError,
+            match="date_index has to be passed in problem_configuration.",
+        ):
+            search(
+                X_train=X,
+                y_train=y,
+                problem_type="time series regression",
+                problem_configuration=problem_configuration,
+            )
+    elif not problem_config:
+        with pytest.raises(
+            ValueError,
+            match="the problem_configuration parameter must be specified.",
+        ):
+            search(
+                X_train=X,
+                y_train=y,
+                problem_type="time series regression",
+                problem_configuration=problem_configuration,
+            )
+    else:
+        problem_configuration = {"date_index": "dates"}
+        automl, data_check_results = search(
+            X_train=X,
+            y_train=y,
+            problem_type="time series regression",
+            problem_configuration=problem_configuration,
+        )
+        assert len(data_check_results["warnings"]) == 1
+        assert len(data_check_results["errors"]) == 1
+
+
+@patch("evalml.data_checks.default_data_checks.DefaultDataChecks.validate")
+@patch("evalml.automl.AutoMLSearch.search")
+def test_search_args(mock_automl_search, mock_data_checks_validate, X_y_binary):
+    X, y = X_y_binary
+    automl, _ = search(
+        X_train=X,
+        y_train=y,
+        problem_type="binary",
+        max_time=42,
+        patience=3,
+        tolerance=0.5,
+        mode="fast",
+    )
+    assert automl.max_time == 42
+    assert automl.patience == 3
+    assert automl.tolerance == 0.5
+    assert automl.max_batches == 4
+    assert isinstance(automl._automl_algorithm, DefaultAlgorithm)
+
+    automl, _ = search(
+        X_train=X,
+        y_train=y,
+        problem_type="binary",
+        max_time=42,
+        patience=3,
+        tolerance=0.5,
+        mode="long",
+    )
+    assert automl.max_time == 42
+    assert automl.patience == 3
+    assert automl.tolerance == 0.5
+    assert automl.max_batches == 999
+    assert isinstance(automl._automl_algorithm, DefaultAlgorithm)
+
+    automl, _ = search(
+        X_train=X,
+        y_train=y,
+        problem_type="binary",
+        mode="long",
+    )
+
+    assert automl.max_batches == 6
+    assert isinstance(automl._automl_algorithm, DefaultAlgorithm)
+
+    with pytest.raises(ValueError):
+        search(
+            X_train=X,
+            y_train=y,
+            problem_type="binary",
+            max_time=42,
+            patience=3,
+            tolerance=0.5,
+            mode="everything",
+        )
