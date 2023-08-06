bd9d6909725d3beac5a9217a3d6a28ce75a99c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:52 +0000

    Add package PyMyORM/1.0.8/PyMyORM-1.0.8.tar.gz

---
+++ packages/PyMyORM/PyMyORM-1.0.8.tar.gz/PyMyORM-1.0.8/tests/my_admin.py
@@ -0,0 +1,47 @@
+from pymyorm.database import Database
+from config import db
+from models.admin import Admin
+from models.admin_role import AdminRole
+from models.admin_auth import AdminAuth
+
+
+def main():
+
+    Database().connect(**db)
+
+    Admin.find().truncate()
+    AdminRole.find().truncate()
+    AdminAuth.find().truncate()
+
+    for name in ['role1', 'role2', 'role3']:
+        exists = AdminRole.find().where(name=name).exists()
+        if not exists:
+            AdminRole(name=name).save()
+
+    fp = open('admin.txt', 'r')
+    for line in fp:
+        line = line.strip('\r\n')
+        username, phone, password, role, type, lock = line.split()
+        model = Admin()
+        model.username = username
+        model.phone = phone
+        model.password = password
+        model.role = role
+        model.type = type
+        model.lock = lock
+        model.save()
+    fp.close()
+
+    fp = open('admin_auth.txt', 'r')
+    for line in fp:
+        line = line.strip('\r\n')
+        role, action = line.split()
+        model = AdminAuth()
+        model.role = role
+        model.action = action
+        model.save()
+    fp.close()
+
+
+if __name__ == '__main__':
+    main()
